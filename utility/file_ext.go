5d942ac4996fbeb35cd99acbe61b26057133bdfd
Conor Hennessy <conor@soundcloud.com>
2013-10-10 20:54:06 +0200

    Add a check for metrics directory existence.
    Previously on startup the program would just quit without stating
    explicitly why.
    
    Change-Id: I833b85eb74d2dd27cdc3f0f2e65d7bb1c42caa39

---
+++ utility/file_ext.go
@@ -0,0 +1,35 @@
+// Copyright 2013 Prometheus Team
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/*
+Useful file/filesystem related functions.
+*/
+
+package utility
+
+import (
+	"fmt"
+	"os"
+)
+
+// Returns true iff dirPath is a valid directory path.
+func IsDir(dirPath string) (bool, error) {
+	finfo, err := os.Stat(dirPath)
+	if err != nil {
+		return false, err
+	}
+	if !finfo.IsDir() {
+		return false, fmt.Errorf("%s not a directory", dirPath)
+	}
+	return true, nil
+}
