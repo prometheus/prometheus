f3d047313feccac6ffa3cbd81834509eccc44625
Julius Volz <julius@soundcloud.com>
2013-12-09 17:03:49 +0100

    Add optional sample replication to OpenTSDB.
    Prometheus needs long-term storage. Since we don't have enough resources
    to build our own timeseries storage from scratch ontop of Riak,
    Cassandra or a similar distributed datastore at the moment, we're
    planning on using OpenTSDB as long-term storage for Prometheus. It's
    data model is roughly compatible with that of Prometheus, with some
    caveats.
    
    As a first step, this adds write-only replication from Prometheus to
    OpenTSDB, with the following things worth noting:
    
    1)
    I tried to keep the integration lightweight, meaning that anything
    related to OpenTSDB is isolated to its own package and only main knows
    about it (essentially it tees all samples to both the existing storage
    and TSDB). It's not touching the existing TieredStorage at all to avoid
    more complexity in that area. This might change in the future,
    especially if we decide to implement a read path for OpenTSDB through
    Prometheus as well.
    
    2)
    Backpressure while sending to OpenTSDB is handled by simply dropping
    samples on the floor when the in-memory queue of samples destined for
    OpenTSDB runs full.  Prometheus also only attempts to send samples once,
    rather than implementing a complex retry algorithm. Thus, replication to
    OpenTSDB is best-effort for now.  If needed, this may be extended in the
    future.
    
    3)
    Samples are sent in batches of limited size to OpenTSDB. The optimal
    batch size, timeout parameters, etc. may need to be adjusted in the
    future.
    
    4)
    OpenTSDB has different rules for legal characters in tag (label) values.
    While Prometheus allows any characters in label values, OpenTSDB limits
    them to a to z, A to Z, 0 to 9, -, _, . and /. Currently any illegal
    characters in Prometheus label values are simply replaced by an
    underscore. Especially when integrating OpenTSDB with the read path in
    Prometheus, we'll need to reconsider this: either we'll need to
    introduce the same limitations for Prometheus labels or escape/encode
    illegal characters in OpenTSDB in such a way that they are fully
    decodable again when reading through Prometheus, so that corresponding
    timeseries in both systems match in their labelsets.
    
    Change-Id: I8394c9c55dbac3946a0fa497f566d5e6e2d600b5

---
+++ storage/remote/queue_manager_test.go
@@ -0,0 +1,77 @@
+// Copyright 2013 Prometheus Team
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package remote
+
+import (
+	"sync"
+	"testing"
+
+	clientmodel "github.com/prometheus/client_golang/model"
+)
+
+type TestTSDBClient struct {
+	receivedSamples clientmodel.Samples
+	expectedSamples clientmodel.Samples
+	wg              sync.WaitGroup
+}
+
+func (c *TestTSDBClient) expectSamples(s clientmodel.Samples) {
+	c.expectedSamples = append(c.expectedSamples, s...)
+	c.wg.Add(len(s))
+}
+
+func (c *TestTSDBClient) waitForExpectedSamples(t *testing.T) {
+	c.wg.Wait()
+	for i, expected := range c.expectedSamples {
+		if !expected.Equal(c.receivedSamples[i]) {
+			t.Fatalf("%d. Expected %v, got %v", i, expected, c.receivedSamples[i])
+		}
+	}
+}
+
+func (c *TestTSDBClient) Store(s clientmodel.Samples) error {
+	c.receivedSamples = append(c.receivedSamples, s...)
+	c.wg.Add(-len(s))
+	return nil
+}
+
+func TestSampleDelivery(t *testing.T) {
+	// Let's create an even number of send batches so we don't run into the
+	// batch timeout case.
+	n := maxSamplesPerSend * 2
+
+	samples := make(clientmodel.Samples, 0, n)
+	for i := 0; i < n; i++ {
+		samples = append(samples, &clientmodel.Sample{
+			Metric: clientmodel.Metric{
+				clientmodel.MetricNameLabel: "test_metric",
+			},
+			Value: clientmodel.SampleValue(i),
+		})
+	}
+
+	c := &TestTSDBClient{}
+	c.expectSamples(samples[:len(samples)/2])
+	m := NewTSDBQueueManager(c, 1)
+
+	// These should be received by the client.
+	m.Queue(samples[:len(samples)/2])
+	// These will be dropped because the queue is full.
+	m.Queue(samples[len(samples)/2:])
+
+	go m.Run()
+	defer m.Close()
+
+	c.waitForExpectedSamples(t)
+}
