a7d3d5b1a01040f1f6e6002a4607c2c1cf9d7849
Julius Volz <julius@soundcloud.com>
2013-12-09 17:03:49 +0100

    Add optional sample replication to OpenTSDB.
    Prometheus needs long-term storage. Since we don't have enough resources to
    build our own timeseries storage from scratch ontop of Riak, Cassandra or a
    similar distributed datastore at the moment, we're planning on using OpenTSDB
    as long-term storage for Prometheus. It's data model is roughly compatible with
    that of Prometheus, with some caveats.
    
    As a first step, this adds write-only replication from Prometheus to OpenTSDB,
    with the following things worth noting:
    
    1)
    I tried to keep the integration lightweight, meaning that anything related to
    OpenTSDB is isolated to its own package and only main knows about it
    (essentially it tees all samples to both the existing storage and TSDB). It's
    not touching the existing TieredStorage at all to avoid more complexity in that
    area. This might change in the future, especially if we decide to implement a
    read path for OpenTSDB through Prometheus as well.
    
    2)
    Backpressure while sending to OpenTSDB is handled by simply dropping samples on
    the floor when the in-memory queue of samples destined for OpenTSDB runs full.
    Prometheus also only attempts to send samples once, rather than implementing a
    complex retry algorithm. Thus, replication to OpenTSDB is best-effort for now.
    If needed, this may be extended in the future.
    
    3)
    Samples are sent in batches of limited size to OpenTSDB. The optimal batch
    size, timeout parameters, etc. may need to be adjusted in the future.
    
    4)
    OpenTSDB has different rules for legal characters in tag (label) values. While
    Prometheus allows any characters in label values, OpenTSDB limits them to a to
    z, A to Z, 0 to 9, -, _, . and /. Currently any illegal characters in
    Prometheus label values are simply replaced by an underscore. Especially when
    integrating OpenTSDB with the read path in Prometheus, we'll need to reconsider
    this: either we'll need to introduce the same limitations for Prometheus labels
    or escape/encode illegal characters in OpenTSDB in such a way that they are
    fully decodable again when reading through Prometheus, so that corresponding
    timeseries in both systems match in their labelsets.
    
    Change-Id: I8394c9c55dbac3946a0fa497f566d5e6e2d600b5

---
+++ tsdb/queue_manager.go
@@ -0,0 +1,144 @@
+// Copyright 2013 Prometheus Team
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package tsdb
+
+import (
+	"time"
+
+	"github.com/golang/glog"
+
+	clientmodel "github.com/prometheus/client_golang/model"
+)
+
+const (
+	maxConcurrentSends = 10
+	maxSamplesPerSend  = 100
+	batchSendDeadline  = 5 * time.Second
+)
+
+// TSDBClient defines an interface for sending a batch of samples to an
+// external timeseries database (TSDB).
+type TSDBClient interface {
+	Store(clientmodel.Samples) error
+}
+
+// TSDBQueueManager manages a queue of samples to be sent to the TSDB indicated
+// by the provided TSDBClient.
+type TSDBQueueManager struct {
+	tsdb           TSDBClient
+	queue          chan clientmodel.Samples
+	pendingSamples clientmodel.Samples
+	sendSemaphore  chan bool
+	stop           chan bool
+	drained        chan bool
+}
+
+// Build a new TSDBQueueManager.
+func NewTSDBQueueManager(tsdb TSDBClient, queueCapacity int) *TSDBQueueManager {
+	return &TSDBQueueManager{
+		tsdb:          tsdb,
+		queue:         make(chan clientmodel.Samples, queueCapacity),
+		sendSemaphore: make(chan bool, maxConcurrentSends),
+		stop:          make(chan bool),
+		drained:       make(chan bool),
+	}
+}
+
+// Queue a sample batch to be sent to the TSDB. This drops the most recently
+// queued samples on the floor if the queue is full.
+func (t *TSDBQueueManager) Queue(s clientmodel.Samples) {
+	if len(t.queue) == cap(t.queue) {
+		samplesCount.IncrementBy(map[string]string{result: dropped}, float64(len(s)))
+		glog.Warningf("TSDB queue full, discarding %d samples", len(s))
+		return
+	}
+	t.queue <- s
+}
+
+func (t *TSDBQueueManager) sendSamples(s clientmodel.Samples) {
+	defer func() {
+		<-t.sendSemaphore
+	}()
+
+	// Samples are sent to the TSDB on a best-effort basis. If a sample isn't
+	// sent correctly the first time, it's simply dropped on the floor.
+	begin := time.Now()
+	err := t.tsdb.Store(s)
+	recordOutcome(time.Since(begin), len(s), err)
+
+	if err != nil {
+		glog.Warningf("error sending %d samples to TSDB: %s", len(s), err)
+	}
+}
+
+// Report notification queue occupancy and capacity.
+func (t *TSDBQueueManager) reportQueues() {
+	queueSize.Set(map[string]string{facet: occupancy}, float64(len(t.queue)))
+	queueSize.Set(map[string]string{facet: capacity}, float64(cap(t.queue)))
+}
+
+// Continuously send samples to the TSDB.
+func (t *TSDBQueueManager) Run() {
+	queueReportTicker := time.NewTicker(time.Second)
+	go func() {
+		for _ = range queueReportTicker.C {
+			t.reportQueues()
+		}
+	}()
+	defer queueReportTicker.Stop()
+
+	// Send batches of at most maxSamplesPerSend samples to the TSDB. If we
+	// have fewer samples than that, flush them out after a deadline anyways.
+	for {
+		select {
+		case s, ok := <-t.queue:
+			if !ok {
+				glog.Infof("Flushing %d samples to OpenTSDB...", len(t.pendingSamples))
+				t.flush()
+				glog.Infof("Done flushing.")
+				t.drained <- true
+				return
+			}
+
+			t.pendingSamples = append(t.pendingSamples, s...)
+
+			for len(t.pendingSamples) >= maxSamplesPerSend {
+				t.sendSemaphore <- true
+				go t.sendSamples(t.pendingSamples[:maxSamplesPerSend])
+				t.pendingSamples = t.pendingSamples[maxSamplesPerSend:]
+			}
+		case <-time.After(batchSendDeadline):
+			t.flush()
+		}
+	}
+}
+
+// Flush remaining queued samples.
+func (t *TSDBQueueManager) flush() {
+	if len(t.pendingSamples) > 0 {
+		t.sendSemaphore <- true
+		go t.sendSamples(t.pendingSamples)
+	}
+	t.pendingSamples = clientmodel.Samples{}
+}
+
+// Stop sending samples to the TSDB and wait for pending sends to complete.
+func (t *TSDBQueueManager) Close() {
+	glog.Infof("TSDB queue manager shutting down...")
+	close(t.queue)
+	<-t.drained
+	for i := 0; i < maxConcurrentSends; i++ {
+		t.sendSemaphore <- true
+	}
+}
