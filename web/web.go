prometheus/prometheus

commit 2128d9d81196f92432c4016bccfeab3f7af3f06a

Migrate to new client_golang.
This change will only be submitted when the new client_golang has been
moved to the new version.

Change-Id: Ifceb59333072a08286a8ac910709a8ba2e3a1581

web/web.go
@@ -19,7 +19,7 @@ import (
 	"html/template"
 	"net"
 	"net/http"
-	"net/http/pprof"
+	_ "net/http/pprof"
 	"os"
 	"time"
 
@@ -27,7 +27,6 @@ import (
 
 	"github.com/golang/glog"
 	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/exp"
 
 	"github.com/prometheus/prometheus/web/api"
 	"github.com/prometheus/prometheus/web/blob"
@@ -52,43 +51,54 @@ type WebService struct {
 }
 
 func (w WebService) ServeForever() error {
-	exp.Handle("/favicon.ico", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	http.Handle("/favicon.ico", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "", 404)
 	}))
 
-	// TODO(julius): This will need to be rewritten once the exp package provides
-	// the coarse mux behaviors via a wrapper function.
-	exp.Handle("/debug/pprof/", http.HandlerFunc(pprof.Index))
-	exp.Handle("/debug/pprof/cmdline", http.HandlerFunc(pprof.Cmdline))
-	exp.Handle("/debug/pprof/profile", http.HandlerFunc(pprof.Profile))
-	exp.Handle("/debug/pprof/symbol", http.HandlerFunc(pprof.Symbol))
-
-	exp.Handle("/", w.StatusHandler)
-	exp.Handle("/databases", w.DatabasesHandler)
-	exp.Handle("/alerts", w.AlertsHandler)
-	exp.Handle("/consoles/", http.StripPrefix("/consoles/", w.ConsolesHandler))
-	exp.HandleFunc("/graph", graphHandler)
-	exp.HandleFunc("/heap", dumpHeap)
+	http.Handle("/", prometheus.InstrumentHandler(
+		"/", w.StatusHandler,
+	))
+	http.Handle("/databases", prometheus.InstrumentHandler(
+		"/databases", w.DatabasesHandler,
+	))
+	http.Handle("/alerts", prometheus.InstrumentHandler(
+		"/alerts", w.AlertsHandler,
+	))
+	http.Handle("/consoles/", prometheus.InstrumentHandler(
+		"/consoles/", http.StripPrefix("/static/", http.FileServer(http.Dir("web/static"))),
+	))
+	http.Handle("/graph", prometheus.InstrumentHandler(
+		"/graph", http.HandlerFunc(graphHandler),
+	))
+	http.Handle("/heap", prometheus.InstrumentHandler(
+		"/heap", http.HandlerFunc(dumpHeap),
+	))
 
 	w.MetricsHandler.RegisterHandler()
-	exp.Handle("/metrics", prometheus.DefaultHandler)
+	http.Handle("/metrics", prometheus.Handler())
 	if *useLocalAssets {
-		exp.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("web/static"))))
+		http.Handle("/static/", prometheus.InstrumentHandler(
+			"/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("web/static"))),
+		))
 	} else {
-		exp.Handle("/static/", http.StripPrefix("/static/", new(blob.Handler)))
+		http.Handle("/static/", prometheus.InstrumentHandler(
+			"/static/", http.StripPrefix("/static/", new(blob.Handler)),
+		))
 	}
 
 	if *userAssetsPath != "" {
-		exp.Handle("/user/", http.StripPrefix("/user/", http.FileServer(http.Dir(*userAssetsPath))))
+		http.Handle("/user/", prometheus.InstrumentHandler(
+			"/user/", http.StripPrefix("/user/", http.FileServer(http.Dir(*userAssetsPath))),
+		))
 	}
 
 	if *enableQuit {
-		exp.HandleFunc("/-/quit", w.quitHandler)
+		http.Handle("/-/quit", http.HandlerFunc(w.quitHandler))
 	}
 
 	glog.Info("listening on ", *listenAddress)
 
-	return http.ListenAndServe(*listenAddress, exp.DefaultCoarseMux)
+	return http.ListenAndServe(*listenAddress, nil)
 }
 
 func (s WebService) quitHandler(w http.ResponseWriter, r *http.Request) {

main.go
@@ -23,6 +23,7 @@ import (
 
 	"github.com/golang/glog"
 	"github.com/prometheus/client_golang/extraction"
+	registry "github.com/prometheus/client_golang/prometheus"
 
 	clientmodel "github.com/prometheus/client_golang/model"
 
@@ -244,13 +245,15 @@ func main() {
 	if err != nil {
 		glog.Fatal("Error opening storage: ", err)
 	}
+	registry.MustRegister(ts)
 
-	var remoteTSDBQueue *remote.TSDBQueueManager = nil
+	var remoteTSDBQueue *remote.TSDBQueueManager
 	if *remoteTSDBUrl == "" {
 		glog.Warningf("No TSDB URL provided; not sending any samples to long-term storage")
 	} else {
 		openTSDB := opentsdb.NewClient(*remoteTSDBUrl, *remoteTSDBTimeout)
 		remoteTSDBQueue = remote.NewTSDBQueueManager(openTSDB, 512)
+		registry.MustRegister(remoteTSDBQueue)
 		go remoteTSDBQueue.Run()
 	}
 
@@ -285,6 +288,7 @@ func main() {
 	go ruleManager.Run()
 
 	notificationHandler := notification.NewNotificationHandler(*alertmanagerUrl, notifications)
+	registry.MustRegister(notificationHandler)
 	go notificationHandler.Run()
 
 	flags := map[string]string{}

notification/instrumentation.go
@@ -1,54 +1,0 @@
-// Copyright 2013 Prometheus Team
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-package notification
-
-import (
-	"time"
-
-	"github.com/prometheus/client_golang/prometheus"
-)
-
-const (
-	result  = "result"
-	success = "success"
-	failure = "failure"
-	dropped = "dropped"
-
-	facet     = "facet"
-	occupancy = "occupancy"
-	capacity  = "capacity"
-)
-
-var (
-	notificationsCount     = prometheus.NewCounter()
-	notificationLatency    = prometheus.NewDefaultHistogram()
-	notificationsQueueSize = prometheus.NewGauge()
-)
-
-func recordOutcome(duration time.Duration, err error) {
-	labels := map[string]string{result: success}
-	if err != nil {
-		labels[result] = failure
-	}
-
-	notificationsCount.Increment(labels)
-	ms := float64(duration / time.Millisecond)
-	notificationLatency.Add(labels, ms)
-}
-
-func init() {
-	prometheus.Register("prometheus_notifications_total", "Total number of processed alert notifications.", prometheus.NilLabels, notificationsCount)
-	prometheus.Register("prometheus_notifications_latency_ms", "Latency quantiles for sending alert notifications in milliseconds.", prometheus.NilLabels, notificationLatency)
-	prometheus.Register("prometheus_notifications_queue_size_total", "The size and capacity of the alert notification queue.", prometheus.NilLabels, notificationsQueueSize)
-}

notification/notification.go
@@ -25,6 +25,7 @@ import (
 	"github.com/golang/glog"
 
 	clientmodel "github.com/prometheus/client_golang/model"
+	"github.com/prometheus/client_golang/prometheus"
 
 	"github.com/prometheus/prometheus/utility"
 )
@@ -32,6 +33,18 @@ import (
 const (
 	alertmanagerApiEventsPath = "/api/alerts"
 	contentTypeJson           = "application/json"
+)
+
+// String constants for instrumentation.
+const (
+	result  = "result"
+	success = "success"
+	failure = "failure"
+	dropped = "dropped"
+
+	facet     = "facet"
+	occupancy = "occupancy"
+	capacity  = "capacity"
 )
 
 var (
@@ -72,6 +85,9 @@ type NotificationHandler struct {
 	pendingNotifications <-chan NotificationReqs
 	// HTTP client with custom timeout settings.
 	httpClient httpPoster
+
+	notificationLatency    *prometheus.SummaryVec
+	notificationsQueueSize *prometheus.GaugeVec
 }
 
 // Construct a new NotificationHandler.
@@ -80,6 +96,21 @@ func NewNotificationHandler(alertmanagerUrl string, notificationReqs <-chan Noti
 		alertmanagerUrl:      alertmanagerUrl,
 		pendingNotifications: notificationReqs,
 		httpClient:           utility.NewDeadlineClient(*deadline),
+
+		notificationLatency: prometheus.NewSummaryVec(
+			prometheus.SummaryOpts{
+				Name: "prometheus_notifications_latency_ms",
+				Help: "Latency quantiles for sending alert notifications in milliseconds.",
+			},
+			[]string{result},
+		),
+		notificationsQueueSize: prometheus.NewGaugeVec(
+			prometheus.GaugeOpts{
+				Name: "prometheus_notifications_queue_size_total",
+				Help: "The size and capacity of the alert notification queue.",
+			},
+			[]string{facet},
+		),
 	}
 }
 
@@ -122,35 +153,40 @@ func (n *NotificationHandler) sendNotifications(reqs NotificationReqs) error {
 	return nil
 }
 
-// Report notification queue occupancy and capacity.
-func (n *NotificationHandler) reportQueues() {
-	notificationsQueueSize.Set(map[string]string{facet: occupancy}, float64(len(n.pendingNotifications)))
-	notificationsQueueSize.Set(map[string]string{facet: capacity}, float64(cap(n.pendingNotifications)))
-}
-
 // Continuously dispatch notifications.
 func (n *NotificationHandler) Run() {
-	queueReportTicker := time.NewTicker(time.Second)
-	go func() {
-		for _ = range queueReportTicker.C {
-			n.reportQueues()
-		}
-	}()
-	defer queueReportTicker.Stop()
-
 	for reqs := range n.pendingNotifications {
 		if n.alertmanagerUrl == "" {
 			glog.Warning("No alert manager configured, not dispatching notification")
-			notificationsCount.Increment(map[string]string{result: dropped})
+			n.notificationLatency.WithLabelValues(dropped).Observe(0)
 			continue
 		}
 
 		begin := time.Now()
 		err := n.sendNotifications(reqs)
-		recordOutcome(time.Since(begin), err)
+		labelValue := success
 
 		if err != nil {
 			glog.Error("Error sending notification: ", err)
+			labelValue = failure
 		}
+
+		n.notificationLatency.WithLabelValues(labelValue).Observe(
+			float64(time.Since(begin) / time.Millisecond),
+		)
 	}
+}
+
+// Describe implements prometheus.Collector.
+func (n *NotificationHandler) Describe(ch chan<- *prometheus.Desc) {
+	n.notificationLatency.Describe(ch)
+	n.notificationsQueueSize.Describe(ch)
+}
+
+// Collect implements prometheus.Collector.
+func (n *NotificationHandler) Collect(ch chan<- prometheus.Metric) {
+	n.notificationLatency.Collect(ch)
+	n.notificationsQueueSize.WithLabelValues(occupancy).Set(float64(len(n.pendingNotifications)))
+	n.notificationsQueueSize.WithLabelValues(capacity).Set(float64(cap(n.pendingNotifications)))
+	n.notificationsQueueSize.Collect(ch)
 }

retrieval/instrumentation.go
@@ -1,61 +1,0 @@
-// Copyright 2013 Prometheus Team
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-package retrieval
-
-import (
-	"github.com/prometheus/client_golang/prometheus"
-)
-
-const (
-	address     = "instance"
-	alive       = "alive"
-	failure     = "failure"
-	outcome     = "outcome"
-	state       = "state"
-	success     = "success"
-	unreachable = "unreachable"
-)
-
-var (
-	networkLatencyHistogram = &prometheus.HistogramSpecification{
-		Starts:                prometheus.LogarithmicSizedBucketsFor(0, 1000),
-		BucketBuilder:         prometheus.AccumulatingBucketBuilder(prometheus.EvictAndReplaceWith(10, prometheus.AverageReducer), 100),
-		ReportablePercentiles: []float64{0.01, 0.05, 0.5, 0.90, 0.99},
-	}
-
-	targetOperationLatencies = prometheus.NewHistogram(networkLatencyHistogram)
-
-	retrievalDurations = prometheus.NewHistogram(&prometheus.HistogramSpecification{
-		Starts:                prometheus.LogarithmicSizedBucketsFor(0, 10000),
-		BucketBuilder:         prometheus.AccumulatingBucketBuilder(prometheus.EvictAndReplaceWith(10, prometheus.AverageReducer), 100),
-		ReportablePercentiles: []float64{0.01, 0.05, 0.5, 0.90, 0.99}})
-
-	targetOperations  = prometheus.NewCounter()
-	dnsSDLookupsCount = prometheus.NewCounter()
-)
-
-func recordOutcome(err error) {
-	message := success
-	if err != nil {
-		message = failure
-	}
-	dnsSDLookupsCount.Increment(map[string]string{outcome: message})
-}
-
-func init() {
-	prometheus.Register("prometheus_target_operations_total", "The total numbers of operations of the various targets that are being monitored.", prometheus.NilLabels, targetOperations)
-	prometheus.Register("prometheus_target_operation_latency_ms", "The latencies for various target operations.", prometheus.NilLabels, targetOperationLatencies)
-	prometheus.Register("prometheus_targetpool_duration_ms", "The durations for each TargetPool to retrieve state from all included entities.", prometheus.NilLabels, retrievalDurations)
-	prometheus.Register("prometheus_dns_sd_lookups_total", "The number of DNS-SD lookup successes/failures per pool.", prometheus.NilLabels, dnsSDLookupsCount)
-}

retrieval/target.go
@@ -23,6 +23,7 @@ import (
 
 	"github.com/golang/glog"
 	"github.com/prometheus/client_golang/extraction"
+	"github.com/prometheus/client_golang/prometheus"
 
 	clientmodel "github.com/prometheus/client_golang/model"
 
@@ -33,9 +34,32 @@ const (
 	InstanceLabel clientmodel.LabelName = "instance"
 	// The metric name for the synthetic health variable.
 	ScrapeHealthMetricName clientmodel.LabelValue = "up"
+
+	// Constants for instrumentation.
+	address = "instance"
+	alive   = "alive"
+	failure = "failure"
+	outcome = "outcome"
+	state   = "state"
+	success = "success"
 )
 
-var localhostRepresentations = []string{"http://127.0.0.1", "http://localhost"}
+var (
+	localhostRepresentations = []string{"http://127.0.0.1", "http://localhost"}
+
+	targetOperationLatencies = prometheus.NewSummaryVec(
+		prometheus.SummaryOpts{
+			Name:       "prometheus_target_operation_latency_ms",
+			Help:       "The latencies for various target operations.",
+			Objectives: []float64{0.01, 0.05, 0.5, 0.90, 0.99},
+		},
+		[]string{address, outcome},
+	)
+)
+
+func init() {
+	prometheus.MustRegister(targetOperationLatencies)
+}
 
 // The state of the given Target.
 type TargetState int
@@ -205,13 +229,12 @@ const acceptHeader = `application/vnd.google.protobuf;proto=io.prometheus.client
 func (t *target) scrape(timestamp clientmodel.Timestamp, ingester extraction.Ingester) (err error) {
 	defer func(start time.Time) {
 		ms := float64(time.Since(start)) / float64(time.Millisecond)
-		labels := map[string]string{address: t.Address(), outcome: success}
+		labels := prometheus.Labels{address: t.Address(), outcome: success}
 		if err != nil {
 			labels[outcome] = failure
 		}
 
-		targetOperationLatencies.Add(labels, ms)
-		targetOperations.Increment(labels)
+		targetOperationLatencies.With(labels).Observe(ms)
 	}(time.Now())
 
 	req, err := http.NewRequest("GET", t.Address(), nil)

retrieval/target_provider.go
@@ -22,6 +22,7 @@ import (
 
 	"github.com/golang/glog"
 	"github.com/miekg/dns"
+	"github.com/prometheus/client_golang/prometheus"
 
 	clientmodel "github.com/prometheus/client_golang/model"
 
@@ -30,6 +31,20 @@ import (
 )
 
 const resolvConf = "/etc/resolv.conf"
+
+var (
+	dnsSDLookupsCount = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "prometheus_dns_sd_lookups_total",
+			Help: "The number of DNS-SD lookup successes/failures per pool.",
+		},
+		[]string{outcome},
+	)
+)
+
+func init() {
+	prometheus.MustRegister(dnsSDLookupsCount)
+}
 
 // TargetProvider encapsulates retrieving all targets for a job.
 type TargetProvider interface {
@@ -60,7 +75,13 @@ func NewSdTargetProvider(job config.JobConfig) *sdTargetProvider {
 
 func (p *sdTargetProvider) Targets() ([]Target, error) {
 	var err error
-	defer func() { recordOutcome(err) }()
+	defer func() {
+		message := success
+		if err != nil {
+			message = failure
+		}
+		dnsSDLookupsCount.WithLabelValues(message).Inc()
+	}()
 
 	if time.Since(p.lastRefresh) < p.refreshInterval {
 		return p.targets, nil

retrieval/targetpool.go
@@ -20,14 +20,30 @@ import (
 
 	"github.com/golang/glog"
 	"github.com/prometheus/client_golang/extraction"
+	"github.com/prometheus/client_golang/prometheus"
 )
 
 const (
-	intervalKey = "interval"
-
 	targetAddQueueSize     = 100
 	targetReplaceQueueSize = 1
+
+	intervalKey = "interval"
 )
+
+var (
+	retrievalDurations = prometheus.NewSummaryVec(
+		prometheus.SummaryOpts{
+			Name:       "prometheus_targetpool_duration_ms",
+			Help:       "The durations for each TargetPool to retrieve state from all included entities.",
+			Objectives: []float64{0.01, 0.05, 0.5, 0.90, 0.99},
+		},
+		[]string{intervalKey},
+	)
+)
+
+func init() {
+	prometheus.MustRegister(retrievalDurations)
+}
 
 type TargetPool struct {
 	sync.RWMutex
@@ -164,7 +180,7 @@ func (p *TargetPool) runIteration(ingester extraction.Ingester, interval time.Du
 	wait.Wait()
 
 	duration := float64(time.Since(begin) / time.Millisecond)
-	retrievalDurations.Add(map[string]string{intervalKey: interval.String()}, duration)
+	retrievalDurations.WithLabelValues(interval.String()).Observe(duration)
 }
 
 func (p *TargetPool) Targets() []Target {

commit 61659ea733ef2a5fa6e2e623b489fde72033d5a7

Fix console handler.
This was accidnetally broken in 2128d9d81196f92432c4016bccfeab3f7af3f06a.

Change-Id: I50ea1fdb8ae4d28ae4555410bee97e5037692aa5

web/web.go
@@ -65,7 +65,7 @@ func (w WebService) ServeForever() error {
 		"/alerts", w.AlertsHandler,
 	))
 	http.Handle("/consoles/", prometheus.InstrumentHandler(
-		"/consoles/", http.StripPrefix("/static/", http.FileServer(http.Dir("web/static"))),
+		"/consoles/", http.StripPrefix("/consoles/", w.ConsolesHandler),
 	))
 	http.Handle("/graph", prometheus.InstrumentHandler(
 		"/graph", http.HandlerFunc(graphHandler),