852566d0bd506d2c46d2d3b0c0fde5b8cb272740
Andres Suarez <zertosh@gmail.com>
2014-09-16 12:20:31 -0400

    Focus expression after selection from dropdown

--- web/static/js/graph.js
+++ web/static/js/graph.js
@@ -132,7 +132,7 @@ Prometheus.Graph.prototype.initialize = function() {
 
   self.insertMetric.change(function() {
     self.expr.selection("replace", {text: self.insertMetric.val(), mode: "before"});
-    self.insertMetric.focus(); // refocusing
+    self.expr.focus(); // refocusing
   });
 
   self.expr.focus(); // TODO: move to external Graph method.
