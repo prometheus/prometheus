dd470845ecdd1fed4638e8002c868acecfc4a6ff
Andres Suarez <zertosh@gmail.com>
2014-09-16 12:13:11 -0400

    Allow selecting metric from Insert Metric

--- web/static/js/graph.js
+++ web/static/js/graph.js
@@ -134,7 +134,6 @@ Prometheus.Graph.prototype.initialize = function() {
     self.expr.selection("replace", {text: self.insertMetric.val(), mode: "before"});
     self.insertMetric.focus(); // refocusing
   });
-  self.insertMetric.click(function() { self.expr.focus() });
 
   self.expr.focus(); // TODO: move to external Graph method.
 
