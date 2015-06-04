5b713911e3ece7252c981d7bf5eebb0bd7323585
Fabian Reinartz <fab.reinartz@gmail.com>
2015-06-04 18:24:04 +0200

    web/api: enable running API legacy and v1 in parallel

--- web/api/legacy/api_test.go
+++ web/api/legacy/api_test.go
@@ -11,7 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package api
+package legacy
 
 import (
 	"io/ioutil"
@@ -93,13 +93,13 @@ func TestQuery(t *testing.T) {
 	})
 	storage.WaitForIndexing()
 
-	api := MetricsService{
+	api := &API{
 		Now:         testNow,
 		Storage:     storage,
 		QueryEngine: promql.NewEngine(storage),
 	}
 	rtr := route.New()
-	api.RegisterHandler(rtr.WithPrefix("/api"))
+	api.Register(rtr.WithPrefix("/api"))
 
 	server := httptest.NewServer(rtr)
 	defer server.Close()
