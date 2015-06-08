535c002f79185b282da5fe0929338ee7c53865d0
Fabian Reinartz <fab.reinartz@gmail.com>
2015-06-08 21:08:06 +0200

    util/route: add WithParam function.

--- util/route/route.go
+++ util/route/route.go
@@ -27,6 +27,11 @@ func Param(ctx context.Context, p string) string {
 	return ctx.Value(param(p)).(string)
 }
 
+// WithParam returns a new context with param p set to v.
+func WithParam(ctx context.Context, p, v string) context.Context {
+	return context.WithValue(ctx, param(p), v)
+}
+
 // handle turns a Handle into httprouter.Handle
 func handle(h http.HandlerFunc) httprouter.Handle {
 	return func(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
