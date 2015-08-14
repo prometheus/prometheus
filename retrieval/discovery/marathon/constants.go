8fa9ec278be58bf1f9ae7a5128f85a248d9d6361
Fabian Reinartz <fab.reinartz@gmail.com>
2015-08-14 14:24:31 +0200

    Add application labels as meta labels
    Removes built-in conditional scraping based on application's
    'prometheus' label.

--- retrieval/discovery/marathon/constants.go
+++ retrieval/discovery/marathon/constants.go
@@ -1,16 +1,19 @@
 package marathon
 
 import (
-	"github.com/prometheus/client_golang/model"
+	clientmodel "github.com/prometheus/client_golang/model"
 )
 
 const (
+	// metaLabelPrefix is the meta prefix used for all meta labels in this discovery.
+	metaLabelPrefix = clientmodel.MetaLabelPrefix + "marathon_"
+	// appLabelPrefix is the prefix for the application labels.
+	appLabelPrefix = metaLabelPrefix + "app_label_"
+
 	// AppLabel is used for the name of the app in Marathon.
-	AppLabel model.LabelName = "__meta_marathon_app"
-
+	appLabel clientmodel.LabelName = metaLabelPrefix + "app"
 	// ImageLabel is the label that is used for the docker image running the service.
-	ImageLabel model.LabelName = "__meta_marathon_image"
-
+	imageLabel clientmodel.LabelName = metaLabelPrefix + "image"
 	// TaskLabel contains the mesos task name of the app instance.
-	TaskLabel model.LabelName = "__meta_marathon_task"
+	taskLabel clientmodel.LabelName = metaLabelPrefix + "task"
 )
