6d42878ee37501fb62f52e650636b53a84b6fd2e
Julius Volz <julius@soundcloud.com>
2015-09-16 20:35:23 +0200

    Make -web.external-url help string more verbose.

--- cmd/prometheus/config.go
+++ cmd/prometheus/config.go
@@ -76,7 +76,7 @@ func init() {
 	)
 	cfg.fs.StringVar(
 		&cfg.prometheusURL, "web.external-url", "",
-		"The URL under which Prometheus is externally reachable (for example, if Prometheus is served via a reverse proxy). Used for generating relative and absolute links back to Prometheus itself. If omitted, relevant URL components will be derived automatically.",
+		"The URL under which Prometheus is externally reachable (for example, if Prometheus is served via a reverse proxy). Used for generating relative and absolute links back to Prometheus itself. If the URL has a path portion, it will be used to prefix all HTTP endpoints served by Prometheus. If omitted, relevant URL components will be derived automatically.",
 	)
 	cfg.fs.StringVar(
 		&cfg.web.MetricsPath, "web.telemetry-path", "/metrics",
