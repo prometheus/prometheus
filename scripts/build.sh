b39a2cee475a501cf4202b1258763d05f3faf9c0
Fabian Reinartz <fab.reinartz@gmail.com>
2015-09-18 10:38:26 +0200

    Fix missing go version in build tags

--- scripts/build.sh
+++ scripts/build.sh
@@ -22,6 +22,7 @@ revision=$( git rev-parse --short HEAD 2> /dev/null || echo 'unknown' )
 branch=$( git rev-parse --abbrev-ref HEAD 2> /dev/null || echo 'unknown' )
 host=$( hostname -f )
 build_date=$( date +%Y%m%d-%H:%M:%S )
+go_version=$( go version | sed -e 's/^[^0-9.]*\([0-9.]*\).*/\1/' )
 
 if [ "$(go env GOOS)" = "windows" ]; then
 	ext=".exe"
