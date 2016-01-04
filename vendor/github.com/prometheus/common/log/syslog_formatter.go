cadca0aa237b4559c6f03f8523f4a5ed4a40e0ee
Fabian Reinartz <fab.reinartz@gmail.com>
2016-01-04 14:48:35 +0100

    Upgrade common/log vendoring.
    Fixes #1186

--- vendor/github.com/prometheus/common/log/syslog_formatter.go
+++ vendor/github.com/prometheus/common/log/syslog_formatter.go
@@ -1,3 +1,18 @@
+// Copyright 2015 The Prometheus Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// +build !windows,!nacl,!plan9
+
 package log
 
 import (
@@ -7,6 +22,26 @@ import (
 
 	"github.com/Sirupsen/logrus"
 )
+
+func init() {
+	setSyslogFormatter = func(appname, local string) error {
+		if appname == "" {
+			return fmt.Errorf("missing appname parameter")
+		}
+		if local == "" {
+			return fmt.Errorf("missing local parameter")
+		}
+
+		fmter, err := newSyslogger(appname, local, origLogger.Formatter)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "error creating syslog formatter: %v\n", err)
+			origLogger.Errorf("can't connect logger to syslog: %v", err)
+			return err
+		}
+		origLogger.Formatter = fmter
+		return nil
+	}
+}
 
 var ceeTag = []byte("@cee:")
 
