e18483b81dd1c79d7e0399ec0c1ad632e508d8cf
eliothedeman <eliot.d.hedeman@gmail.com>
2016-03-07 15:50:49 -0500

    don't use smoothie to do the smoothing

--- promql/functions_test.go
+++ promql/functions_test.go
@@ -2,12 +2,12 @@ package promql
 
 import "testing"
 
-func BenchmarkHoltWinters4Week5Min(b *testing.B) {
+func BenchmarkDoubleSmooth4Week5Min(b *testing.B) {
 	input := `
 load 5m
     http_requests{path="/foo"}    0+10x8064
 
-eval instant at 4w holt_winters(http_requests[4w], 0.3, 0.3)
+eval instant at 4w double_smooth(http_requests[4w], 0.3, 0.3)
     {path="/foo"} 0
 `
 
@@ -16,12 +16,12 @@ eval instant at 4w holt_winters(http_requests[4w], 0.3, 0.3)
 
 }
 
-func BenchmarkHoltWinters1Week5Min(b *testing.B) {
+func BenchmarkDoubleSmooth1Week5Min(b *testing.B) {
 	input := `
 load 5m
     http_requests{path="/foo"}    0+10x2016
 
-eval instant at 1w holt_winters(http_requests[1w], 0.3, 0.3)
+eval instant at 1w double_smooth(http_requests[1w], 0.3, 0.3)
     {path="/foo"} 0
 `
 
@@ -29,13 +29,13 @@ eval instant at 1w holt_winters(http_requests[1w], 0.3, 0.3)
 	bench.Run()
 }
 
-func BenchmarkHoltWinters1Day1Min(b *testing.B) {
+func BenchmarkDoubleSmooth1Day1Min(b *testing.B) {
 	input := `
 
 load 1m
     http_requests{path="/foo"}    0+10x1440
 
-eval instant at 1d holt_winters(http_requests[1d], 0.3, 0.3)
+eval instant at 1d double_smooth(http_requests[1d], 0.3, 0.3)
     {path="/foo"} 0
 `
 
