bf191085c483f8c33be5fcdeb48abd28ac963baf
Tom Wilkie <tom.wilkie@gmail.com>
2016-06-22 17:30:53 +0100

    Make it compile, add a main function, plumb through append.

--- frankenstein/distributor.go
+++ frankenstein/distributor.go
@@ -3,21 +3,25 @@ package frankenstein
 import (
 	"sync"
 
+	"github.com/prometheus/prometheus/storage"
+
 	"github.com/prometheus/common/model"
 )
 
 type distributor struct {
-	consul ConsulClient
-	ring   *Ring
-	cfg    distributorConfig
+	consul     ConsulClient
+	ring       *Ring
+	cfg        DistributorConfig
+	clientsMtx sync.RWMutex
+	clients    map[string]storage.SampleAppender
 
 	quit chan struct{}
 	done sync.WaitGroup
 }
 
-type distributorConfig struct {
-	consulHost   string
-	consulPrefix string
+type DistributorConfig struct {
+	ConsulHost   string
+	ConsulPrefix string
 }
 
 type collector struct {
@@ -25,8 +29,8 @@ type collector struct {
 	tokens   []uint64
 }
 
-func NewDistributor(cfg distributorConfig) (*distributor, error) {
-	consul, err := NewConsulClient(cfg.consulHost)
+func NewDistributor(cfg DistributorConfig) (*distributor, error) {
+	consul, err := NewConsulClient(cfg.ConsulHost)
 	if err != nil {
 		return nil, err
 	}
@@ -48,16 +52,48 @@ func (d *distributor) Stop() {
 
 func (d *distributor) loop() {
 	defer d.done.Done()
-	d.consul.WatchPrefix(d.cfg.consulPrefix, &collector{}, d.quit, func(key string, value interface{}) bool {
+	d.consul.WatchPrefix(d.cfg.ConsulPrefix, &collector{}, d.quit, func(key string, value interface{}) bool {
 		c := *value.(*collector)
 		d.ring.Update(c)
 		return true
 	})
 }
 
+func (d *distributor) getClientFor(hostname string) storage.SampleAppender {
+	d.clientsMtx.RLock()
+	client, ok := d.clients[hostname]
+	d.clientsMtx.RUnlock()
+	if ok {
+		return client
+	}
+
+	d.clientsMtx.Lock()
+	defer d.clientsMtx.Unlock()
+	client, ok = d.clients[hostname]
+	if ok {
+		return client
+	}
+
+	client = nil // TODO, when we have a client/server for SampleAppender
+	d.clients[hostname] = client
+	return client
+}
+
 // Append implements storage.SampleAppender
-func (d *distributor) Append(*model.Sample) error {
-	return nil
+func (d *distributor) Append(sample *model.Sample) error {
+	for ln, lv := range sample.Metric {
+		if len(lv) == 0 {
+			delete(sample.Metric, ln)
+		}
+	}
+	key := sample.Metric.Fingerprint()
+	collector, err := d.ring.Get(uint64(key))
+	if err != nil {
+		return err
+	}
+
+	client := d.getClientFor(collector.hostname)
+	return client.Append(sample)
 }
 
 // NeedsThrottling implements storage.SampleAppender
--- frankenstein/prog/main.go
+++ frankenstein/prog/main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"flag"
 
+	"github.com/prometheus/common/log"
+	"github.com/prometheus/prometheus/frankenstein"
+)
+
+func main() {
+	var (
+		listen       string
+		consulHost   string
+		consulPrefix string
+	)
+
+	flag.StringVar(&listen, "listen", ":80", "HTTP server listen address")
+	flag.StringVar(&consulHost, "consul.hostname", "consul:8500", "Hostname & port of consul")
+	flag.StringVar(&consulPrefix, "consul.prefix", "collectors/", "Prefix for keys in consul")
+
+	_, err := frankenstein.NewDistributor(frankenstein.DistributorConfig{
+		ConsulHost:   consulHost,
+		ConsulPrefix: consulPrefix,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
 }
