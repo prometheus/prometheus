35177f6f3071e2c6cf97a5702b269619d4b24dc9
Julius Volz <julius@soundcloud.com>
2016-07-05 17:20:13 +0200

    POC server for PromQL->MergeQuerier->Ingester query path.

--- frankenstein/cmd/frankenstein/main.go
+++ frankenstein/cmd/frankenstein/main.go
@@ -19,9 +19,12 @@ import (
 	"time"
 
 	"github.com/prometheus/common/log"
+	"github.com/prometheus/common/route"
 
 	"github.com/prometheus/prometheus/frankenstein"
+	"github.com/prometheus/prometheus/promql"
 	"github.com/prometheus/prometheus/storage/remote"
+	"github.com/prometheus/prometheus/web/api/v1"
 )
 
 func main() {
@@ -64,6 +67,25 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	// TODO: REMOVE - this is just a proof-of-concept for querying back data
+	// from a local Prometheus server via
+	// PromQL->MergeQuerier->IngesterQuerier. The distributor is not in
+	// the picture yet.
+	ingesterQuerier, err := frankenstein.NewIngesterQuerier("http://localhost:9090/")
+	if err != nil {
+		log.Fatal(err)
+	}
+	querier := frankenstein.MergeQuerier{
+		Queriers: []frankenstein.Querier{ingesterQuerier},
+	}
+	engine := promql.NewEngine(nil, querier, nil)
+
+	api := v1.NewAPI(engine, nil)
+	router := route.New()
+	api.Register(router.WithPrefix("/api/v1"))
+	http.Handle("/", router)
+	// END OF SECTION TO REMOVE
 
 	http.Handle("/push", frankenstein.AppenderHandler(distributor))
 	http.ListenAndServe(listen, nil)
