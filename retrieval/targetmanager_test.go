79216011cb72be07ab760ede71bf96c2bc3b4bd1
Dan Milstein <milstein.dan@gmail.com>
2016-08-25 14:36:26 -0400

    Add basic test for TargetManager.targetSet
    Verify that if the configs change, target groups are cleaned on
    TargetManager.reload (rather than having old ones linger around, even if
    they are no longer present in the configs).
    
    This covers the bug fixed in #1907 -- I verified that by checking out
    source from before that commit.
    
    This is a start on #1906

--- retrieval/targetmanager_test.go
+++ retrieval/targetmanager_test.go
@@ -12,3 +12,63 @@
 // limitations under the License.
 
 package retrieval
+
+import (
+	"testing"
+
+	"golang.org/x/net/context"
+	"gopkg.in/yaml.v2"
+
+	"github.com/prometheus/prometheus/config"
+	"github.com/prometheus/prometheus/storage/local"
+)
+
+func TestTargetSetRecreatesTargetGroupsEveryRun(t *testing.T) {
+	scrapeConfig := &config.ScrapeConfig{}
+
+	sOne := `
+job_name: "foo"
+dns_sd_configs:
+- names:
+  - "srv.name.one.example.org"
+`
+	if err := yaml.Unmarshal([]byte(sOne), scrapeConfig); err != nil {
+		t.Fatalf("Unable to load YAML config sOne: %s", err)
+	}
+
+	// Not properly setting it up, but that seems okay
+	mss := &local.MemorySeriesStorage{}
+
+	ts := newTargetSet(scrapeConfig, mss)
+
+	ts.runProviders(context.Background(), providersFromConfig(scrapeConfig))
+
+	verifyPresence(t, ts.tgroups, "dns/0/srv.name.one.example.org", true)
+
+	sTwo := `
+job_name: "foo"
+dns_sd_configs:
+- names:
+  - "srv.name.two.example.org"
+`
+	if err := yaml.Unmarshal([]byte(sTwo), scrapeConfig); err != nil {
+		t.Fatalf("Unable to load YAML config sTwo: %s", err)
+	}
+
+	ts.runProviders(context.Background(), providersFromConfig(scrapeConfig))
+
+	verifyPresence(t, ts.tgroups, "dns/0/srv.name.one.example.org", false)
+	verifyPresence(t, ts.tgroups, "dns/0/srv.name.two.example.org", true)
+
+}
+
+func verifyPresence(t *testing.T, tgroups map[string][]*Target, name string, present bool) {
+	if _, ok := tgroups[name]; ok != present {
+		msg := ""
+		if !present {
+			msg = "not "
+		}
+		t.Fatalf("'%s' should %sbe present in TargetSet.tgroups: %s", name, msg, tgroups)
+	}
+
+}
