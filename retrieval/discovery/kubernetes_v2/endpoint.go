7c439a90608d8347cccd0d1f6738486b9ec72fe8
Fabian Reinartz <fab.reinartz@gmail.com>
2016-09-30 11:33:23 +0200

    kubernetes: use and vendor 1.5 client

--- retrieval/discovery/kubernetes_v2/endpoint.go
+++ retrieval/discovery/kubernetes_v2/endpoint.go
@@ -1,3 +1,16 @@
+// Copyright 2016 The Prometheus Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package kubernetesv2
 
 import (
@@ -10,8 +23,8 @@ import (
 	"github.com/prometheus/common/log"
 	"github.com/prometheus/common/model"
 	"golang.org/x/net/context"
-	apiv1 "k8s.io/kubernetes/pkg/api/v1"
-	"k8s.io/kubernetes/pkg/client/cache"
+	apiv1 "k8s.io/client-go/1.5/pkg/api/v1"
+	"k8s.io/client-go/1.5/tools/cache"
 )
 
 // Endpoints discovers new endpoint targets.
@@ -126,8 +139,8 @@ func (e *Endpoints) buildEndpoints(eps *apiv1.Endpoints) *config.TargetGroup {
 	e.decorateService(eps.Namespace, eps.Name, tg)
 
 	// type podEntry struct {
-	// 	namespace string
-	// 	name      string
+	// 	pod          *apiv1.Pod
+	// 	servicePorts []apiv1.ServicePort
 	// }
 	// seenPods := map[string]podEntry{}
 
--- retrieval/discovery/kubernetes_v2/pod.go
+++ retrieval/discovery/kubernetes_v2/pod.go
@@ -1,3 +1,16 @@
+// Copyright 2016 The Prometheus Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package kubernetesv2
 
 import (
@@ -10,9 +23,9 @@ import (
 	"github.com/prometheus/common/model"
 	"github.com/prometheus/prometheus/config"
 	"golang.org/x/net/context"
-	"k8s.io/kubernetes/pkg/api"
-	apiv1 "k8s.io/kubernetes/pkg/api/v1"
-	"k8s.io/kubernetes/pkg/client/cache"
+	"k8s.io/client-go/1.5/pkg/api"
+	apiv1 "k8s.io/client-go/1.5/pkg/api/v1"
+	"k8s.io/client-go/1.5/tools/cache"
 )
 
 // Pods discovers new pod targets.
