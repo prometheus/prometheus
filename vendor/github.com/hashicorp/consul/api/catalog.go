43cddad30b1d7d16299830e96442e305502a2c8a
beorn7 <beorn@soundcloud.com>
2016-10-28 15:27:00 +0200

    vendor: Update consul client
    We were so much behind...

--- vendor/github.com/hashicorp/consul/api/catalog.go
+++ vendor/github.com/hashicorp/consul/api/catalog.go
@@ -1,18 +1,21 @@
 package api
 
 type Node struct {
-	Node    string
-	Address string
+	Node            string
+	Address         string
+	TaggedAddresses map[string]string
 }
 
 type CatalogService struct {
-	Node           string
-	Address        string
-	ServiceID      string
-	ServiceName    string
-	ServiceAddress string
-	ServiceTags    []string
-	ServicePort    int
+	Node                     string
+	Address                  string
+	TaggedAddresses          map[string]string
+	ServiceID                string
+	ServiceName              string
+	ServiceAddress           string
+	ServiceTags              []string
+	ServicePort              int
+	ServiceEnableTagOverride bool
 }
 
 type CatalogNode struct {
@@ -21,11 +24,12 @@ type CatalogNode struct {
 }
 
 type CatalogRegistration struct {
-	Node       string
-	Address    string
-	Datacenter string
-	Service    *AgentService
-	Check      *AgentCheck
+	Node            string
+	Address         string
+	TaggedAddresses map[string]string
+	Datacenter      string
+	Service         *AgentService
+	Check           *AgentCheck
 }
 
 type CatalogDeregistration struct {
