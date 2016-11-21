d50454480530af6fa7d228d5deea36add3de5f3f
Fabian Reinartz <fab.reinartz@gmail.com>
2016-11-21 15:59:03 +0100

    discovery: move into top-level package

--- retrieval/targetmanager.go
+++ retrieval/targetmanager.go
@@ -26,8 +26,8 @@ import (
 	"golang.org/x/net/context"
 
 	"github.com/prometheus/prometheus/config"
+	"github.com/prometheus/prometheus/discovery"
 	"github.com/prometheus/prometheus/relabel"
-	"github.com/prometheus/prometheus/retrieval/discovery"
 	"github.com/prometheus/prometheus/storage"
 )
 
