1579e12011fbf6249c507f9acae6b610e2870a71
Fabian Reinartz <fab.reinartz@gmail.com>
2017-04-04 15:59:52 +0200

    Add cross-platform mmap

--- db_unix.go
+++ db_unix.go
@@ -4,24 +4,13 @@ package tsdb
 
 import (
 	"os"
-	"unsafe"
-
-	"golang.org/x/sys/unix"
+	"syscall"
 )
 
 func mmap(f *os.File, length int) ([]byte, error) {
-	return unix.Mmap(int(f.Fd()), 0, length, unix.PROT_READ, unix.MAP_SHARED)
+	return syscall.Mmap(int(f.Fd()), 0, length, syscall.PROT_READ, syscall.MAP_SHARED)
 }
 
 func munmap(b []byte) (err error) {
-	return unix.Munmap(b)
-}
-
-// unix.Madvise is not defined for darwin, so we define it ourselves.
-func madvise(b []byte, advice int) (err error) {
-	_, _, e1 := unix.Syscall(unix.SYS_MADVISE, uintptr(unsafe.Pointer(&b[0])), uintptr(len(b)), uintptr(advice))
-	if e1 != 0 {
-		err = e1
-	}
-	return
+	return syscall.Munmap(b)
 }
