47b0b9f7b0aeb9e1b566f4eddc877528b2a38a68
Fabian Reinartz <fab.reinartz@gmail.com>
2017-04-04 16:04:29 +0200

    vendor: add tsdb support for windows

---
+++ vendor/github.com/prometheus/tsdb/db_windows.go
@@ -0,0 +1,34 @@
+package tsdb
+
+import (
+	"os"
+	"syscall"
+	"unsafe"
+)
+
+func mmap(f *os.File, sz int) ([]byte, error) {
+	low, high := uint32(size), uint32(size>>32)
+
+	h, errno := syscall.CreateFileMapping(syscall.Handle(f.Fd()), nil, syscall.PAGE_READONLY, low, high, nil)
+	if h == 0 {
+		return os.NewSyscallError("CreateFileMapping", errno)
+	}
+
+	addr, errno := syscall.MapViewOfFile(h, syscall.FILE_MAP_READ, 0, 0, uintptr(sz))
+	if addr == 0 {
+		return os.NewSyscallError("MapViewOfFile", errno)
+	}
+
+	if err := syscall.CloseHandle(syscall.Handle(h)); err != nil {
+		return os.NewSyscallError("CloseHandle", err)
+	}
+
+	return (*[1 << 30]byte)(unsafe.Pointer(addr))[:sz], nil
+}
+
+func munmap(b []byte) error {
+	if err := syscall.UnmapViewOfFile((uintptr)(unsafe.Pointer(&b[0]))); err != nil {
+		return os.NewSyscallError("UnmapViewOfFile", err)
+	}
+	return nil
+}
