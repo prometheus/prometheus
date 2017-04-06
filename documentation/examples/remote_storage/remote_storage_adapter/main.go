9896c8f6b0e28e2482358e5ac5d5fb73eb975cc2
Brian Brazil <brian.brazil@robustperception.io>
2017-04-06 16:56:01 +0100

    Correct example name to adapter.

--- documentation/examples/remote_storage/remote_storage_adapter/main.go
+++ documentation/examples/remote_storage/remote_storage_adapter/main.go
@@ -33,9 +33,9 @@ import (
 
 	influx "github.com/influxdata/influxdb/client/v2"
 
-	"github.com/prometheus/prometheus/documentation/examples/remote_storage/remote_storage_bridge/graphite"
-	"github.com/prometheus/prometheus/documentation/examples/remote_storage/remote_storage_bridge/influxdb"
-	"github.com/prometheus/prometheus/documentation/examples/remote_storage/remote_storage_bridge/opentsdb"
+	"github.com/prometheus/prometheus/documentation/examples/remote_storage/remote_storage_adapter/graphite"
+	"github.com/prometheus/prometheus/documentation/examples/remote_storage/remote_storage_adapter/influxdb"
+	"github.com/prometheus/prometheus/documentation/examples/remote_storage/remote_storage_adapter/opentsdb"
 	"github.com/prometheus/prometheus/storage/remote"
 )
 
