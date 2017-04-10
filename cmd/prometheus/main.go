757cba7c31db95ad71d4de5e878017aa9f59f394
Fabian Reinartz <fab.reinartz@gmail.com>
2017-04-10 16:22:01 +0200

    cmd/prometheus: Undo GOGC adjustment

--- cmd/prometheus/main.go
+++ cmd/prometheus/main.go
@@ -20,7 +20,6 @@ import (
 	_ "net/http/pprof" // Comment this line to disable pprof endpoint.
 	"os"
 	"os/signal"
-	"runtime/debug"
 	"syscall"
 	"time"
 
@@ -41,13 +40,6 @@ import (
 func main() {
 	os.Exit(Main())
 }
-
-// defaultGCPercent is the value used to to call SetGCPercent if the GOGC
-// environment variable is not set or empty. The value here is intended to hit
-// the sweet spot between memory utilization and GC effort. It is lower than the
-// usual default of 100 as a lot of the heap in Prometheus is used to cache
-// memory chunks, which have a lifetime of hours if not days or weeks.
-const defaultGCPercent = 40
 
 var (
 	configSuccess = prometheus.NewGauge(prometheus.GaugeOpts{
@@ -76,10 +68,6 @@ func Main() int {
 	if cfg.printVersion {
 		fmt.Fprintln(os.Stdout, version.Print("prometheus"))
 		return 0
-	}
-
-	if os.Getenv("GOGC") == "" {
-		debug.SetGCPercent(defaultGCPercent)
 	}
 
 	log.Infoln("Starting prometheus", version.Info())
