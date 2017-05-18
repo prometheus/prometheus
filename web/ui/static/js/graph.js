9287a01bbf8c1c1a42eed6b2f80f22d6bc290824
conorbroderick <cjayjayb@gmail.com>
2017-05-18 15:03:58 +0100

    Fixed fixed yaxis of stacked graph being cut off

--- web/ui/static/js/graph.js
+++ web/ui/static/js/graph.js
@@ -557,24 +557,40 @@ Prometheus.Graph.prototype.updateGraph = function() {
   });
 
   // Find and set graph's max/min
-  var min = Infinity;
-  var max = -Infinity;
-  self.data.forEach(function(timeSeries) {
-    timeSeries.data.forEach(function(dataPoint) {
+  if (self.isStacked() === true) {
+    // When stacked is toggled
+    var max = 0;
+    self.data.forEach(function(timeSeries) {
+      var currSeriesMax = 0;
+      timeSeries.data.forEach(function(dataPoint) {
+        if (dataPoint.y > currSeriesMax && dataPoint.y != null) {
+          currSeriesMax = dataPoint.y;
+        }
+      });
+      max += currSeriesMax;
+    });
+    self.rickshawGraph.max = max*1.05;
+    self.rickshawGraph.min = 0;
+  } else {
+    var min = Infinity;
+    var max = -Infinity;
+    self.data.forEach(function(timeSeries) {
+      timeSeries.data.forEach(function(dataPoint) {
         if (dataPoint.y < min && dataPoint.y != null) {
           min = dataPoint.y;
         }
         if (dataPoint.y > max && dataPoint.y != null) {
           max = dataPoint.y;
         }
+      });
     });
-  });
-  if (min === max) {
-    self.rickshawGraph.max = max + 1;
-    self.rickshawGraph.min = min - 1;
-  } else {
-    self.rickshawGraph.max = max + (0.1*(Math.abs(max - min)));
-    self.rickshawGraph.min = min - (0.1*(Math.abs(max - min)));
+    if (min === max) {
+      self.rickshawGraph.max = max + 1;
+      self.rickshawGraph.min = min - 1;
+    } else {
+      self.rickshawGraph.max = max + (0.1*(Math.abs(max - min)));
+      self.rickshawGraph.min = min - (0.1*(Math.abs(max - min)));
+    }
   }
 
   var xAxis = new Rickshaw.Graph.Axis.Time({ graph: self.rickshawGraph });
