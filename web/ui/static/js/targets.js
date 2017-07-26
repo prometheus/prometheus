c30baa95092d1c753dbe2b4c59045da8632622dd
r.khavronenko <r.khavronenko@vertamedia.com>
2017-07-26 15:09:16 +0300

    trying to make localstorage safe

--- web/ui/static/js/targets.js
+++ web/ui/static/js/targets.js
@@ -1,15 +1,18 @@
 function init() {
-  var ls = localStorage;
   $(".job_header").click(function() {
     var selector = $(this).attr("data-target");
-    ls.setItem(selector, !$(selector).hasClass("in"));
+    try {
+        window.localStorage.setItem(selector, !$(selector).hasClass("in"));
+    } finally {}
   });
 
   $(".job_header").each(function(i, obj) {
     var selector = $(obj).attr("data-target");
-    if (ls.getItem(selector) === "true" || ls.getItem(selector) === null) {
-      $(selector).addClass("in");
-    }
+    try {
+        if (window.localStorage.getItem(selector) === "true" || window.localStorage.getItem(selector) === null) {
+            $(selector).addClass("in");
+        }
+    } finally {}
   });
 }
 
