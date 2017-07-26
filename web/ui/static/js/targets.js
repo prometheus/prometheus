202d0cec86fc024482a5fc9b6e3284365539a2b1
r.khavronenko <r.khavronenko@vertamedia.com>
2017-07-26 13:55:19 +0300

    update targets.js due to codacy issues

--- web/ui/static/js/targets.js
+++ web/ui/static/js/targets.js
@@ -1,15 +1,16 @@
 function init() {
-  $('.job_header').click(function() {
-    var selector = $(this).attr('data-target');
-    localStorage.setItem(selector, !$(selector).hasClass('in'));
+  var ls = localStorage;
+  $(".job_header").click(function() {
+    var selector = $(this).attr("data-target");
+    ls.setItem(selector, !$(selector).hasClass("in"));
   });
 
-  $('.job_header').each(function(i, obj) {
-    var selector = $(obj).attr('data-target');
-    if (localStorage.getItem(selector) === "true" || localStorage.getItem(selector) === null) {
-      $(selector).addClass('in');
+  $(".job_header").each(function(i, obj) {
+    var selector = $(obj).attr("data-target");
+    if (ls.getItem(selector) === "true" || ls.getItem(selector) === null) {
+      $(selector).addClass("in");
     }
-  })
+  });
 }
 
 $(init);
