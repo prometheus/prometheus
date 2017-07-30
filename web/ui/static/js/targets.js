baa36369f162952c9e3bda61b7665e4e0c971632
hagen1778 <hagen1778@gmail.com>
2017-07-30 18:21:33 +0300

    codacy fixes

--- web/ui/static/js/targets.js
+++ web/ui/static/js/targets.js
@@ -1,3 +1,18 @@
+function toggle(obj, state){
+    var icon = $(obj).find("i");
+    if (icon.length === 0 ) {
+        return;
+    }
+
+    if (state === true) {
+        icon.removeClass("icon-chevron-down").addClass("icon-chevron-up");
+    } else {
+        icon.removeClass("icon-chevron-up").addClass("icon-chevron-down");
+    }
+
+    $(obj).next().toggle(state);
+}
+
 function init() {
     $(".job_header").click(function() {
         var job = $(this).find("a").attr("id"),
@@ -12,27 +27,12 @@ function init() {
         }
     });
 
-    $('.job_header a').each(function(i, obj) {
-        var selector = $(obj).attr('id');
+    $(".job_header a").each(function(i, obj) {
+        var selector = $(obj).attr("id");
         if (localStorage.getItem(selector) === "true") {
           toggle($(this).parents(".job_header"), false);
         }
-    })
+    });
 }
 
 $(init);
-
-function toggle(obj, state){
-    var icon = $(obj).find("i");
-    if (icon.length === 0 ) {
-        return
-    }
-
-    if (state === true) {
-        icon.removeClass("icon-chevron-down").addClass("icon-chevron-up");
-    } else {
-        icon.removeClass("icon-chevron-up").addClass("icon-chevron-down");
-    }
-
-    $(obj).next().toggle(state);
-}
