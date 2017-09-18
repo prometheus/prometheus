b2ca81d57eafddc0ff2cbbcf6f00a85d6f81a536
Hylke Visser <htdvisser@gmail.com>
2017-09-18 14:53:45 +0200

    Add comment to explain the reason for PR #3174

--- web/web.go
+++ web/web.go
@@ -398,6 +398,10 @@ func (h *Handler) Run(ctx context.Context) error {
 	}
 	ip := net.ParseIP(host)
 	if ip.Equal(net.IPv4zero) || ip.Equal(net.IPv6zero) {
+		// The gRPC server is listening on an empty/wildcard address (0.0.0.0 or ::).
+		// We should be able to connect to an empty IP just fine, but as there were
+		// some issues with this in the past (#3004, #3149), we explicitly tell
+		// the REST API gateway to connect to localhost.
 		lAddr = net.JoinHostPort("localhost", port)
 	}
 
