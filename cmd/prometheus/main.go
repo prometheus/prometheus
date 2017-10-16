6083200cc09b3f01a2653512df284cb29a9c105e
Julius Volz <julius.volz@gmail.com>
2017-10-16 15:00:05 -0700

    Fix typo in flag description (#3302)

--- cmd/prometheus/main.go
+++ cmd/prometheus/main.go
@@ -107,7 +107,7 @@ func main() {
 	a.Flag("config.file", "Prometheus configuration file path.").
 		Default("prometheus.yml").StringVar(&cfg.configFile)
 
-	a.Flag("web.listen-address", "Address to listen on for UI, API, and telemtry.").
+	a.Flag("web.listen-address", "Address to listen on for UI, API, and telemetry.").
 		Default("0.0.0.0:9090").StringVar(&cfg.web.ListenAddress)
 
 	a.Flag("web.read-timeout",
