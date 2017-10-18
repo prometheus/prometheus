edd0958407ba973648f8f83b923d0e2174983a9d
Alexander Kazarin <boiler@xdot.ru>
2017-10-18 18:51:06 +0300

    fix for ZookeeperLogger (issue 2976)

--- util/treecache/treecache.go
+++ util/treecache/treecache.go
@@ -49,6 +49,10 @@ type ZookeeperLogger struct {
 	logger log.Logger
 }
 
+func NewZookeeperLogger(logger log.Logger) ZookeeperLogger {
+	return ZookeeperLogger{logger: logger}
+}
+
 // Implements zk.Logger
 func (zl ZookeeperLogger) Printf(s string, i ...interface{}) {
 	level.Info(zl.logger).Log("msg", fmt.Sprintf(s, i...))
