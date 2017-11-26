2c2702e22bebdb86061d005853cf22bcdf425ddb
Lovisa Svallingson <applegrain@users.noreply.github.com>
2017-11-25 21:49:24 -0700

    Align columns in Targets view (#3445)
    - rebase and resolve merge conflicts

--- web/ui/static/js/targets.js
+++ web/ui/static/js/targets.js
@@ -1,38 +1,62 @@
-function toggle(obj, state){
-    var icon = $(obj).find("i");
-    if (icon.length === 0 ) {
-        return;
-    }
+function toggleJobTable(button, shouldExpand){
+  if (button.length === 0) { return; }
 
-    if (state === true) {
-        icon.removeClass("icon-chevron-down").addClass("icon-chevron-up");
-    } else {
-        icon.removeClass("icon-chevron-up").addClass("icon-chevron-down");
-    }
+  if (shouldExpand) {
+    button.removeClass("collapsed-table").addClass("expanded-table").html("show less");
+  } else {
+    button.removeClass("expanded-table").addClass("collapsed-table").html("show more");
+  }
 
-    $(obj).next().toggle(state);
+  button.parents(".table-container").find("table").toggle(shouldExpand);
 }
 
 function init() {
-    $(".job_header").click(function() {
-        var job = $(this).find("a").attr("id"),
-            expanderIcon = $(this).find("i.icon-chevron-down");
+  $("button.targets").click(function () {
+    const tableTitle = $(this).closest("h2").find("a").attr("id");
 
-        if (expanderIcon.length !== 0) {
-            localStorage.setItem(job, false);
-            toggle(this, true);
-        } else {
-            localStorage.setItem(job, true);
-            toggle(this, false);
-        }
-    });
+    if ($(this).hasClass("collapsed-table")) {
+      localStorage.setItem(tableTitle, "expanded");
+      toggleJobTable($(this), true);
+    } else if ($(this).hasClass("expanded-table")) {
+      localStorage.setItem(tableTitle, "collapsed");
+      toggleJobTable($(this), false);
+    }
+  });
 
-    $(".job_header a").each(function(i, obj) {
-        var selector = $(obj).attr("id");
-        if (localStorage.getItem(selector) === "true") {
-          toggle($(this).parents(".job_header"), false);
-        }
-    });
+  $(".job_header a").each(function (_, link) {
+    const cachedTableState = localStorage.getItem($(link).attr("id"));
+    if (cachedTableState === "collapsed") {
+      toggleJobTable($(this).siblings("button"), false);
+    }
+  });
+
+  $(".filters button.unhealthy-targets").click(function(e) {
+    const button = $(e.target);
+    const icon = $(e.target).children("i");
+
+    if (icon.hasClass("glyphicon-unchecked")) {
+      icon.removeClass("glyphicon-unchecked")
+          .addClass("glyphicon-check btn-primary");
+      button.addClass("is-checked");
+
+      $(".table-container").each(showUnhealthy);
+    } else if (icon.hasClass("glyphicon-check")) {
+      icon.removeClass("glyphicon-check btn-primary")
+          .addClass("glyphicon-unchecked");
+      button.removeClass("is-checked");
+
+      $(".table-container").each(showAll);
+    }
+  });
+}
+
+function showAll(_, container) {
+  $(container).show();
+}
+
+function showUnhealthy(_, container) {
+  const isHealthy = $(container).find("h2").attr("class").indexOf("danger") < 0;
+  if (isHealthy) { $(container).hide(); }
 }
 
 $(init);
