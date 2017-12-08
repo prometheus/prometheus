0ac149b34e6fa4b5d8ef391b010339c888d3965c
Alberto Cortés <alcortesm@gmail.com>
2017-12-08 17:52:25 +0100

    config: simplify tests by using testutil.NotOk (#3289)
    Also include filename in all LoadFile errors
    
    Also add mesage to testuitl.NotOk so we can identify failing tests when
    using table driven tests.

--- util/testutil/testing.go
+++ util/testutil/testing.go
@@ -49,10 +49,10 @@ func Ok(tb testing.TB, err error) {
 }
 
 // NotOk fails the test if an err is nil.
-func NotOk(tb testing.TB, err error) {
+func NotOk(tb testing.TB, err error, msg string, v ...interface{}) {
 	if err == nil {
 		_, file, line, _ := runtime.Caller(1)
-		fmt.Printf("\033[31m%s:%d: expected error, got nothing \033[39m\n\n", filepath.Base(file), line)
+		fmt.Printf("\033[31m%s:%d: "+msg+"expected error, got nothing\033[39m\n\n", append([]interface{}{filepath.Base(file), line}, v...)...)
 		tb.FailNow()
 	}
 }
