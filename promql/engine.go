98008530df813045e9dfdbc6431abe6015293c73
Brian Brazil <brian.brazil@robustperception.io>
2018-03-09 15:53:36 +0000

    Reduce allocs when working with matrix selectors
    Signed-off-by: Brian Brazil <brian.brazil@robustperception.io>

--- promql/engine.go
+++ promql/engine.go
@@ -776,7 +776,7 @@ func (ev *evaluator) eval(expr Expr) Value {
 				if ok {
 					mat := make(Matrix, 0, len(sel.series))
 					offset := durationMilliseconds(sel.Offset)
-					points := make([]Point, 0, 10)
+					points := getPointSlice(10)
 					// Process all the calls for one time series at a time.
 					for i, it := range sel.iterators {
 						ss := Series{}
@@ -799,6 +799,7 @@ func (ev *evaluator) eval(expr Expr) Value {
 							mat = append(mat, ss)
 						}
 					}
+          putPointSlice(points)
 					return mat
 				}
 			}
