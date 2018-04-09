6a81b958cdf71a9c63c0b668d2648ed9fd3f93b9
beorn7 <beorn@soundcloud.com>
2018-04-09 17:44:53 +0200

    Update vendoring of prometheus/common/route to include data race fix
    See https://github.com/prometheus/common/pull/125
    
    Signed-off-by: beorn7 <beorn@soundcloud.com>

--- vendor/github.com/prometheus/common/route/route.go
+++ vendor/github.com/prometheus/common/route/route.go
@@ -19,11 +19,12 @@ func WithParam(ctx context.Context, p, v string) context.Context {
 	return context.WithValue(ctx, param(p), v)
 }
 
-// Router wraps httprouter.Router and adds support for prefixed sub-routers
-// and per-request context injections.
+// Router wraps httprouter.Router and adds support for prefixed sub-routers,
+// per-request context injections and instrumentation.
 type Router struct {
 	rtr    *httprouter.Router
 	prefix string
+	instrh func(handlerName string, handler http.HandlerFunc) http.HandlerFunc
 }
 
 // New returns a new Router.
@@ -33,13 +34,22 @@ func New() *Router {
 	}
 }
 
+// WithInstrumentation returns a router with instrumentation support.
+func (r *Router) WithInstrumentation(instrh func(handlerName string, handler http.HandlerFunc) http.HandlerFunc) *Router {
+	return &Router{rtr: r.rtr, prefix: r.prefix, instrh: instrh}
+}
+
 // WithPrefix returns a router that prefixes all registered routes with prefix.
 func (r *Router) WithPrefix(prefix string) *Router {
-	return &Router{rtr: r.rtr, prefix: r.prefix + prefix}
+	return &Router{rtr: r.rtr, prefix: r.prefix + prefix, instrh: r.instrh}
 }
 
 // handle turns a HandlerFunc into an httprouter.Handle.
-func (r *Router) handle(h http.HandlerFunc) httprouter.Handle {
+func (r *Router) handle(handlerName string, h http.HandlerFunc) httprouter.Handle {
+	if r.instrh != nil {
+		// This needs to be outside the closure to avoid data race when reading and writing to 'h'.
+		h = r.instrh(handlerName, h)
+	}
 	return func(w http.ResponseWriter, req *http.Request, params httprouter.Params) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
@@ -53,27 +63,27 @@ func (r *Router) handle(h http.HandlerFunc) httprouter.Handle {
 
 // Get registers a new GET route.
 func (r *Router) Get(path string, h http.HandlerFunc) {
-	r.rtr.GET(r.prefix+path, r.handle(h))
+	r.rtr.GET(r.prefix+path, r.handle(path, h))
 }
 
 // Options registers a new OPTIONS route.
 func (r *Router) Options(path string, h http.HandlerFunc) {
-	r.rtr.OPTIONS(r.prefix+path, r.handle(h))
+	r.rtr.OPTIONS(r.prefix+path, r.handle(path, h))
 }
 
 // Del registers a new DELETE route.
 func (r *Router) Del(path string, h http.HandlerFunc) {
-	r.rtr.DELETE(r.prefix+path, r.handle(h))
+	r.rtr.DELETE(r.prefix+path, r.handle(path, h))
 }
 
 // Put registers a new PUT route.
 func (r *Router) Put(path string, h http.HandlerFunc) {
-	r.rtr.PUT(r.prefix+path, r.handle(h))
+	r.rtr.PUT(r.prefix+path, r.handle(path, h))
 }
 
 // Post registers a new POST route.
 func (r *Router) Post(path string, h http.HandlerFunc) {
-	r.rtr.POST(r.prefix+path, r.handle(h))
+	r.rtr.POST(r.prefix+path, r.handle(path, h))
 }
 
 // Redirect takes an absolute path and sends an internal HTTP redirect for it,
