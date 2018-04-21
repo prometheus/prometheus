3ea7e5e33835925728307d6f63b02a493f49b9e4
Thomas Jackson <jacksontj.89@gmail.com>
2018-04-21 11:51:18 -0700

    Only set CORS headers if they aren't set already
    This allows for middleware to set these to more specific values if desired

--- web/api/v1/api.go
+++ web/api/v1/api.go
@@ -101,7 +101,9 @@ type response struct {
 // Enables cross-site script calls.
 func setCORS(w http.ResponseWriter) {
 	for h, v := range corsHeaders {
-		w.Header().Set(h, v)
+		if _, ok := w.Header()[h]; !ok {
+			w.Header().Set(h, v)
+		}
 	}
 }
 
