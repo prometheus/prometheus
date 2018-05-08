810006b4b06460de0a69b73fee0855bf55c88e5d
Henri DF <henridf@gmail.com>
2018-05-08 01:48:13 -0700

    Pass query hints down into remote read query proto (#4122)
    Signed-off-by: Henri DF <henridf@gmail.com>

--- storage/remote/read.go
+++ storage/remote/read.go
@@ -43,8 +43,8 @@ type querier struct {
 
 // Select implements storage.Querier and uses the given matchers to read series
 // sets from the Client.
-func (q *querier) Select(_ *storage.SelectParams, matchers ...*labels.Matcher) (storage.SeriesSet, error) {
-	query, err := ToQuery(q.mint, q.maxt, matchers)
+func (q *querier) Select(p *storage.SelectParams, matchers ...*labels.Matcher) (storage.SeriesSet, error) {
+	query, err := ToQuery(q.mint, q.maxt, matchers, p)
 	if err != nil {
 		return nil, err
 	}
