aa5549f293e5040edf8f64447d36a90cc9afef45
chyeh <chyeh.taiwan@gmail.com>
2018-06-02 23:18:13 +0300

    Read and print for each profile
    It reads the text of each profile in `/debug/pprof/` and prints them.
    
    Signed-off-by: chyeh <chyeh.taiwan@gmail.com>

--- cmd/promtool/pprof.go
+++ cmd/promtool/pprof.go
@@ -11,6 +11,15 @@ import (
 	"github.com/prometheus/client_golang/api"
 )
 
+var pprofUrls = []string{
+	"/debug/pprof/block?debug=1",
+	"/debug/pprof/goroutine?debug=1",
+	"/debug/pprof/heap?debug=1",
+	"/debug/pprof/mutex?debug=1",
+	"/debug/pprof/threadcreate?debug=1",
+	"/debug/pprof/goroutine?debug=2",
+}
+
 func DebugPprof(url *url.URL) int {
 	config := api.Config{
 		Address: url.String(),
@@ -22,24 +31,26 @@ func DebugPprof(url *url.URL) int {
 		return 1
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Minute)
-	req, err := http.NewRequest(http.MethodGet, url.String()+"/debug/pprof", nil)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "debug pprof error:", err)
-		return 1
-	}
+	for _, u := range pprofUrls {
+		ctx, cancel := context.WithTimeout(context.Background(), 2*time.Minute)
+		req, err := http.NewRequest(http.MethodGet, url.String()+u, nil)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "debug pprof error:", err)
+			return 1
+		}
 
-	_, body, err := c.Do(ctx, req)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "debug pprof error:", err)
-		return 1
-	}
-	cancel()
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "debug pprof error:", err)
-		return 1
-	}
+		_, body, err := c.Do(ctx, req)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "debug pprof error:", err)
+			return 1
+		}
+		cancel()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "debug pprof error:", err)
+			return 1
+		}
 
-	fmt.Println(string(body))
+		fmt.Println(string(body))
+	}
 	return 0
 }
