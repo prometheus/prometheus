514b4d976315a8f9be5088b1b404878715e8f51d
chyeh <chyeh.taiwan@gmail.com>
2018-06-07 19:46:42 +0300

    Archive `metrics.txt` in `debug.tar.gz`
    Signed-off-by: chyeh <chyeh.taiwan@gmail.com>

--- cmd/promtool/metrics.go
+++ cmd/promtool/metrics.go
@@ -1,19 +1,28 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
-	"io/ioutil"
 	"os"
 )
 
 func DebugMetrics() int {
-	buf, err := NewBodyGetter(BodyGetterConfig{Path: "/metrics"}).Get()
+	tw := NewTarGzFileFileWriter(TarGzFileWriterConfig{FileName: "debug.tar.gz"})
+	defer func() {
+		if err := tw.Close(); err != nil {
+			panic(err)
+		}
+	}()
+	body, err := NewBodyGetter(BodyGetterConfig{Path: "/metrics"}).Get()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 	}
-	if err := ioutil.WriteFile("metrics.txt", buf, 0644); err != nil {
+
+	var buf bytes.Buffer
+	buf.Write(body)
+	if err := tw.AddFile("metrics.txt", buf); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 	}
-	fmt.Println(string(buf))
+	fmt.Println(buf.String())
 	return 0
 }
