70ebf3e5084e9d1d756c9a22c5af8f70c6d3044f
Sneha Inguva <singuva@digitalocean.com>
2018-06-18 10:33:46 -0700

    Update vendoring of Prometheus Go client
    This is to pickup changes from
    https://github.com/prometheus/client_golang/pull/414. It leads to
    better error output in promtool.
    
    Signed-off-by: Sneha Inguva <singuva@digitalocean.com>

--- vendor/github.com/prometheus/client_golang/prometheus/metric.go
+++ vendor/github.com/prometheus/client_golang/prometheus/metric.go
@@ -127,20 +127,6 @@ func (s LabelPairSorter) Less(i, j int) bool {
 	return s[i].GetName() < s[j].GetName()
 }
 
-type hashSorter []uint64
-
-func (s hashSorter) Len() int {
-	return len(s)
-}
-
-func (s hashSorter) Swap(i, j int) {
-	s[i], s[j] = s[j], s[i]
-}
-
-func (s hashSorter) Less(i, j int) bool {
-	return s[i] < s[j]
-}
-
 type invalidMetric struct {
 	desc *Desc
 	err  error
