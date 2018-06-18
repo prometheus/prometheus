4b01352fffaff9e6ca912f3ea8a1e0f910ffdc4d
Tom Wilkie <tom.wilkie@gmail.com>
2018-06-18 16:33:04 +0100

    Review feedback.
    Signed-off-by: Tom Wilkie <tom.wilkie@gmail.com>

--- storage/remote/codec.go
+++ storage/remote/codec.go
@@ -95,8 +95,8 @@ func ToQuery(from, to int64, matchers []*labels.Matcher, p *storage.SelectParams
 	if err != nil {
 		return nil, err
 	}
-	var rp *prompb.ReadHints = nil
 
+	var rp *prompb.ReadHints
 	if p != nil {
 		rp = &prompb.ReadHints{
 			StepMs: p.Step,
