20efec6289c5dd6d560cf620145958f5b31ba4d8
Sneha Inguva <sneha.inguva@gmail.com>
2018-07-17 22:08:38 -0600

    Update vendoring of Prometheus Go client (#4283)
    This is to pickup changes from
    https://github.com/prometheus/client_golang/pull/414. It leads to
    better error output in promtool.
    
    Signed-off-by: Sneha Inguva <singuva@digitalocean.com>

--- vendor/github.com/prometheus/client_golang/api/prometheus/v1/api.go
+++ vendor/github.com/prometheus/client_golang/api/prometheus/v1/api.go
@@ -455,6 +455,11 @@ type apiResponse struct {
 	Error     string          `json:"error"`
 }
 
+func apiError(code int) bool {
+	// These are the codes that Prometheus sends when it returns an error.
+	return code == statusAPIError || code == http.StatusBadRequest
+}
+
 func (c apiClient) Do(ctx context.Context, req *http.Request) (*http.Response, []byte, error) {
 	resp, body, err := c.Client.Do(ctx, req)
 	if err != nil {
@@ -463,7 +468,7 @@ func (c apiClient) Do(ctx context.Context, req *http.Request) (*http.Response, [
 
 	code := resp.StatusCode
 
-	if code/100 != 2 && code != statusAPIError {
+	if code/100 != 2 && !apiError(code) {
 		return resp, body, &Error{
 			Type: ErrBadResponse,
 			Msg:  fmt.Sprintf("bad response code %d", resp.StatusCode),
@@ -479,14 +484,14 @@ func (c apiClient) Do(ctx context.Context, req *http.Request) (*http.Response, [
 		}
 	}
 
-	if (code == statusAPIError) != (result.Status == "error") {
+	if apiError(code) != (result.Status == "error") {
 		err = &Error{
 			Type: ErrBadResponse,
 			Msg:  "inconsistent body for response code",
 		}
 	}
 
-	if code == statusAPIError && result.Status == "error" {
+	if apiError(code) && result.Status == "error" {
 		err = &Error{
 			Type: result.ErrorType,
 			Msg:  result.Error,
