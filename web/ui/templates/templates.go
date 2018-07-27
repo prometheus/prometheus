086e364e53e15e36533616ef9c437ba2d72d3c09
Simon Pasquier <spasquie@redhat.com>
2018-07-27 13:59:55 +0200

    Generate templates assets
    Signed-off-by: Simon Pasquier <spasquie@redhat.com>

--- web/ui/templates/templates.go
+++ web/ui/templates/templates.go
@@ -13,14 +13,12 @@
 
 // +build dev
 
-package static
+package templates
 
 import (
 	"go/build"
 	"log"
 	"net/http"
-	"os"
-	"strings"
 
 	"github.com/shurcooL/httpfs/filter"
 )
@@ -35,17 +33,6 @@ func importPathToDir(importPath string) string {
 
 // Assets contains the project's static assets.
 var Assets http.FileSystem = filter.Keep(
-	http.Dir(importPathToDir("github.com/prometheus/prometheus/web/ui/static")),
-	func(path string, fi os.FileInfo) bool {
-		switch {
-		case fi.IsDir():
-			return true
-		case strings.HasPrefix(path, "/css") || strings.HasPrefix(path, "/img") || strings.HasPrefix(path, "/js") || strings.HasPrefix(path, "/vendor"):
-			return !strings.HasSuffix(path, "map.js") &&
-				!strings.HasSuffix(path, "/bootstrap.js") &&
-				!strings.HasSuffix(path, "/bootstrap-theme.css") &&
-				!strings.HasSuffix(path, "/bootstrap.css")
-		}
-		return false
-	},
+	http.Dir(importPathToDir("github.com/prometheus/prometheus/web/ui/templates")),
+	filter.FilesWithExtensions(".html"),
 )
