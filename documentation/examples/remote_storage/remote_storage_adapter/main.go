4fb59d1e612323236f1b9c0e87e2bc6d3acec95b
beorn7 <beorn@soundcloud.com>
2018-09-17 13:05:43 +0200

    Remove use of deprecated prometheus.Handler
    Signed-off-by: beorn7 <beorn@soundcloud.com>

--- documentation/examples/remote_storage/remote_storage_adapter/main.go
+++ documentation/examples/remote_storage/remote_storage_adapter/main.go
@@ -30,6 +30,7 @@ import (
 	"github.com/gogo/protobuf/proto"
 	"github.com/golang/snappy"
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/prometheus/common/model"
 
 	influx "github.com/influxdata/influxdb/client/v2"
@@ -97,7 +98,7 @@ func init() {
 
 func main() {
 	cfg := parseFlags()
-	http.Handle(cfg.telemetryPath, prometheus.Handler())
+	http.Handle(cfg.telemetryPath, promhttp.Handler())
 
 	logLevel := promlog.AllowedLevel{}
 	if err := logLevel.Set(cfg.logLevel); err != nil {
