6bbc91294a3c4ebe22e447266f9e105a1650e18e
Julius Volz <julius.volz@gmail.com>
2018-12-03 11:26:47 +0800

    Better rounding for incoming query timestamps
    Fixes https://github.com/prometheus/prometheus/issues/4939
    
    Signed-off-by: Julius Volz <julius.volz@gmail.com>

--- web/api/v1/api.go
+++ web/api/v1/api.go
@@ -1150,6 +1150,7 @@ func (api *API) respondError(w http.ResponseWriter, apiErr *apiError, data inter
 func parseTime(s string) (time.Time, error) {
 	if t, err := strconv.ParseFloat(s, 64); err == nil {
 		s, ns := math.Modf(t)
+		ns = math.Round(ns*1000) / 1000
 		return time.Unix(int64(s), int64(ns*float64(time.Second))), nil
 	}
 	if t, err := time.Parse(time.RFC3339Nano, s); err == nil {
