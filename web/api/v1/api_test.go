302148fd69a852aa8a25c37032349852038beeb1
Matt Layher <mdlayher@gmail.com>
2019-01-16 17:28:08 -0500

    *: apply gofmt -s
    Signed-off-by: Matt Layher <mdlayher@gmail.com>

--- web/api/v1/api_test.go
+++ web/api/v1/api_test.go
@@ -1053,7 +1053,7 @@ func TestAdminEndpoints(t *testing.T) {
 			db:          tsdb,
 			enableAdmin: true,
 			endpoint:    snapshotAPI,
-			values:      map[string][]string{"skip_head": []string{"true"}},
+			values:      map[string][]string{"skip_head": {"true"}},
 
 			errType: errorNone,
 		},
@@ -1061,7 +1061,7 @@ func TestAdminEndpoints(t *testing.T) {
 			db:          tsdb,
 			enableAdmin: true,
 			endpoint:    snapshotAPI,
-			values:      map[string][]string{"skip_head": []string{"xxx"}},
+			values:      map[string][]string{"skip_head": {"xxx"}},
 
 			errType: errorBadData,
 		},
@@ -1127,7 +1127,7 @@ func TestAdminEndpoints(t *testing.T) {
 			db:          tsdb,
 			enableAdmin: true,
 			endpoint:    deleteAPI,
-			values:      map[string][]string{"match[]": []string{"123"}},
+			values:      map[string][]string{"match[]": {"123"}},
 
 			errType: errorBadData,
 		},
@@ -1135,7 +1135,7 @@ func TestAdminEndpoints(t *testing.T) {
 			db:          tsdb,
 			enableAdmin: true,
 			endpoint:    deleteAPI,
-			values:      map[string][]string{"match[]": []string{"up"}, "start": []string{"xxx"}},
+			values:      map[string][]string{"match[]": {"up"}, "start": {"xxx"}},
 
 			errType: errorBadData,
 		},
@@ -1143,7 +1143,7 @@ func TestAdminEndpoints(t *testing.T) {
 			db:          tsdb,
 			enableAdmin: true,
 			endpoint:    deleteAPI,
-			values:      map[string][]string{"match[]": []string{"up"}, "end": []string{"xxx"}},
+			values:      map[string][]string{"match[]": {"up"}, "end": {"xxx"}},
 
 			errType: errorBadData,
 		},
@@ -1151,7 +1151,7 @@ func TestAdminEndpoints(t *testing.T) {
 			db:          tsdb,
 			enableAdmin: true,
 			endpoint:    deleteAPI,
-			values:      map[string][]string{"match[]": []string{"up"}},
+			values:      map[string][]string{"match[]": {"up"}},
 
 			errType: errorNone,
 		},
@@ -1159,7 +1159,7 @@ func TestAdminEndpoints(t *testing.T) {
 			db:          tsdb,
 			enableAdmin: true,
 			endpoint:    deleteAPI,
-			values:      map[string][]string{"match[]": []string{"up{job!=\"foo\"}", "{job=~\"bar.+\"}", "up{instance!~\"fred.+\"}"}},
+			values:      map[string][]string{"match[]": {"up{job!=\"foo\"}", "{job=~\"bar.+\"}", "up{instance!~\"fred.+\"}"}},
 
 			errType: errorNone,
 		},
@@ -1167,7 +1167,7 @@ func TestAdminEndpoints(t *testing.T) {
 			db:          tsdbWithError,
 			enableAdmin: true,
 			endpoint:    deleteAPI,
-			values:      map[string][]string{"match[]": []string{"up"}},
+			values:      map[string][]string{"match[]": {"up"}},
 
 			errType: errorInternal,
 		},
