10ec2552af1be4a421b89afd4a1f8eb17948a187
Giedrius Statkevičius <giedriuswork@gmail.com>
2019-03-16 12:10:07 +0200

    promparse: sort all labels when parsing (#5372)
    * promparse: sort all labels when parsing
    
    Some label names might start with an uppercase letter in which case it
    needs to come before __name__. Without this it means that we are not
    maintaining the same invariant that all label names should be sorted in
    such cases.
    
    Amend the tests to check this problem automatically. Without the change
    the tests do not pass.
    
    Signed-off-by: Giedrius Statkevičius <giedriuswork@gmail.com>

--- pkg/textparse/promparse.go
+++ pkg/textparse/promparse.go
@@ -227,9 +227,8 @@ func (p *PromParser) Metric(l *labels.Labels) string {
 		*l = append(*l, labels.Label{Name: s[a:b], Value: s[c:d]})
 	}
 
-	// Sort labels. We can skip the first entry since the metric name is
-	// already at the right place.
-	sort.Sort((*l)[1:])
+	// Sort labels to maintain the sorted labels invariant.
+	sort.Sort(*l)
 
 	return s
 }
