27d08f3e6d073c81dffd71bc524fed62b8534e23
Thomas Jackson <jacksontj.89@gmail.com>
2019-04-30 10:34:24 -0700

    Add more noop types
    Signed-off-by: Thomas Jackson <jacksontj.89@gmail.com>

--- storage/noop.go
+++ storage/noop.go
@@ -14,10 +14,55 @@
 package storage
 
 import (
+	"context"
 	"math"
 
 	"github.com/prometheus/prometheus/pkg/labels"
 )
+
+type noopStorage struct{}
+
+func NoopStorage() Storage {
+	return &noopStorage{}
+}
+
+// Querier returns a new Querier on the storage.
+func (n *noopStorage) Querier(ctx context.Context, mint, maxt int64) (Querier, error) {
+	return NoopQuerier(), nil
+}
+
+// StartTime returns the oldest timestamp stored in the storage.
+func (n *noopStorage) StartTime() (int64, error) {
+	return 0, nil
+}
+
+// Appender returns a new appender against the storage.
+func (n *noopStorage) Appender() (Appender, error) {
+	return NoopAppender(), nil
+}
+
+// Close closes the storage and all its underlying resources.
+func (n *noopStorage) Close() error {
+	return nil
+}
+
+type noopAppender struct{}
+
+func NoopAppender() Appender {
+	return &noopAppender{}
+}
+
+func (a *noopAppender) Add(l labels.Labels, t int64, v float64) (uint64, error) {
+	return 0, nil
+}
+
+func (a *noopAppender) AddFast(l labels.Labels, ref uint64, t int64, v float64) error {
+	return nil
+}
+
+// Commit submits the collected samples and purges the batch.
+func (a *noopAppender) Commit() error   { return nil }
+func (a *noopAppender) Rollback() error { return nil }
 
 type noopQuerier struct{}
 
