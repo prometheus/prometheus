68606a851a47a782f5b0a3ea154bee2a6a8c208d
Lee Gaines <leetgaines@gmail.com>
2019-05-15 09:41:56 -0700

    change build constraint for windows
    Signed-off-by: Lee Gaines <leetgaines@gmail.com>

--- pkg/runtime/statfs_386.go
+++ pkg/runtime/statfs_386.go
@@ -11,6 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// +build !windows
 // +build !darwin
 // +build !freebsd
 // +build 386
