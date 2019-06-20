ba64fc542b4fa19f2e4ced60d9269fc232e451dd
Julius Volz <julius.volz@gmail.com>
2019-06-20 15:00:40 +0200

    Add some missing React output assets
    Signed-off-by: Julius Volz <julius.volz@gmail.com>

---
+++ web/ui/static/graph-new/precache-manifest.62cbd9a713fc77974f5dfd381174484f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5831dcef239d47632924",
+    "url": "./static/css/main.5350f5aa.chunk.css"
+  },
+  {
+    "revision": "5831dcef239d47632924",
+    "url": "./static/js/main.5831dcef.chunk.js"
+  },
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "29de1a25a45bd7796550",
+    "url": "./static/css/2.8c201799.chunk.css"
+  },
+  {
+    "revision": "29de1a25a45bd7796550",
+    "url": "./static/js/2.29de1a25.chunk.js"
+  },
+  {
+    "revision": "81c3a1d51fe639b83426208093dd6a61",
+    "url": "./index.html"
+  }
+];
