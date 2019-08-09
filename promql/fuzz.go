5391aeccbbd97eaf90a88ea8403330d150dfdb3f
AllenZMC <zhongming.chang@daocloud.io>
2019-08-09 21:18:43 +0800

    fix mis-spelling in fuzz.go
    Signed-off-by: czm <zhongming.chang@daocloud.io>

--- promql/fuzz.go
+++ promql/fuzz.go
@@ -32,7 +32,7 @@ import "github.com/prometheus/prometheus/pkg/textparse"
 //
 // Further input samples should go in the folders fuzz-data/ParseMetric/corpus.
 //
-// Repeat for ParseMetricSeletion, ParseExpr and ParseStmt.
+// Repeat for ParseMetricSelection, ParseExpr and ParseStmt.
 
 // Tuning which value is returned from Fuzz*-functions has a strong influence
 // on how quick the fuzzer converges on "interesting" cases. At least try
