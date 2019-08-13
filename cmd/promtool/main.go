714b5a649a7d24c8b18f4ab2b9d3b0f49eb2e9b7
Julius Volz <julius.volz@gmail.com>
2019-08-13 16:22:55 +0200

    Add issue for showing query warnings in promtool
    Signed-off-by: Julius Volz <julius.volz@gmail.com>

--- cmd/promtool/main.go
+++ cmd/promtool/main.go
@@ -501,7 +501,7 @@ func QueryLabels(url *url.URL, name string, p printer) int {
 	// Run query against client.
 	api := v1.NewAPI(c)
 	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Minute)
-	// TODO: Show warnings (second return value) somehow?
+	// TODO: Show warnings (second return value) somehow? See https://github.com/prometheus/prometheus/issues/5885
 	val, _, err := api.LabelValues(ctx, name)
 	cancel()
 
