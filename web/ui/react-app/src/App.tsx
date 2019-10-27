8afa8452fd5610e2209160ae996c42e1ad558ffd
Boyko <boyskila@users.noreply.github.com>
2019-10-27 23:03:39 +0200

    reach router integration initial commit (#6232)
    Signed-off-by: blalov <boyko.lalov@tick42.com>

--- web/ui/react-app/src/App.tsx
+++ web/ui/react-app/src/App.tsx
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
-
-import {
-  Container,
-} from 'reactstrap';
+import Navigation from "./Navbar";
+import { Container } from 'reactstrap';
 
 import './App.css';
-import PanelList from './PanelList';
+import { Router } from '@reach/router';
+import {
+  Alerts,
+  Config,
+  Flags,
+  Rules,
+  Services,
+  Status,
+  Targets,
+  PanelList,
+} from './pages';
 
 class App extends Component {
   render() {
     return (
-      <Container fluid={true}>
-        <PanelList />
-      </Container>
+      <>
+        <Navigation />
+        <Container fluid>
+          <Router basepath="/react">
+            <PanelList path="/graph" />
+            <Alerts path="/alerts" />
+            <Config path="/config" />
+            <Flags path="/flags" />
+            <Rules path="/rules" />
+            <Services path="/service-discovery" />
+            <Status path="/status" />
+            <Targets path="/targets" />
+          </Router>
+        </Container>
+      </>
     );
   }
 }
