fffb5ca1e9f0caa431ef6be1bf81ca3c9281662c
Julius Volz <julius.volz@gmail.com>
2019-11-02 21:45:22 +0100

    React UI: More conversions to Function Components (#6259)
    * React UI: More conversions to Function Components
    
    Signed-off-by: Julius Volz <julius.volz@gmail.com>
    
    * Address chat feedback over Riot
    
    Signed-off-by: Julius Volz <julius.volz@gmail.com>

--- web/ui/react-app/src/Legend.tsx
+++ web/ui/react-app/src/Legend.tsx
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React, { FC } from 'react';
 
 import SeriesName from './SeriesName';
 
@@ -6,31 +6,23 @@ interface LegendProps {
   series: any; // TODO: Type this.
 }
 
-class Legend extends PureComponent<LegendProps> {
-  renderLegendItem(s: any) {
-    return (
-      <tr key={s.index} className="legend-item">
-        <td>
-          <div className="legend-swatch" style={{ backgroundColor: s.color }}></div>
-        </td>
-        <td>
-          <SeriesName labels={s.labels} format={true} />
-        </td>
-      </tr>
-    );
-  }
-
-  render() {
-    return (
-      <table className="graph-legend">
-        <tbody>
-          {this.props.series.map((s: any) => {
-            return this.renderLegendItem(s);
-          })}
-        </tbody>
-      </table>
-    );
-  }
-}
+const Legend: FC<LegendProps> = ({ series }) => {
+  return (
+    <table className="graph-legend">
+      <tbody>
+        {series.map((s: any) => (
+          <tr key={s.index} className="legend-item">
+            <td>
+              <div className="legend-swatch" style={{ backgroundColor: s.color }}></div>
+            </td>
+            <td>
+              <SeriesName labels={s.labels} format={true} />
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
 
 export default Legend;
