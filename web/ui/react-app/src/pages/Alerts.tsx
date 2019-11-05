431844f0a7c289e4255a68f09a18fcca09637fb2
Julius Volz <julius.volz@gmail.com>
2019-11-05 20:09:37 +0100

    React UI: Mark not-yet-done pages as under construction (#6277)
    This is a quick change because the release is about to be cut in a few
    hours and it would be good to clarify to users what the empty pages in
    the new UI are about.
    
    Signed-off-by: Julius Volz <julius.volz@gmail.com>

--- web/ui/react-app/src/pages/Alerts.tsx
+++ web/ui/react-app/src/pages/Alerts.tsx
@@ -1,7 +1,15 @@
 import React, { FC } from 'react';
 import { RouteComponentProps } from '@reach/router';
 import PathPrefixProps from '../PathPrefixProps';
+import { Alert } from 'reactstrap';
 
-const Alerts: FC<RouteComponentProps & PathPrefixProps> = props => <div>Alerts page</div>;
+const Alerts: FC<RouteComponentProps & PathPrefixProps> = ({ pathPrefix }) => (
+  <>
+    <h2>Alerts</h2>
+    <Alert color="warning">
+      This page is still under construction. Please try it in the <a href={`${pathPrefix}/alerts`}>Classic UI</a>.
+    </Alert>
+  </>
+);
 
 export default Alerts;
