f141f97d0bc4e45818a3679a6180b913ad471828
shichun.fsc <shichun.fsc@alibaba-inc.com>
2019-11-26 16:55:29 +0800

    fix bug

--- discovery/ecs/ecs.go
+++ discovery/ecs/ecs.go
@@ -319,7 +319,7 @@ func (d *Discovery) queryFromDescribeInstances() (instances []ecs_pop.Instance, 
 				}
 			}
 
-			if describeInstancesResponse.Instances == nil || len(describeInstancesResponse.Instances.Instance) == 0 {
+			if len(describeInstancesResponse.Instances.Instance) == 0 {
 				break
 			}
 			currentTotalCount += newInstanceIndex
