9d5c2185fe31e8bd1eddf7074706195677417d95
Harkishen Singh <harkishensingh@hotmail.com>
2020-01-07 02:07:30 +0530

    yamlv3 in manage_test
    Signed-off-by: Harkishen Singh <harkishensingh@hotmail.com>

--- rules/manager_test.go
+++ rules/manager_test.go
@@ -25,7 +25,7 @@ import (
 
 	"github.com/go-kit/kit/log"
 	"github.com/prometheus/common/model"
-	yaml "gopkg.in/yaml.v2"
+	yaml "gopkg.in/yaml.v3"
 
 	"github.com/prometheus/prometheus/pkg/labels"
 	"github.com/prometheus/prometheus/pkg/rulefmt"
