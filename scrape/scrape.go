1265ea0da00c3fa669e42fbd595c3b0d48d4e600
Julien Pivotto <roidelapluie@inuits.eu>
2020-01-21 10:57:51 +0100

    update still
    Signed-off-by: Julien Pivotto <roidelapluie@inuits.eu>

--- scrape/scrape.go
+++ scrape/scrape.go
@@ -1344,7 +1344,7 @@ func zeroConfig(c *config.ScrapeConfig) *config.ScrapeConfig {
 	return &z
 }
 
-// reusableCache compares two scrape config and tells wheter the cache is stoll
+// reusableCache compares two scrape config and tells wheter the cache is still
 // valid.
 func reusableCache(r, l *config.ScrapeConfig) bool {
 	if r == nil || l == nil {
