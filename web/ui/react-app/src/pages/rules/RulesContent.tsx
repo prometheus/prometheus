eed32ef3e1bb0dca5f96772986e9d634349284ff
Julius Volz <julius.volz@gmail.com>
2020-01-27 10:57:24 +0100

    React UI: Some /rules page follow-ups
    Just a few follow-ups for now, as I'm a bit short on time today:
    
    - Single table for all rule groups, so that all columns are aligned on
      the page (like in the old UI).
    - Remove redundant direct cell styling / switch to using "rule-cell"
      also for recording rules.
    - Remove extra left spacing of graph expression links.
    - Remove unused CSS.
    
    Signed-off-by: Julius Volz <julius.volz@gmail.com>

--- web/ui/react-app/src/pages/rules/RulesContent.tsx
+++ web/ui/react-app/src/pages/rules/RulesContent.tsx
@@ -26,10 +26,7 @@ export interface RulesMap {
 const GraphExpressionLink: FC<{ expr: string; title: string }> = props => {
   return (
     <>
-      <strong>{props.title}:</strong>
-      <Link className="ml-4" to={createExpressionLink(props.expr)}>
-        {props.expr}
-      </Link>
+      <strong>{props.title}:</strong> <Link to={createExpressionLink(props.expr)}>{props.expr}</Link>
       <br />
     </>
   );
@@ -54,75 +51,77 @@ export const RulesContent: FC<RouteComponentProps & RulesContentProps> = ({ resp
     return (
       <>
         <h2>Rules</h2>
-        {groups.map((g, i) => {
-          return (
-            <Table bordered key={i}>
-              <thead>
-                <tr>
-                  <td colSpan={3}>
-                    <a href={'#' + g.name}>
-                      <h2 id={g.name}>{g.name}</h2>
-                    </a>
-                  </td>
-                  <td>
-                    <h2>{formatRelative(g.lastEvaluation, now())} ago</h2>
-                  </td>
-                  <td>
-                    <h2>{humanizeDuration(parseFloat(g.evaluationTime) * 1000)}</h2>
-                  </td>
-                </tr>
-              </thead>
-              <tbody>
-                <tr className="font-weight-bold">
-                  <td>Rule</td>
-                  <td>State</td>
-                  <td>Error</td>
-                  <td>Last Evaluation</td>
-                  <td>Evaluation Time</td>
-                </tr>
-                {g.rules.map((r, i) => {
-                  return (
-                    <tr key={i}>
-                      {r.alerts ? (
-                        <td style={{ backgroundColor: '#F5F5F5' }} className="rule_cell">
-                          <GraphExpressionLink title="alert" expr={r.name} />
-                          <GraphExpressionLink title="expr" expr={r.query} />
-                          <div>
-                            <strong>labels:</strong>
-                            {Object.entries(r.labels).map(([key, value]) => (
-                              <div className="ml-4" key={key}>
-                                {key}: {value}
-                              </div>
-                            ))}
-                          </div>
-                          <div>
-                            <strong>annotations:</strong>
-                            {Object.entries(r.annotations).map(([key, value]) => (
-                              <div className="ml-4" key={key}>
-                                {key}: {value}
-                              </div>
-                            ))}
-                          </div>
+        <Table bordered>
+          {groups.map((g, i) => {
+            return (
+              <React.Fragment key={i}>
+                <thead>
+                  <tr>
+                    <td colSpan={3}>
+                      <a href={'#' + g.name}>
+                        <h2 id={g.name}>{g.name}</h2>
+                      </a>
+                    </td>
+                    <td>
+                      <h2>{formatRelative(g.lastEvaluation, now())} ago</h2>
+                    </td>
+                    <td>
+                      <h2>{humanizeDuration(parseFloat(g.evaluationTime) * 1000)}</h2>
+                    </td>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr className="font-weight-bold">
+                    <td>Rule</td>
+                    <td>State</td>
+                    <td>Error</td>
+                    <td>Last Evaluation</td>
+                    <td>Evaluation Time</td>
+                  </tr>
+                  {g.rules.map((r, i) => {
+                    return (
+                      <tr key={i}>
+                        {r.alerts ? (
+                          <td className="rule-cell">
+                            <GraphExpressionLink title="alert" expr={r.name} />
+                            <GraphExpressionLink title="expr" expr={r.query} />
+                            <div>
+                              <strong>labels:</strong>
+                              {Object.entries(r.labels).map(([key, value]) => (
+                                <div className="ml-4" key={key}>
+                                  {key}: {value}
+                                </div>
+                              ))}
+                            </div>
+                            <div>
+                              <strong>annotations:</strong>
+                              {Object.entries(r.annotations).map(([key, value]) => (
+                                <div className="ml-4" key={key}>
+                                  {key}: {value}
+                                </div>
+                              ))}
+                            </div>
+                          </td>
+                        ) : (
+                          <td className="rule-cell">
+                            <GraphExpressionLink title="record" expr={r.name} />
+                            <GraphExpressionLink title="expr" expr={r.query} />
+                          </td>
+                        )}
+                        <td>
+                          <Badge color={getBadgeColor(r.health)}>{r.health.toUpperCase()}</Badge>
                         </td>
-                      ) : (
-                        <td style={{ backgroundColor: '#F5F5F5' }}>
-                          <GraphExpressionLink title="record" expr={r.name} />
-                          <GraphExpressionLink title="expr" expr={r.query} />
-                        </td>
-                      )}
-                      <td>
-                        <Badge color={getBadgeColor(r.health)}>{r.health.toUpperCase()}</Badge>
-                      </td>
-                      <td>{r.lastError ? <Alert color="danger">{r.lastError}</Alert> : null}</td>
-                      <td>{formatRelative(r.lastEvaluation, now())} ago</td>
-                      <td>{humanizeDuration(parseFloat(r.evaluationTime) * 1000)}</td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </Table>
-          );
-        })}
+                        <td>{r.lastError ? <Alert color="danger">{r.lastError}</Alert> : null}</td>
+                        <td>{formatRelative(r.lastEvaluation, now())} ago</td>
+                        <td>{humanizeDuration(parseFloat(r.evaluationTime) * 1000)}</td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </React.Fragment>
+            );
+          })}
+        </Table>
       </>
     );
   }
