a885a6ac17aea1e71eb2d6280e6e7c6776274dba
fuling <fuling.lgz@alibaba-inc.com>
2020-01-30 00:27:00 +0800

    Merge
    Signed-off-by: fuling <fuling.lgz@alibaba-inc.com>

--- storage/remote/codec.go
+++ storage/remote/codec.go
@@ -164,6 +164,42 @@ func FromQueryResult(res *prompb.QueryResult) storage.SeriesSet {
 	}
 }
 
+
+func sortSamples(arr *[]prompb.Sample, startIndex, endIndex int) int {
+	if startIndex >= endIndex {
+		return -1
+	}
+	var pivotIndex = partition(arr, startIndex, endIndex)
+	sortSamples(arr, startIndex, pivotIndex-1)
+	sortSamples(arr, pivotIndex+1, endIndex)
+	return 0
+}
+
+func partition(arr0 *[]prompb.Sample, startIndex, endIndex int) int {
+	arr := *arr0
+	var pivot = arr[startIndex].Timestamp
+	var left = startIndex
+	var right = endIndex
+	for left != right {
+		for left < right && arr[right].Timestamp > pivot {
+			right--
+		}
+
+		for left < right && arr[left].Timestamp <= pivot {
+			left++
+		}
+		if left < right {
+			p := arr[left]
+			arr[left] = arr[right]
+			arr[right] = p
+		}
+	}
+	p := arr[left]
+	arr[left] = arr[startIndex]
+	arr[startIndex] = p
+	return left
+}
+
 // NegotiateResponseType returns first accepted response type that this server supports.
 // On the empty accepted list we assume that the SAMPLES response type was requested. This is to maintain backward compatibility.
 func NegotiateResponseType(accepted []prompb.ReadRequest_ResponseType) (prompb.ReadRequest_ResponseType, error) {
