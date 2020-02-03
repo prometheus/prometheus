65f5b9827d41d0a5fb4adc5e7bffe021ef359f54
Tobias Guggenmos <tguggenm@redhat.com>
2020-02-03 17:23:44 +0100

    Rename package
    Signed-off-by: Tobias Guggenmos <tguggenm@redhat.com>

--- promql/parser/functions.go
+++ promql/parser/functions.go
@@ -11,7 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package promql
+package parser
 
 // Function represents a function of the expression language and is
 // used by function nodes.
--- promql/parser/generated_parser.y
+++ promql/parser/generated_parser.y
@@ -12,7 +12,7 @@
 // limitations under the License.
 
 %{
-package promql
+package parser
 
 import (
         "math"
