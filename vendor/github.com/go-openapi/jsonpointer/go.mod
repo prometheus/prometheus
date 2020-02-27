ec35a4fcf6bab8d3776277e0b6a88f99cb9a07b3
Sylvain Rabot <s.rabot@lectra.com>
2020-02-27 18:40:57 +0100

    Remove vendoring because GOPROXY has our backs
    Signed-off-by: Sylvain Rabot <s.rabot@lectra.com>

--- vendor/github.com/go-openapi/jsonpointer/go.mod
+++
@@ -1,6 +0,0 @@
-module github.com/go-openapi/jsonpointer
-
-require (
-	github.com/go-openapi/swag v0.19.2
-	github.com/stretchr/testify v1.3.0
-)
--- vendor/github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger/genswagger/helpers.go
+++
@@ -1,9 +0,0 @@
-//+build go1.12
-
-package genswagger
-
-import "strings"
-
-func fieldName(k string) string {
-	return strings.ReplaceAll(strings.Title(k), "-", "_")
-}
