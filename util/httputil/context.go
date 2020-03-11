bf63dd5449a06572c192e6efb5bc136c1ab4a113
beorn7 <beorn@grafana.com>
2020-03-11 13:29:15 +0100

    Use `struct{}` as underlying type for context keys
    This is an alternative to #6963.
    
    Signed-off-by: beorn7 <beorn@grafana.com>

--- util/httputil/context.go
+++ util/httputil/context.go
@@ -21,14 +21,12 @@ import (
 	"github.com/prometheus/prometheus/promql"
 )
 
-type ctxParam int
-
-var pathParam ctxParam
+type pathParam struct{}
 
 // ContextWithPath returns a new context with the given path to be used later
 // when logging the query.
 func ContextWithPath(ctx context.Context, path string) context.Context {
-	return context.WithValue(ctx, pathParam, path)
+	return context.WithValue(ctx, pathParam{}, path)
 }
 
 // ContextFromRequest returns a new context with identifiers of
@@ -40,7 +38,7 @@ func ContextFromRequest(ctx context.Context, r *http.Request) context.Context {
 		ip, _, _ = net.SplitHostPort(r.RemoteAddr)
 	}
 	var path string
-	if v := ctx.Value(pathParam); v != nil {
+	if v := ctx.Value(pathParam{}); v != nil {
 		path = v.(string)
 	}
 	return promql.NewOriginContext(ctx, map[string]interface{}{
