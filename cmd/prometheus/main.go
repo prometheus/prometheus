f761c0a22e42e4485a95ae42fbdeb4cf9d6fa37d
Julien Pivotto <roidelapluie@inuits.eu>
2020-05-07 22:57:57 +0200

    Fix register
    Signed-off-by: Julien Pivotto <roidelapluie@inuits.eu>

--- cmd/prometheus/main.go
+++ cmd/prometheus/main.go
@@ -67,7 +67,7 @@ import (
 	"github.com/prometheus/prometheus/util/strutil"
 	"github.com/prometheus/prometheus/web"
 
-	_ "github.com/prometheus/prometheus/discovery/file"
+	_ "github.com/prometheus/prometheus/discovery/consul"
 )
 
 var (
