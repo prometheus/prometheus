1b6107020cd34d43dc7db6f583c7ca29e9ffd1e6
Bert Hartmann <berthartm@gmail.com>
2020-06-01 19:40:28 -0400

    revert back to DefBuckets with addons to be backwards compatible
    Signed-off-by: Bert Hartmann <berthartm@gmail.com>

--- storage/remote/queue_manager.go
+++ storage/remote/queue_manager.go
@@ -115,7 +115,7 @@ func newQueueManagerMetrics(r prometheus.Registerer, rn, e string) *queueManager
 		Subsystem:   subsystem,
 		Name:        "sent_batch_duration_seconds",
 		Help:        "Duration of sample batch send calls to the remote storage.",
-		Buckets:     prometheus.ExponentialBuckets(.03125, 2, 15),
+		Buckets:     append(prometheus.DefBuckets, 20, 45, 60, 150, 300),
 		ConstLabels: constLabels,
 	})
 	m.highestSentTimestamp = &maxGauge{
