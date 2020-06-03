52bb2032ae5b2e2416030f07fd895f65aa04acef
Ganesh Vernekar <cs15btech11018@iith.ac.in>
2020-06-03 20:04:13 +0530

    Update protos
    Signed-off-by: Ganesh Vernekar <cs15btech11018@iith.ac.in>

--- prompb/rpc.pb.gw.go
+++ prompb/rpc.pb.gw.go
@@ -55,7 +55,10 @@ func local_request_Admin_TSDBSnapshot_0(ctx context.Context, marshaler runtime.M
 	var protoReq TSDBSnapshotRequest
 	var metadata runtime.ServerMetadata
 
-	if err := runtime.PopulateQueryParameters(&protoReq, req.URL.Query(), filter_Admin_TSDBSnapshot_0); err != nil {
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Admin_TSDBSnapshot_0); err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
