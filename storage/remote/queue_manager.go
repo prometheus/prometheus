133411225c1f89d9526d35f35e07ac10d5541fee
njingco <jingco.nicole@gmail.com>
2020-07-31 14:24:04 -0700

    removed offset element in the SegmentRecord.json
    Signed-off-by: njingco <jingco.nicole@gmail.com>

--- storage/remote/queue_manager.go
+++ storage/remote/queue_manager.go
@@ -272,7 +272,6 @@ var Checkpoint = Checkpoints{}
 
 type SegmentRecord struct {
 	Segment  string
-	Offset   string
 	Endpoint string
 }
 
@@ -398,9 +397,6 @@ func (t *QueueManager) Start() {
 	if err != nil {
 		fmt.Println(err)
 	} else {
-		// TODO:
-		// - Set communication with WAL to set the segment file
-
 		for _, record := range check.Checkpoints {
 			segmentNumber, err := strconv.Atoi(record.Segment)
 			if err != nil {
@@ -454,7 +450,6 @@ func (t *QueueManager) getSegmentRecords() SegmentRecord {
 
 	record := SegmentRecord{
 		Segment:  RecordSegment,
-		Offset:   "00000000",
 		Endpoint: RecordEndpoint,
 	}
 
