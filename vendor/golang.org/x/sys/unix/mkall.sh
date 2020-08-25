8de24091c147ab7b7ddf8a1953aacba7a1adbe6e
Luke Chen <showuon@gmail.com>
2020-08-25 16:10:41 +0800

    Fix typo and add links for better readability
    Signed-off-by: Luke Chen <showuon@gmail.com>

--- vendor/golang.org/x/sys/unix/mkall.sh
+++ vendor/golang.org/x/sys/unix/mkall.sh
@@ -48,7 +48,7 @@ esac
 
 if [[ "$GOOS" = "linux" ]]; then
 	# Use the Docker-based build system
-	# Files generated through docker (use $cmd so you can Ctl-C the build or run)
+	# Files generated through docker (use $cmd so you can Ctrl-C the build or run)
 	$cmd docker build --tag generate:$GOOS $GOOS
 	$cmd docker run --interactive --tty --volume $(cd -- "$(dirname -- "$0")" && /bin/pwd):/build generate:$GOOS
 	exit
