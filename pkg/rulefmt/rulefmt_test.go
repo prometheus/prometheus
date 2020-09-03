1d1131a76f4463b5a8cdd07d4dd49e57ff5084ed
Harkishen-Singh <harkishensingh@hotmail.com>
2020-09-03 22:35:10 +0530

    Fix tests for windows.
    Signed-off-by: Harkishen-Singh <harkishensingh@hotmail.com>

--- pkg/rulefmt/rulefmt_test.go
+++ pkg/rulefmt/rulefmt_test.go
@@ -38,7 +38,11 @@ func TestParseFileFailure(t *testing.T) {
 	}{
 		{
 			filename: "duplicate_grp.bad.yaml",
-			errMsg:   `testdata/duplicate_grp.bad.yaml: 3:9: group name: "yolo" is repeated in the same file`,
+			errMsg:   `group name: "yolo" is repeated in the same file`,
+		},
+		{
+			filename: "unknown_fields.bad.yaml",
+			errMsg: "line 13: field anotations not found in type rulefmt.Rule",
 		},
 		{
 			filename: "bad_expr.bad.yaml",
