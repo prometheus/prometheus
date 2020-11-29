3300b5b5cc34301d04a84708eb56705feba70ba8
shir menaya <shir@perimeterx.com>
2020-11-29 16:48:40 +0200

    Network and not network

--- discovery/gce/gce.go
+++ discovery/gce/gce.go
@@ -157,7 +157,7 @@ func (d *Discovery) refresh(ctx context.Context) ([]*targetgroup.Group, error) {
 				continue
 			}
 			networkFullPath := "https://www.googleapis.com/compute/v1/projects/" + d.project + "/global/networks/" + d.network
-			if inst.NetworkInterfaces[0].network != networkFullPath {
+			if inst.NetworkInterfaces[0].Network != networkFullPath {
 				continue
 			}
 			labels := model.LabelSet{
