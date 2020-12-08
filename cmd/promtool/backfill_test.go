82b5f1d8b12cd4a676f1c77051691d29bd211cad
Julien Pivotto <roidelapluie@inuits.eu>
2020-12-08 21:55:45 +0100

    Backfill: Use mmap to reuse parser code
    Signed-off-by: Julien Pivotto <roidelapluie@inuits.eu>

--- cmd/promtool/backfill_test.go
+++ cmd/promtool/backfill_test.go
@@ -33,17 +33,6 @@ type backfillSample struct {
 	Labels    labels.Labels
 }
 
-func createTemporaryOpenMetricsFile(t *testing.T, text string) string {
-	newf, err := ioutil.TempFile("", "")
-	require.NoError(t, err)
-
-	_, err = newf.WriteString(text)
-	require.NoError(t, err)
-	require.NoError(t, newf.Close())
-
-	return newf.Name()
-}
-
 func sortSamples(samples []backfillSample) {
 	sort.Slice(samples, func(x, y int) bool {
 		sx, sy := samples[x], samples[y]
@@ -330,6 +319,30 @@ http_requests_total{code="400"} 1 1565166113.989
 			},
 		},
 		{
+			ToParse: `no_newline_after_eof 42 6900
+# EOF`,
+			IsOk:                 true,
+			Description:          "Sample without newline after # EOF.",
+			MaxSamplesInAppender: 5000,
+			Expected: struct {
+				MinTime   int64
+				MaxTime   int64
+				NumBlocks int
+				Samples   []backfillSample
+			}{
+				MinTime:   6900000,
+				MaxTime:   6900000,
+				NumBlocks: 1,
+				Samples: []backfillSample{
+					{
+						Timestamp: 6900000,
+						Value:     42,
+						Labels:    labels.FromStrings("__name__", "no_newline_after_eof"),
+					},
+				},
+			},
+		},
+		{
 			ToParse: `bare_metric 42.24 1001
 # EOF
 `,
@@ -382,16 +395,27 @@ no_nl{type="no newline"}
 			IsOk:        false,
 			Description: "No newline.",
 		},
+		{
+			ToParse: `# HELP no_eof This test has no EOF so will fail
+# TYPE no_eof gauge
+no_eof 1 1
+`,
+			IsOk:        false,
+			Description: "No EOF.",
+		},
+		{
+			ToParse: `# HELP after_eof There is data after EOF.
+# TYPE after_eof gauge
+after_eof 1 1
+# EOF
+after_eof 1 2
+`,
+			IsOk:        false,
+			Description: "Data after EOF.",
+		},
 	}
 	for _, test := range tests {
 		t.Logf("Test:%s", test.Description)
-
-		openMetricsFile := createTemporaryOpenMetricsFile(t, test.ToParse)
-		input, err := os.Open(openMetricsFile)
-		require.NoError(t, err)
-		defer func() {
-			require.NoError(t, input.Close())
-		}()
 
 		outputDir, err := ioutil.TempDir("", "myDir")
 		require.NoError(t, err)
@@ -399,7 +423,7 @@ no_nl{type="no newline"}
 			require.NoError(t, os.RemoveAll(outputDir))
 		}()
 
-		err = backfill(test.MaxSamplesInAppender, input, outputDir)
+		err = backfill(test.MaxSamplesInAppender, []byte(test.ToParse), outputDir)
 
 		if !test.IsOk {
 			require.Error(t, err, test.Description)
