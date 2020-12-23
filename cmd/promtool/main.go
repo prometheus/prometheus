dca6954b0a69d6fe67fc84def84639abf4fcd0f6
AdaephonBen <ma18btech11011@iith.ac.in>
2020-12-24 00:22:04 +0530

    promtool: Add URL scheme when not provided (#7956)
    Signed-off-by: AdaephonBen <ma18btech11011@iith.ac.in>

--- cmd/promtool/main.go
+++ cmd/promtool/main.go
@@ -71,12 +71,12 @@ func main() {
 	queryCmdFmt := queryCmd.Flag("format", "Output format of the query.").Short('o').Default("promql").Enum("promql", "json")
 
 	queryInstantCmd := queryCmd.Command("instant", "Run instant query.")
-	queryInstantServer := queryInstantCmd.Arg("server", "Prometheus server to query.").Required().String()
+	queryInstantServer := queryInstantCmd.Arg("server", "Prometheus server to query.").Required().URL()
 	queryInstantExpr := queryInstantCmd.Arg("expr", "PromQL query expression.").Required().String()
 	queryInstantTime := queryInstantCmd.Flag("time", "Query evaluation time (RFC3339 or Unix timestamp).").String()
 
 	queryRangeCmd := queryCmd.Command("range", "Run range query.")
-	queryRangeServer := queryRangeCmd.Arg("server", "Prometheus server to query.").Required().String()
+	queryRangeServer := queryRangeCmd.Arg("server", "Prometheus server to query.").Required().URL()
 	queryRangeExpr := queryRangeCmd.Arg("expr", "PromQL query expression.").Required().String()
 	queryRangeHeaders := queryRangeCmd.Flag("header", "Extra headers to send to server.").StringMap()
 	queryRangeBegin := queryRangeCmd.Flag("start", "Query range start time (RFC3339 or Unix timestamp).").String()
@@ -437,9 +437,12 @@ func CheckMetrics() int {
 }
 
 // QueryInstant performs an instant query against a Prometheus server.
-func QueryInstant(url, query, evalTime string, p printer) int {
+func QueryInstant(url *url.URL, query, evalTime string, p printer) int {
+	if url.Scheme == "" {
+		url.Scheme = "http"
+	}
 	config := api.Config{
-		Address: url,
+		Address: url.String(),
 	}
 
 	// Create new client.
@@ -475,9 +478,12 @@ func QueryInstant(url, query, evalTime string, p printer) int {
 }
 
 // QueryRange performs a range query against a Prometheus server.
-func QueryRange(url string, headers map[string]string, query, start, end string, step time.Duration, p printer) int {
+func QueryRange(url *url.URL, headers map[string]string, query, start, end string, step time.Duration, p printer) int {
+	if url.Scheme == "" {
+		url.Scheme = "http"
+	}
 	config := api.Config{
-		Address: url,
+		Address: url.String(),
 	}
 
 	if len(headers) > 0 {
@@ -547,6 +553,9 @@ func QueryRange(url string, headers map[string]string, query, start, end string,
 
 // QuerySeries queries for a series against a Prometheus server.
 func QuerySeries(url *url.URL, matchers []string, start, end string, p printer) int {
+	if url.Scheme == "" {
+		url.Scheme = "http"
+	}
 	config := api.Config{
 		Address: url.String(),
 	}
@@ -581,6 +590,9 @@ func QuerySeries(url *url.URL, matchers []string, start, end string, p printer) 
 
 // QueryLabels queries for label values against a Prometheus server.
 func QueryLabels(url *url.URL, name string, start, end string, p printer) int {
+	if url.Scheme == "" {
+		url.Scheme = "http"
+	}
 	config := api.Config{
 		Address: url.String(),
 	}
