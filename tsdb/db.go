6c8bf54270e906c024a7dcb7e30c19a1ef0428ff
Nguyen Le Vu Long <vulongvn98@gmail.com>
2021-01-09 16:02:26 +0700

    fix: remove pre-2.21 tmp blocks on start (#8353)
    * fix: remove pre-2.21 tmp blocks on start
    
    Signed-off-by: Nguyen Le Vu Long <vulongvn98@gmail.com>
    
    * fix: commenting
    
    Signed-off-by: Nguyen Le Vu Long <vulongvn98@gmail.com>
    Signed-off-by: Dominic Yin <yindongchao@inspur.com>

--- tsdb/db.go
+++ tsdb/db.go
@@ -56,6 +56,8 @@ const (
 	// about removing those too on start to save space. Currently only blocks tmp dirs are removed.
 	tmpForDeletionBlockDirSuffix = ".tmp-for-deletion"
 	tmpForCreationBlockDirSuffix = ".tmp-for-creation"
+	// Pre-2.21 tmp dir suffix, used in clean-up functions.
+	tmpLegacy = ".tmp"
 )
 
 var (
@@ -1570,7 +1572,7 @@ func isTmpBlockDir(fi os.FileInfo) bool {
 
 	fn := fi.Name()
 	ext := filepath.Ext(fn)
-	if ext == tmpForDeletionBlockDirSuffix || ext == tmpForCreationBlockDirSuffix {
+	if ext == tmpForDeletionBlockDirSuffix || ext == tmpForCreationBlockDirSuffix || ext == tmpLegacy {
 		if _, err := ulid.ParseStrict(fn[:len(fn)-len(ext)]); err == nil {
 			return true
 		}
