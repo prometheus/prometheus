d8389a7e95160ddb3380e342e098f4faec3607c3
Thomas Jackson <jacksontj.89@gmail.com>
2021-01-14 21:30:42 -0800

    Make remote read use the new selectParams for start/end
    Signed-off-by: Thomas Jackson <jacksontj.89@gmail.com>
    
    Fixes: prometheus#4224

--- storage/remote/read.go
+++ storage/remote/read.go
@@ -160,7 +160,11 @@ func (q *querier) Select(ctx context.Context, sortSeries bool, hints *storage.Se
 	}
 
 	m, added := q.addExternalLabels(matchers)
-	query, err := ToQuery(q.mint, q.maxt, m, hints)
+	start, end := q.mint, q.maxt
+	if hints != nil {
+		start, end = hints.Start, hints.End
+	}
+	query, err := ToQuery(start, end, m, hints)
 	if err != nil {
 		return storage.ErrSeriesSet(fmt.Errorf("toQuery: %w", err))
 	}
