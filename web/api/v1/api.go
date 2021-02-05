06e53b7ecfca1350e73a6e75f789313ae01139d1
Julien Pivotto <roidelapluie@inuits.eu>
2021-02-05 12:45:44 +0100

    API: Fix global URL when external address has no port (#8359)
    * API: Fix global URL when external address has no port
    
    Signed-off-by: Julien Pivotto <roidelapluie@inuits.eu>
    
    * Update test
    
    Signed-off-by: Julien Pivotto <roidelapluie@inuits.eu>
    
    * Handle IPv6 correctly.
    
    Signed-off-by: Julien Pivotto <roidelapluie@inuits.eu>

--- web/api/v1/api.go
+++ web/api/v1/api.go
@@ -82,7 +82,7 @@ const (
 )
 
 var (
-	LocalhostRepresentations = []string{"127.0.0.1", "localhost"}
+	LocalhostRepresentations = []string{"127.0.0.1", "localhost", "::1"}
 	remoteReadQueries        = prometheus.NewGauge(prometheus.GaugeOpts{
 		Namespace: namespace,
 		Subsystem: subsystem,
@@ -750,8 +750,24 @@ type GlobalURLOptions struct {
 	Scheme        string
 }
 
+// sanitizeSplitHostPort acts like net.SplitHostPort.
+// Additionally, if there is no port in the host passed as input, we return the
+// original host, making sure that IPv6 addresses are not surrounded by square
+// brackets.
+func sanitizeSplitHostPort(input string) (string, string, error) {
+	host, port, err := net.SplitHostPort(input)
+	if err != nil && strings.HasSuffix(err.Error(), "missing port in address") {
+		var errWithPort error
+		host, _, errWithPort = net.SplitHostPort(input + ":80")
+		if errWithPort == nil {
+			err = nil
+		}
+	}
+	return host, port, err
+}
+
 func getGlobalURL(u *url.URL, opts GlobalURLOptions) (*url.URL, error) {
-	host, port, err := net.SplitHostPort(u.Host)
+	host, port, err := sanitizeSplitHostPort(u.Host)
 	if err != nil {
 		return u, err
 	}
@@ -778,11 +794,14 @@ func getGlobalURL(u *url.URL, opts GlobalURLOptions) (*url.URL, error) {
 				// externally, so we replace only the hostname by the one in the
 				// external URL. It could be the wrong hostname for the service on
 				// this port, but it's still the best possible guess.
-				host, _, err := net.SplitHostPort(opts.Host)
+				host, _, err := sanitizeSplitHostPort(opts.Host)
 				if err != nil {
 					return u, err
 				}
-				u.Host = host + ":" + port
+				u.Host = host
+				if port != "" {
+					u.Host = net.JoinHostPort(u.Host, port)
+				}
 			}
 			break
 		}
