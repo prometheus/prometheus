1774f7c52925b33ea689254135b40857df50f20a
Robert Fratto <robertfratto@gmail.com>
2021-02-19 19:57:24 -0500

    reformat
    Signed-off-by: Robert Fratto <robertfratto@gmail.com>

--- config/config.go
+++ config/config.go
@@ -636,7 +636,7 @@ func (c *RemoteWriteConfig) UnmarshalYAML(unmarshal func(interface{}) error) err
 	}
 
 	httpClientConfigEnabled := c.HTTPClientConfig.BasicAuth != nil ||
-		c.HTTPClientConfig.Authorization != nil 
+		c.HTTPClientConfig.Authorization != nil
 
 	if httpClientConfigEnabled && c.SigV4Config != nil {
 		return fmt.Errorf("at most one of basic_auth & bearer_token & bearer_token_file & sigv4 must be configured")
