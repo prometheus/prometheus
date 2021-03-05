b2d1f003edefd3fb022b566f4dc353d8a8d82f10
Rémy Léone <rleone@scaleway.com>
2021-03-05 17:38:41 +0100

    Update discovery/scaleway/scaleway.go
    Co-authored-by: Julien Pivotto <roidelapluie@gmail.com>
    Signed-off-by: Rémy Léone <rleone@scaleway.com>

--- discovery/scaleway/scaleway.go
+++ discovery/scaleway/scaleway.go
@@ -74,7 +74,7 @@ type SDConfig struct {
 	// Project: The Scaleway Project ID used to filter discovery on.
 	Project string `yaml:"project"`
 
-	// APIURL: URL of the Scaleway API to use (will default to https://api.scaleway.com).
+	// APIURL: URL of the Scaleway API to use.
 	APIURL string `yaml:"api_url"`
 	// Zone: The zone of the scrape targets.
 	// If you need to configure multiple zones use multiple scaleway_sd_configs
