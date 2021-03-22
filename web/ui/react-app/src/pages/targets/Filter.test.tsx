0d8db5295421a8a38f6e68a5f8bf974824d7dd4c
Dustin Hooten <hooten@users.noreply.github.com>
2021-03-22 04:54:12 -0600

    Feature: Add collapse/expand all button to target page for react ui (#8486)
    * Feature: Add collapse/expand all button to target page for react ui
    
    Signed-off-by: Dustin Hooten <dustinhooten@gmail.com>
    
    * update local storage key to prevent bad state
    
    Signed-off-by: Dustin Hooten <dustinhooten@gmail.com>
    
    * PR feedback
    
    Signed-off-by: Dustin Hooten <dustinhooten@gmail.com>
    
    * split big state object into smaller ones
    
    Signed-off-by: Dustin Hooten <dustinhooten@gmail.com>
    
    * fix duplication typo
    
    Signed-off-by: Dustin Hooten <dustinhooten@gmail.com>

--- web/ui/react-app/src/pages/targets/Filter.test.tsx
+++ web/ui/react-app/src/pages/targets/Filter.test.tsx
@@ -5,12 +5,23 @@ import Filter, { FilterData, FilterProps } from './Filter';
 import sinon, { SinonSpy } from 'sinon';
 
 describe('Filter', () => {
-  const initialState: FilterData = { showHealthy: true, showUnhealthy: true };
+  const initialExpanded = {
+    scrapePool1: true,
+    scrapePool2: true,
+  };
+  let setExpaned: SinonSpy;
+  const initialState: FilterData = {
+    showHealthy: true,
+    showUnhealthy: true,
+  };
   let setFilter: SinonSpy;
   let filterWrapper: ShallowWrapper<FilterProps, Readonly<{}>, Component<{}, {}, Component>>;
   beforeEach(() => {
     setFilter = sinon.spy();
-    filterWrapper = shallow(<Filter filter={initialState} setFilter={setFilter} />);
+    setExpaned = sinon.spy();
+    filterWrapper = shallow(
+      <Filter filter={initialState} setFilter={setFilter} expanded={initialExpanded} setExpanded={setExpaned} />
+    );
   });
 
   it('renders a button group', () => {
@@ -29,6 +40,12 @@ describe('Filter', () => {
     expect(btn.prop('color')).toBe('primary');
   });
 
+  it('renders an expansion filter button that is inactive', () => {
+    const btn = filterWrapper.find(Button).filterWhere((btn): boolean => btn.hasClass('expansion'));
+    expect(btn.prop('active')).toBe(false);
+    expect(btn.prop('color')).toBe('primary');
+  });
+
   it('renders an all filter button which shows all targets', () => {
     const btn = filterWrapper.find(Button).filterWhere((btn): boolean => btn.hasClass('all'));
     btn.simulate('click');
@@ -40,6 +57,46 @@ describe('Filter', () => {
     const btn = filterWrapper.find(Button).filterWhere((btn): boolean => btn.hasClass('unhealthy'));
     btn.simulate('click');
     expect(setFilter.calledOnce).toBe(true);
-    expect(setFilter.getCall(0).args[0]).toEqual({ showHealthy: false, showUnhealthy: true });
+    expect(setFilter.getCall(0).args[0]).toEqual({
+      showHealthy: false,
+      showUnhealthy: true,
+    });
+  });
+
+  describe('Expansion filter', () => {
+    [
+      {
+        name: 'expanded => collapsed',
+        initial: initialExpanded,
+        final: { scrapePool1: false, scrapePool2: false },
+        text: 'Collapse All',
+      },
+      {
+        name: 'collapsed => expanded',
+        initial: { scrapePool1: false, scrapePool2: false },
+        final: initialExpanded,
+        text: 'Expand All',
+      },
+      {
+        name: 'some expanded => expanded',
+        initial: { scrapePool1: true, scrapePool2: false },
+        final: initialExpanded,
+        text: 'Expand All',
+      },
+    ].forEach(({ name, text, initial, final }) => {
+      it(`filters targets ${name}`, (): void => {
+        const filter = { ...initialState };
+        const filterCallback = sinon.spy();
+        const expandedCallback = sinon.spy();
+        const filterW = shallow(
+          <Filter filter={filter} setFilter={filterCallback} expanded={initial} setExpanded={expandedCallback} />
+        );
+        const btn = filterW.find(Button).filterWhere((btn): boolean => btn.hasClass('expansion'));
+        expect(btn.children().text()).toEqual(text);
+        btn.simulate('click');
+        expect(expandedCallback.calledOnce).toBe(true);
+        expect(expandedCallback.getCall(0).args[0]).toEqual(final);
+      });
+    });
   });
 });
--- web/ui/react-app/src/pages/targets/Filter.tsx
+++ web/ui/react-app/src/pages/targets/Filter.tsx
@@ -7,13 +7,28 @@ export interface FilterData {
   showUnhealthy: boolean;
 }
 
+export interface Expanded {
+  [scrapePool: string]: boolean;
+}
+
 export interface FilterProps {
   filter: FilterData;
   setFilter: Dispatch<SetStateAction<FilterData>>;
+  expanded: Expanded;
+  setExpanded: Dispatch<SetStateAction<Expanded>>;
 }
 
-const Filter: FC<FilterProps> = ({ filter, setFilter }) => {
+const Filter: FC<FilterProps> = ({ filter, setFilter, expanded, setExpanded }) => {
   const { showHealthy } = filter;
+  const allExpanded = Object.values(expanded).every((v: boolean): boolean => v);
+  const mapExpansion = (next: boolean): Expanded =>
+    Object.keys(expanded).reduce(
+      (acc: { [scrapePool: string]: boolean }, scrapePool: string) => ({
+        ...acc,
+        [scrapePool]: next,
+      }),
+      {}
+    );
   const btnProps = {
     all: {
       active: showHealthy,
@@ -27,11 +42,18 @@ const Filter: FC<FilterProps> = ({ filter, setFilter }) => {
       color: 'primary',
       onClick: (): void => setFilter({ ...filter, showHealthy: false }),
     },
+    expansionState: {
+      active: false,
+      className: `expansion ${styles.btn}`,
+      color: 'primary',
+      onClick: (): void => setExpanded(mapExpansion(!allExpanded)),
+    },
   };
   return (
     <ButtonGroup>
       <Button {...btnProps.all}>All</Button>
       <Button {...btnProps.unhealthy}>Unhealthy</Button>
+      <Button {...btnProps.expansionState}>{allExpanded ? 'Collapse All' : 'Expand All'}</Button>
     </ButtonGroup>
   );
 };
