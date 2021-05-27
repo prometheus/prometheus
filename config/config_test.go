ed396a074e6e795a0cc63fdf841ac5d722b20c9b
TJ Hoplock <t.hoplock@gmail.com>
2021-05-27 16:17:02 -0400

    fix config tests after introducing Linode SD
    Signed-off-by: TJ Hoplock <t.hoplock@gmail.com>

--- config/config_test.go
+++ config/config_test.go
@@ -853,6 +853,7 @@ var expectedConf = &Config{
 						FollowRedirects: true,
 					},
 					Port:            80,
+					TagSeparator:    linode.DefaultSDConfig.TagSeparator,
 					RefreshInterval: model.Duration(60 * time.Second),
 				},
 			},
@@ -941,7 +942,7 @@ func TestElideSecrets(t *testing.T) {
 	yamlConfig := string(config)
 
 	matches := secretRe.FindAllStringIndex(yamlConfig, -1)
-	require.Equal(t, 14, len(matches), "wrong number of secret matches found")
+	require.Equal(t, 15, len(matches), "wrong number of secret matches found")
 	require.NotContains(t, yamlConfig, "mysecret",
 		"yaml marshal reveals authentication credentials.")
 }
