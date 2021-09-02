27bd8fef4041e5746b48a403614e85eefd423f02
Augustin Husson <husson.augustin@gmail.com>
2021-09-02 10:29:20 +0200

    fix condition in withStartingIndicator
    Signed-off-by: Augustin Husson <husson.augustin@gmail.com>

--- web/ui/react-app/src/components/withStartingIndicator.tsx
+++ web/ui/react-app/src/components/withStartingIndicator.tsx
@@ -23,21 +23,21 @@ export const StartingContent: FC<StartingContentProps> = ({ status, isUnexpected
     <div className="text-center m-3">
       <div className="m-4">
         <h2>Starting up...</h2>
-        {/* {status?.max! > 0 ? (
+        {status && status.max > 0 ? (
           <div>
             <p>
               Replaying WAL ({status?.current}/{status?.max})
             </p>
             <Progress
               animated
-              value={status?.current! - status?.min! + 1}
-              min={status?.min}
-              max={status?.max! - status?.min! + 1}
-              color={status?.max === status?.current ? 'success' : undefined}
+              value={status.current - status.min + 1}
+              min={status.min}
+              max={status.max - status.min + 1}
+              color={status.max === status.current ? 'success' : undefined}
               style={{ width: '10%', margin: 'auto' }}
             />
           </div>
-        ) : null} */}
+        ) : null}
       </div>
     </div>
   );
