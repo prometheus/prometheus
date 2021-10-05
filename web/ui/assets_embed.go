73dc69e84799fffe161fa415fc6690948ff5b068
Jan Fajerski <jfajersk@redhat.com>
2021-10-05 13:46:26 +0200

    we/ui/assets_embed.go: add gzip layer
    This adds a PoC gzip layer to the embed fs in order to reduce the binary
    size. This requires an additional asset compression  step before building prometheus:
    find web/ui/templates web/ui/static -type f -exec gzip {} \;
    After the build completed run the following to reset the repository:
    find web/ui/static web/ui/templates -name "*gz" -exec rm {} \; && g restore web/ui/static web/ui/templates
    
    Signed-off-by: Jan Fajerski <jfajersk@redhat.com>

--- web/ui/assets_embed.go
+++ web/ui/assets_embed.go
@@ -17,11 +17,102 @@
 package ui
 
 import (
+	"compress/gzip"
 	"embed"
+	"io"
+	"io/fs"
 	"net/http"
+	"time"
+)
+
+const (
+	zipSuffix = ".gz"
 )
 
 //go:embed static templates
 var embedFS embed.FS
 
-var Assets = http.FS(embedFS)
+var Assets = http.FS(FileSystem{embedFS})
+
+type FileSystem struct {
+	embed embed.FS
+}
+
+func (compressed FileSystem) Open(path string) (fs.File, error) {
+	// if we have the file in our embed FS, just return that (could be a
+	// dir)
+	var f fs.File
+	if f, err := compressed.embed.Open(path); err == nil {
+		return f, nil
+	}
+	// try opening path with .gz suffix
+	f, err := compressed.embed.Open(path + zipSuffix)
+	if err != nil {
+		return f, err
+	}
+	// read the (decompressed) content into a buffer
+	gr, err := gzip.NewReader(f)
+	defer gr.Close()
+	if err != nil {
+		return f, err
+	}
+	c, err := io.ReadAll(gr)
+	if err != nil {
+		return f, err
+	}
+	// and wrap everything in ui.File
+	return &File{file: f, content: c}, nil
+}
+
+type File struct {
+	// the underlying file
+	file fs.File
+	// the decrompressed content, needed to return an accurate size
+	content []byte
+	// offset for calls to Read()
+	offset int
+}
+
+func (f File) Stat() (fs.FileInfo, error) {
+	stat, err := f.file.Stat()
+	if err != nil {
+		return stat, err
+	}
+	return FileInfo{stat, int64(len(f.content))}, nil
+}
+
+func (f *File) Read(buf []byte) (n int, err error) {
+	if len(buf) > len(f.content)-f.offset {
+		buf = buf[0:len(f.content[f.offset:])]
+	}
+	n = copy(buf, f.content[f.offset:])
+	if n == len(f.content)-f.offset {
+		return n, io.EOF
+	}
+	f.offset += n
+	return
+}
+
+func (f File) Close() error {
+	return f.file.Close()
+}
+
+type FileInfo struct {
+	fi         fs.FileInfo
+	actualSize int64
+}
+
+func (fi FileInfo) Name() string {
+	name := fi.fi.Name()
+	return name[:len(name)-len(zipSuffix)]
+}
+
+func (fi FileInfo) Size() int64 { return fi.actualSize }
+
+func (fi FileInfo) Mode() fs.FileMode { return fi.fi.Mode() }
+
+func (fi FileInfo) ModTime() time.Time { return fi.fi.ModTime() }
+
+func (fi FileInfo) IsDir() bool { return fi.fi.IsDir() }
+
+func (fi FileInfo) Sys() interface{} { return nil }
