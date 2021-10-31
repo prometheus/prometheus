eceb401c6adc43781cc5c0dc500746e75c5b4056
mtfoley <mtfoley.mae@gmail.com>
2021-10-30 22:54:57 -0400

    feat: add search to metrics explorer
    Signed-off-by: mtfoley <mtfoley.mae@gmail.com>

---
+++ web/ui/react-app/src/pages/graph/MetricsExplorer.test.tsx
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import { mount, ReactWrapper} from 'enzyme';
+import MetricsExplorer from './MetricsExplorer';
+import { Input } from 'reactstrap';
+
+describe('MetricsExplorer', () => {
+  const spyInsertAtCursor = jest.fn().mockImplementation((value:string) => {});
+  const metricsExplorerProps = {
+    show: true,
+    updateShow:(show: boolean): void => {},
+    metrics:['go_test_1','prometheus_test_1'],
+    insertAtCursor: spyInsertAtCursor,
+  };
+
+  let metricsExplorer: ReactWrapper;
+  beforeEach(() => {
+    metricsExplorer = mount(<MetricsExplorer {...metricsExplorerProps} />);
+  });
+
+  it('renders an Input[type=text]', () => {
+    const input = metricsExplorer.find(Input);
+    expect(input.prop('type')).toEqual('text');
+  });
+
+  it('lists all metrics in props', () => {
+    const metrics = metricsExplorer.find('.metric');
+    expect(metrics).toHaveLength(metricsExplorerProps.metrics.length);
+  });
+
+  it('filters metrics with search', () => {
+    const input = metricsExplorer.find(Input);
+    input.simulate('change', { target: { value: 'go' }});
+    const metrics = metricsExplorer.find('.metric');
+    expect(metrics).toHaveLength(1);
+  });
+
+  it('handles click on metric', () => {
+    const metric = metricsExplorer.find('.metric').at(0);
+    metric.simulate('click');
+    expect(metricsExplorerProps.insertAtCursor).toHaveBeenCalled();
+  });
+
+/*  it('executes the query when clicking the execute button', () => {
+    const spyExecuteQuery = jest.fn();
+    const props = { ...expressionInputProps, executeQuery: spyExecuteQuery };
+    const wrapper = mount(<ExpressionInput {...props} />);
+    const btn = wrapper.find(Button).filterWhere((btn) => btn.hasClass('execute-btn'));
+    btn.simulate('click');
+    expect(spyExecuteQuery).toHaveBeenCalledTimes(1);
+  });  */
+});
--- web/ui/react-app/src/pages/graph/MetricsExplorer.tsx
+++ web/ui/react-app/src/pages/graph/MetricsExplorer.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Modal, ModalBody, ModalHeader } from 'reactstrap';
+import { Modal, ModalBody, ModalHeader, Input } from 'reactstrap';
 
 interface Props {
   show: boolean;
@@ -7,27 +7,42 @@ interface Props {
   metrics: string[];
   insertAtCursor(value: string): void;
 }
-
-class MetricsExplorer extends Component<Props> {
+type MetricsExplorerState = {
+  searchTerm: string;
+};
+class MetricsExplorer extends Component<Props, MetricsExplorerState> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { searchTerm: '' };
+  }
+  handleSearchTerm = (event: any): void => {
+    this.setState({ searchTerm: event.target.value });
+  };
   handleMetricClick = (query: string): void => {
     this.props.insertAtCursor(query);
     this.props.updateShow(false);
+    this.setState({ searchTerm: '' });
   };
 
   toggle = (): void => {
     this.props.updateShow(!this.props.show);
   };
-
   render(): JSX.Element {
     return (
-      <Modal isOpen={this.props.show} toggle={this.toggle} className="metrics-explorer">
+      <Modal isOpen={this.props.show} toggle={this.toggle} className="metrics-explorer" scrollable>
         <ModalHeader toggle={this.toggle}>Metrics Explorer</ModalHeader>
         <ModalBody>
-          {this.props.metrics.map((metric) => (
-            <p key={metric} className="metric" onClick={this.handleMetricClick.bind(this, metric)}>
-              {metric}
-            </p>
-          ))}
+          <Input placeholder="Search" value={this.state.searchTerm} type="text" onChange={this.handleSearchTerm} />
+          {this.props.metrics
+            .filter((metric) => {
+              if (this.state.searchTerm === '') return true;
+              return metric.toLowerCase().includes(this.state.searchTerm.toLowerCase());
+            })
+            .map((metric) => (
+              <p key={metric} className="metric" onClick={this.handleMetricClick.bind(this, metric)}>
+                {metric}
+              </p>
+            ))}
         </ModalBody>
       </Modal>
     );
