45438cc0e46d4b86cdbba506a76caa715f7e4716
Matej Gera <matejgera@gmail.com>
2021-11-16 10:47:47 +0100

    Add tests for tracing manager
    Signed-off-by: Matej Gera <matejgera@gmail.com>

---
+++ tracing/tracing_test.go
@@ -0,0 +1,63 @@
+// Copyright 2021 The Prometheus Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package tracing
+
+import (
+	"testing"
+
+	"github.com/go-kit/log"
+	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/otel"
+
+	"github.com/prometheus/prometheus/config"
+)
+
+func TestInstallingNewTracerProvider(t *testing.T) {
+	tpBefore := otel.GetTracerProvider()
+
+	m := NewManager(log.NewNopLogger())
+	cfg := config.Config{
+		TracingConfig: config.TracingConfig{
+			ServiceName: "test",
+		},
+	}
+
+	m.ApplyConfig(&cfg)
+	require.NotEqual(t, tpBefore, otel.GetTracerProvider())
+}
+
+func TestReinstallingTracerProvider(t *testing.T) {
+	m := NewManager(log.NewNopLogger())
+	cfg := config.Config{
+		TracingConfig: config.TracingConfig{
+			ServiceName: "test",
+		},
+	}
+
+	m.ApplyConfig(&cfg)
+	tpFirstConfig := otel.GetTracerProvider()
+
+	// Trying to apply the same config should not reinstall provider.
+	m.ApplyConfig(&cfg)
+	require.Equal(t, tpFirstConfig, otel.GetTracerProvider())
+
+	cfg2 := config.Config{
+		TracingConfig: config.TracingConfig{
+			ServiceName: "test-2",
+		},
+	}
+	m.ApplyConfig(&cfg2)
+
+	require.NotEqual(t, tpFirstConfig, otel.GetTracerProvider())
+}
