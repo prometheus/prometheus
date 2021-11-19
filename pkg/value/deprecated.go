11b7537a87c5c27cba4ab91419d3cb542408e4d4
Peter Štibraný <pstibrany@gmail.com>
2021-11-19 15:50:25 +0100

    Use gofumpt -extra to format files.
    Signed-off-by: Peter Štibraný <pstibrany@gmail.com>

--- pkg/value/deprecated.go
+++ pkg/value/deprecated.go
@@ -16,7 +16,9 @@ package value
 
 import "github.com/prometheus/prometheus/model/value"
 
-const NormalNaN = value.NormalNaN
-const StateNan = value.StaleNaN
+const (
+	NormalNaN = value.NormalNaN
+	StateNan  = value.StaleNaN
+)
 
 var IsStaleNaN = value.IsStaleNaN
