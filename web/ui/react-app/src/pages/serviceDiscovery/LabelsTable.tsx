9d14e3b86d3cf6d2fd130cd3c979dd39fcb572bd
Augustin Husson <husson.augustin@gmail.com>
2022-01-06 14:47:15 +0100

    rework service discovery page
    Signed-off-by: Augustin Husson <husson.augustin@gmail.com>

--- web/ui/react-app/src/pages/serviceDiscovery/LabelsTable.tsx
+++ web/ui/react-app/src/pages/serviceDiscovery/LabelsTable.tsx
@@ -1,12 +1,15 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { Badge, Table } from 'reactstrap';
 import { TargetLabels } from './Services';
 import { ToggleMoreLess } from '../../components/ToggleMoreLess';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 interface LabelProps {
   value: TargetLabels[];
   name: string;
 }
+
+const initialNumberOfTargetsDisplayed = 50;
 
 const formatLabels = (labels: Record<string, string> | string) => {
   return Object.entries(labels).map(([key, value]) => {
@@ -22,6 +25,24 @@ const formatLabels = (labels: Record<string, string> | string) => {
 
 export const LabelsTable: FC<LabelProps> = ({ value, name }) => {
   const [showMore, setShowMore] = useState(false);
+  const [items, setItems] = useState<TargetLabels[]>(value.slice(0, 50));
+  const [index, setIndex] = useState<number>(initialNumberOfTargetsDisplayed);
+  const [hasMore, setHasMore] = useState<boolean>(value.length > initialNumberOfTargetsDisplayed);
+
+  useEffect(() => {
+    setItems(value.slice(0, initialNumberOfTargetsDisplayed));
+    setHasMore(value.length > initialNumberOfTargetsDisplayed);
+  }, [value]);
+
+  const fetchMoreData = () => {
+    if (items.length === value.length) {
+      setHasMore(false);
+    } else {
+      const newIndex = index + initialNumberOfTargetsDisplayed;
+      setIndex(newIndex);
+      setItems(value.slice(0, newIndex));
+    }
+  };
 
   return (
     <>
@@ -36,28 +57,36 @@ export const LabelsTable: FC<LabelProps> = ({ value, name }) => {
         </ToggleMoreLess>
       </div>
       {showMore ? (
-        <Table size="sm" bordered hover striped>
-          <thead>
-            <tr>
-              <th>Discovered Labels</th>
-              <th>Target Labels</th>
-            </tr>
-          </thead>
-          <tbody>
-            {value.map((_, i) => {
-              return (
-                <tr key={i}>
-                  <td>{formatLabels(value[i].discoveredLabels)}</td>
-                  {value[i].isDropped ? (
-                    <td style={{ fontWeight: 'bold' }}>Dropped</td>
-                  ) : (
-                    <td>{formatLabels(value[i].labels)}</td>
-                  )}
-                </tr>
-              );
-            })}
-          </tbody>
-        </Table>
+        <InfiniteScroll
+          next={fetchMoreData}
+          hasMore={hasMore}
+          loader={<h4>loading...</h4>}
+          dataLength={items.length}
+          height={items.length > 25 ? '75vh' : ''}
+        >
+          <Table size="sm" bordered hover striped>
+            <thead>
+              <tr>
+                <th>Discovered Labels</th>
+                <th>Target Labels</th>
+              </tr>
+            </thead>
+            <tbody>
+              {items.map((_, i) => {
+                return (
+                  <tr key={i}>
+                    <td>{formatLabels(items[i].discoveredLabels)}</td>
+                    {items[i].isDropped ? (
+                      <td style={{ fontWeight: 'bold' }}>Dropped</td>
+                    ) : (
+                      <td>{formatLabels(items[i].labels)}</td>
+                    )}
+                  </tr>
+                );
+              })}
+            </tbody>
+          </Table>
+        </InfiniteScroll>
       ) : null}
     </>
   );
