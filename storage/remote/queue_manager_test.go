d5538f584f5d8c0523d8955c5a8cda082dc12b5a
Chris Marchbanks <csmarchbanks@gmail.com>
2022-02-10 10:08:55 -0700

    Fix deadlock when stopping a shard
    If a queue is stopped and one of its shards happens to hit the
    batch_send_deadline at the same time a deadlock can occur where stop
    holds the mutex and will not release it until the send is finished, but
    the send needs the mutex to retrieve the most recent batch. This is
    fixed by using a second mutex just for writing.
    
    In addition, the test I wrote exposed a case where during shutdown a
    batch could be sent twice due to concurrent calls to queue.Batch() and
    queue.FlushAndShutdown(). Protect these with a mutex as well.
    
    Signed-off-by: Chris Marchbanks <csmarchbanks@gmail.com>

--- storage/remote/queue_manager_test.go
+++ storage/remote/queue_manager_test.go
@@ -382,6 +382,44 @@ func TestReshardRaceWithStop(t *testing.T) {
 	<-exitCh
 }
 
+func TestReshardPartialBatch(t *testing.T) {
+	samples, series := createTimeseries(1, 10)
+
+	c := NewTestBlockedWriteClient()
+
+	cfg := config.DefaultQueueConfig
+	mcfg := config.DefaultMetadataConfig
+	cfg.MaxShards = 1
+	batchSendDeadline := time.Millisecond
+	flushDeadline := 10 * time.Millisecond
+	cfg.BatchSendDeadline = model.Duration(batchSendDeadline)
+
+	metrics := newQueueManagerMetrics(nil, "", "")
+	m := NewQueueManager(metrics, nil, nil, nil, t.TempDir(), newEWMARate(ewmaWeight, shardUpdateDuration), cfg, mcfg, nil, nil, c, flushDeadline, newPool(), newHighestTimestampMetric(), nil, false)
+	m.StoreSeries(series, 0)
+
+	m.Start()
+
+	for i := 0; i < 100; i++ {
+		done := make(chan struct{})
+		go func() {
+			m.Append(samples)
+			time.Sleep(batchSendDeadline)
+			m.shards.stop()
+			m.shards.start(1)
+			done <- struct{}{}
+		}()
+		select {
+		case <-done:
+		case <-time.After(2 * time.Second):
+			t.Error("Deadlock between sending and stopping detected")
+			t.FailNow()
+		}
+	}
+	// We can only call stop if there was not a deadlock.
+	m.Stop()
+}
+
 func TestReleaseNoninternedString(t *testing.T) {
 	cfg := config.DefaultQueueConfig
 	mcfg := config.DefaultMetadataConfig
