dca7f50be80c8c1cd2cae84b1aaafc1cc9cccef0
Łukasz Mierzwa <l.mierzwa@gmail.com>
2022-03-03 18:23:29 +0000

    Upgrade create-react-app to v5
    Some other dependencies needs to be upgraded as well, plus some typescript errors fixed.
    
    Signed-off-by: Łukasz Mierzwa <l.mierzwa@gmail.com>

--- web/ui/react-app/src/Navbar.test.tsx
+++ web/ui/react-app/src/Navbar.test.tsx
@@ -5,7 +5,7 @@ import { NavItem, NavLink } from 'reactstrap';
 
 describe('Navbar should contain console Link', () => {
   it('with non-empty consoleslink', () => {
-    const app = shallow(<Navigation pathPrefix="/path/prefix" consolesLink="/path/consoles" />);
+    const app = shallow(<Navigation consolesLink="/path/consoles" agentMode={false} />);
     expect(
       app.contains(
         <NavItem>
@@ -18,7 +18,7 @@ describe('Navbar should contain console Link', () => {
 
 describe('Navbar should not contain consoles link', () => {
   it('with empty string in consolesLink', () => {
-    const app = shallow(<Navigation pathPrefix="/path/prefix" consolesLink={null} />);
+    const app = shallow(<Navigation consolesLink={null} agentMode={false} />);
     expect(
       app.contains(
         <NavItem>
