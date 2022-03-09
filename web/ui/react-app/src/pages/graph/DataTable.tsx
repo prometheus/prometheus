prometheus/prometheus

commit 16e610ee8fd5642ba8e747390d38aabe6975f956

Add a tooltip for unix times (ISO strings) (#10376)
* Add a tooltip for unix times (ISO strings)

Signed-off-by: Ondrej Kokes <ondrej.kokes@gmail.com>

* Leverage useLocalTime to adjust ISO string tooltips

Signed-off-by: Ondrej Kokes <ondrej.kokes@gmail.com>

* revert pre styling removal

Signed-off-by: Ondrej Kokes <ondrej.kokes@gmail.com>

web/ui/react-app/src/pages/graph/DataTable.tsx
@@ -5,6 +5,8 @@ import { Alert, Table } from 'reactstrap';
 import SeriesName from './SeriesName';
 import { Metric } from '../../types/types';
 
+import moment from 'moment';
+
 export interface QueryResult {
   data:
     | null
@@ -24,6 +26,7 @@ export interface QueryResult {
         resultType: 'string';
         result: string;
       };
+  useLocalTime: boolean;
 }
 
 interface InstantSample {
@@ -47,7 +50,7 @@ const limitSeries = <S extends InstantSample | RangeSamples>(series: S[]): S[] =
   return series;
 };
 
-const DataTable: FC<QueryResult> = ({ data }) => {
+const DataTable: FC<QueryResult> = ({ data, useLocalTime }) => {
   if (data === null) {
     return <Alert color="light">No data queried yet</Alert>;
   }
@@ -76,17 +79,21 @@ const DataTable: FC<QueryResult> = ({ data }) => {
       break;
     case 'matrix':
       rows = (limitSeries(data.result) as RangeSamples[]).map((s, index) => {
-        const valueText = s.values
-          .map((v) => {
-            return v[1] + ' @' + v[0];
-          })
-          .join('\n');
+        const valuesAndTimes = s.values.map((v) => {
+          const printedDatetime = moment.unix(v[0]).toISOString(useLocalTime);
+          return (
+            <React.Fragment>
+              {v[1]} @{<span title={printedDatetime}>{v[0]}</span>}
+              <br />
+            </React.Fragment>
+          );
+        });
         return (
           <tr style={{ whiteSpace: 'pre' }} key={index}>
             <td>
               <SeriesName labels={s.metric} format={doFormat} />
             </td>
-            <td>{valueText}</td>
+            <td>{valuesAndTimes}</td>
           </tr>
         );
       });

web/ui/react-app/src/pages/graph/Panel.tsx
@@ -325,7 +325,7 @@ class Panel extends Component<PanelProps, PanelState> {
                         onChangeTime={this.handleChangeEndTime}
                       />
                     </div>
-                    <DataTable data={this.state.data} />
+                    <DataTable data={this.state.data} useLocalTime={this.props.useLocalTime} />
                   </>
                 )}
               </TabPane>

commit 90c3ce5a2cf86d649da270f95731d96ec2960112

Fix DataTable tests and missing value key warning
Fixes issues introduced in https://github.com/prometheus/prometheus/pull/10376

Signed-off-by: Julius Volz <julius.volz@gmail.com>

web/ui/react-app/src/pages/graph/DataTable.tsx
@@ -78,18 +78,18 @@ const DataTable: FC<QueryResult> = ({ data, useLocalTime }) => {
       limited = rows.length !== data.result.length;
       break;
     case 'matrix':
-      rows = (limitSeries(data.result) as RangeSamples[]).map((s, index) => {
-        const valuesAndTimes = s.values.map((v) => {
+      rows = (limitSeries(data.result) as RangeSamples[]).map((s, seriesIdx) => {
+        const valuesAndTimes = s.values.map((v, valIdx) => {
           const printedDatetime = moment.unix(v[0]).toISOString(useLocalTime);
           return (
-            <React.Fragment>
+            <React.Fragment key={valIdx}>
               {v[1]} @{<span title={printedDatetime}>{v[0]}</span>}
               <br />
             </React.Fragment>
           );
         });
         return (
-          <tr style={{ whiteSpace: 'pre' }} key={index}>
+          <tr style={{ whiteSpace: 'pre' }} key={seriesIdx}>
             <td>
               <SeriesName labels={s.metric} format={doFormat} />
             </td>

web/ui/react-app/src/pages/graph/DataTable.test.tsx
@@ -233,26 +233,9 @@ describe('DataTable', () => {
       const rows = table.find('tr');
       expect(table.find('tr')).toHaveLength(3);
       const row = rows.at(0);
-      expect(row.text()).toEqual(`<SeriesName />9 @1572097950.93
-10 @1572097965.931
-11 @1572097980.929
-12 @1572097995.931
-13 @1572098010.932
-14 @1572098025.933
-15 @1572098040.93
-16 @1572098055.93
-17 @1572098070.93
-18 @1572098085.936
-19 @1572098100.936
-20 @1572098115.933
-21 @1572098130.932
-22 @1572098145.932
-23 @1572098160.933
-24 @1572098175.934
-25 @1572098190.937
-26 @1572098205.934
-27 @1572098220.933
-28 @1572098235.934`);
+      expect(row.text()).toEqual(
+        `<SeriesName />9 @1572097950.9310 @1572097965.93111 @1572097980.92912 @1572097995.93113 @1572098010.93214 @1572098025.93315 @1572098040.9316 @1572098055.9317 @1572098070.9318 @1572098085.93619 @1572098100.93620 @1572098115.93321 @1572098130.93222 @1572098145.93223 @1572098160.93324 @1572098175.93425 @1572098190.93726 @1572098205.93427 @1572098220.93328 @1572098235.934`
+      );
     });
   });
 