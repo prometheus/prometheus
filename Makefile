957084dc9c5174746371294e58d99a1b67e511ac
Augustin Husson <husson.augustin@gmail.com>
2022-04-01 14:04:59 +0200

    use jest to run the lezer-promql test
    Signed-off-by: Augustin Husson <husson.augustin@gmail.com>

--- Makefile
+++ Makefile
@@ -43,7 +43,7 @@ ui-build-module:
 
 .PHONY: ui-test
 ui-test:
-	cd $(UI_PATH) && CI=true npm run test:coverage
+	cd $(UI_PATH) && CI=true npm run test
 
 .PHONY: ui-lint
 ui-lint:
--- web/ui/module/lezer-promql/test/promql.test.js
+++ web/ui/module/lezer-promql/test/promql.test.js
@@ -9,7 +9,7 @@ let caseDir = path.dirname(fileURLToPath(import.meta.url))
 for (const file of fs.readdirSync(caseDir)) {
     if (!/\.txt$/.test(file)) continue;
 
-    const name = /^[^\.]*/.exec(file)[0];
+    const name = /^[^.]*/.exec(file)[0];
     describe(name, () => {
         for (const { name, run } of fileTests(fs.readFileSync(path.join(caseDir, file), 'utf8'), file)) it(name, () => run(parser));
     });
