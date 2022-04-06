0c47642ea464ff7efd82c09148393dd1399b7b6a
Julien Pivotto <roidelapluie@inuits.eu>
2022-04-06 22:11:38 +0200

    Fix assets tarball
    The tarball should be compressed and only use uncompressed assets. In
    that way, uit can be used by downstream distros easily. they can either
    download the assets and serve it as is or use the compress script to
    have it in the binary.
    
    Signed-off-by: Julien Pivotto <roidelapluie@inuits.eu>

--- scripts/package_assets.sh
+++ scripts/package_assets.sh
@@ -5,4 +5,4 @@
 set -euo pipefail
 
 cd web/ui
-find static -type f -name '*.gz' -print0 | xargs -0 tar cf static.tar.gz
+find static -type f -not -name '*.gz' -print0 | xargs -0 tar czf static.tar.gz
