1186974715c58edc3317221bdd7bf6d510bceb59
Julien Pivotto <roidelapluie@inuits.eu>
2022-04-12 19:23:02 +0200

    Publish UI release artefact in our github releases (#10577)
    This will make UI static files part of the release artefacts, for
    consumption by downstream distributions.
    
    Signed-off-by: Julien Pivotto <roidelapluie@inuits.eu>

--- scripts/package_assets.sh
+++ scripts/package_assets.sh
@@ -4,5 +4,7 @@
 
 set -euo pipefail
 
+version="$(< VERSION)"
+mkdir -p .tarballs
 cd web/ui
-find static -type f -not -name '*.gz' -print0 | xargs -0 tar czf static.tar.gz
+find static -type f -not -name '*.gz' -print0 | xargs -0 tar czf ../../.tarballs/prometheus-web-ui-${version}.tar.gz
