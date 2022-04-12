5db6963dea6c03cfd4c7e3ef102877af69c6f77c
Jesus Vazquez <jesusvazquez@users.noreply.github.com>
2022-04-12 12:15:22 +0200

    Fix linting errors (#198)

--- template/template.go
+++ template/template.go
@@ -175,7 +175,7 @@ func NewTemplateExpander(
 				return html_template.HTML(text)
 			},
 			"match":     regexp.MatchString,
-			"title":     strings.Title,
+			"title":     strings.Title, //nolint:staticcheck
 			"toUpper":   strings.ToUpper,
 			"toLower":   strings.ToLower,
 			"graphLink": strutil.GraphLinkForExpression,
