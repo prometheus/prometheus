d8623d75df66e91fac5dbcaccf325433a1b41d8b
Matthieu MOREL <mmorel-35@users.noreply.github.com>
2022-04-29 22:05:45 +0200

    gofumpt Signed-off-by: Matthieu MOREL <mmorel-35@users.noreply.github.com>

--- cmd/promtool/backfill.go
+++ cmd/promtool/backfill.go
@@ -220,7 +220,7 @@ func backfill(maxSamplesInAppender int, input []byte, outputDir string, humanRea
 	maxt, mint, err := getMinAndMaxTimestamps(p)
 	if err != nil {
 		return fmt.Errorf("getting min and max timestamp: %w", err)
-	}	
+	}
 	if err = createBlocks(input, mint, maxt, int64(maxBlockDuration/time.Millisecond), maxSamplesInAppender, outputDir, humanReadable, quiet); err != nil {
 		return fmt.Errorf("block creation: %w", err)
 	}
