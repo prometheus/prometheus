02f3297719a770bc9a1b5335f1e7657238ba7c3a
Julien Pivotto <roidelapluie@o11y.eu>
2022-07-01 14:23:23 +0200

    Split notifier select in 2 to ensure newer targets are used. (#10948)
    * Split notifier select in 2 to ensure newer targets are used.
    
    Signed-off-by: Julien Pivotto <roidelapluie@o11y.eu>

--- notifier/notifier.go
+++ notifier/notifier.go
@@ -302,12 +302,22 @@ func (n *Manager) nextBatch() []*Alert {
 // Run dispatches notifications continuously.
 func (n *Manager) Run(tsets <-chan map[string][]*targetgroup.Group) {
 	for {
+		// The select is split in two parts, such as we will first try to read
+		// new alertmanager targets if they are available, before sending new
+		// alerts.
 		select {
 		case <-n.ctx.Done():
 			return
 		case ts := <-tsets:
 			n.reload(ts)
-		case <-n.more:
+		default:
+			select {
+			case <-n.ctx.Done():
+				return
+			case ts := <-tsets:
+				n.reload(ts)
+			case <-n.more:
+			}
 		}
 		alerts := n.nextBatch()
 
