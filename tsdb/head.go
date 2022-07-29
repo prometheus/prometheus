cb8582637aaa38a72ab1857c4d2f97715a256ca5
Levi Harrison <git@leviharrison.dev>
2022-07-29 03:48:53 -0500

    Implement rollback for histograms (#11071)
    Signed-off-by: Levi Harrison <git@leviharrison.dev>

--- tsdb/head.go
+++ tsdb/head.go
@@ -76,6 +76,7 @@ type Head struct {
 	logger          log.Logger
 	appendPool      sync.Pool
 	exemplarsPool   sync.Pool
+	histogramsPool  sync.Pool
 	seriesPool      sync.Pool
 	bytesPool       sync.Pool
 	memChunkPool    sync.Pool
