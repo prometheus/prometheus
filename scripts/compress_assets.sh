51623cd90727879213ae4abb846418c2b2c49760
Matthew Themis <matthew.themis@anatwine.com>
2022-09-05 16:55:39 +0100

    Make gzip option check more distro independent
    Signed-off-by: Matthew Themis <matthew.themis@anatwine.com>

--- scripts/compress_assets.sh
+++ scripts/compress_assets.sh
@@ -7,8 +7,8 @@ set -euo pipefail
 cd web/ui
 cp embed.go.tmpl embed.go
 
-# gzip option '-k' does not exist on RHEL7 based instances, so remove it
-GZIP_OPTS="-fk" && [[ $(awk -F= '/^NAME/{print $2}' /etc/os-release) =~ (CentOS|Red Hat).* ]] && [[ $(awk -F= '/^VERSION_ID/{print $2}' /etc/os-release) =~ 7.* ]] && GZIP_OPTS="-f"
+# gzip option '-k' may not always exist in the latest gzip available on different distros
+GZIP_OPTS="-fk" && [[ ! $(gzip -k -h 2>/dev/null) ]] && GZIP_OPTS="-f"
 
 find static -type f -name '*.gz' -delete
 find static -type f -exec gzip $GZIP_OPTS '{}' \; -print0 | xargs -0 -I % echo %.gz | xargs echo //go:embed >> embed.go
