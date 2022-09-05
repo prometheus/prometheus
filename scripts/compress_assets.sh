a14d69e6bf6b4b47619f62d32ed6de8caba357a5
Matthew Themis <matthew.themis@anatwine.com>
2022-09-05 14:21:53 +0100

    Add RHEL7 compatibility for gzip options
    Signed-off-by: Matthew Themis <matthew.themis@anatwine.com>

--- scripts/compress_assets.sh
+++ scripts/compress_assets.sh
@@ -6,6 +6,10 @@ set -euo pipefail
 
 cd web/ui
 cp embed.go.tmpl embed.go
+
+# gzip option '-k' does not exist on RHEL7 based instances, so remove it
+GZIP_OPTS="-fk" && [[ $(awk -F= '/^NAME/{print $2}' /etc/os-release) =~ (CentOS|Red Hat).* ]] && [[ $(awk -F= '/^VERSION_ID/{print $2}' /etc/os-release) =~ 7.* ]] && GZIP_OPTS="-f"
+
 find static -type f -name '*.gz' -delete
-find static -type f -exec gzip -fk '{}' \; -print0 | xargs -0 -I % echo %.gz | xargs echo //go:embed >> embed.go
+find static -type f -exec gzip $GZIP_OPTS '{}' \; -print0 | xargs -0 -I % echo %.gz | xargs echo //go:embed >> embed.go
 echo var EmbedFS embed.FS >> embed.go
