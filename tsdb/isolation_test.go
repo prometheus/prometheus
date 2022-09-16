f17b3c5747d971ead7dbe9668c3566557a8a7a22
Bryan Boreham <bjboreham@gmail.com>
2022-09-16 11:35:53 +0000

    tsdb: store the min time with isolationAppender
    So that we can see when appending has moved past a certain point in time.
    
    Signed-off-by: Bryan Boreham <bjboreham@gmail.com>

--- tsdb/isolation_test.go
+++ tsdb/isolation_test.go
@@ -32,28 +32,32 @@ func TestIsolation(t *testing.T) {
 
 	// Low watermark starts at 1.
 	require.Equal(t, uint64(0), iso.lowWatermark())
+	require.Equal(t, int64(math.MaxInt64), iso.lowestAppendTime())
 
 	// Pretend we are starting to append.
-	appendA.id, appendA.lowWatermark = iso.newAppendID()
+	appendA.id, appendA.lowWatermark = iso.newAppendID(10)
 	require.Equal(t, result{1, 1}, appendA)
 	require.Equal(t, uint64(1), iso.lowWatermark())
 
 	require.Equal(t, 0, countOpenReads(iso))
+	require.Equal(t, int64(10), iso.lowestAppendTime())
 
 	// Now we start a read.
 	stateA := iso.State(10, 20)
 	require.Equal(t, 1, countOpenReads(iso))
 
 	// Second appender.
-	appendB.id, appendB.lowWatermark = iso.newAppendID()
+	appendB.id, appendB.lowWatermark = iso.newAppendID(20)
 	require.Equal(t, result{2, 1}, appendB)
 	require.Equal(t, uint64(1), iso.lowWatermark())
+	require.Equal(t, int64(10), iso.lowestAppendTime())
 
 	iso.closeAppend(appendA.id)
 	// Low watermark remains at 1 because stateA is still open
 	require.Equal(t, uint64(1), iso.lowWatermark())
 
 	require.Equal(t, 1, countOpenReads(iso))
+	require.Equal(t, int64(20), iso.lowestAppendTime())
 
 	// Finish the read and low watermark should rise.
 	stateA.Close()
@@ -63,7 +67,7 @@ func TestIsolation(t *testing.T) {
 
 	iso.closeAppend(appendB.id)
 	require.Equal(t, uint64(2), iso.lowWatermark())
-
+	require.Equal(t, int64(math.MaxInt64), iso.lowestAppendTime())
 }
 
 func countOpenReads(iso *isolation) int {
@@ -91,7 +95,7 @@ func BenchmarkIsolation(b *testing.B) {
 					<-start
 
 					for i := 0; i < b.N; i++ {
-						appendID, _ := iso.newAppendID()
+						appendID, _ := iso.newAppendID(0)
 
 						iso.closeAppend(appendID)
 					}
@@ -121,7 +125,7 @@ func BenchmarkIsolationWithState(b *testing.B) {
 					<-start
 
 					for i := 0; i < b.N; i++ {
-						appendID, _ := iso.newAppendID()
+						appendID, _ := iso.newAppendID(0)
 
 						iso.closeAppend(appendID)
 					}
