5b3c721d33c5c436436cbe869b3fd48196e7387b
Bryan Boreham <bjboreham@gmail.com>
2022-09-17 12:09:52 +0100

    TSDB chunks: remove race between writing and reading
    Because the data is stored as a bit-stream, the last byte in the stream
    could change if the stream is appended to after an Iterator is obtained.
    Copy the last byte when the Iterator is created, so we don't have to
    read it later.
    
    Clarify in comments that concurrent Iterator and Appender are allowed,
    but the chunk must not be modified while an Iterator is created.
    (This was already the case, in order to copy the bstream slice header.)
    
    Signed-off-by: Bryan Boreham <bjboreham@gmail.com>

--- tsdb/chunkenc/bstream.go
+++ tsdb/chunkenc/bstream.go
@@ -119,11 +119,18 @@ type bstreamReader struct {
 
 	buffer uint64 // The current buffer, filled from the stream, containing up to 8 bytes from which read bits.
 	valid  uint8  // The number of right-most bits valid to read (from left) in the current 8 byte buffer.
+	last   byte   // A copy of the last byte of the stream.
 }
 
 func newBReader(b []byte) bstreamReader {
+	// The last byte of the stream can be updated later, so we take a copy.
+	var last byte
+	if len(b) > 0 {
+		last = b[len(b)-1]
+	}
 	return bstreamReader{
 		stream: b,
+		last:   last,
 	}
 }
 
@@ -223,17 +230,24 @@ func (b *bstreamReader) loadNextBuffer(nbits uint8) bool {
 		return true
 	}
 
-	// We're here if the are 8 or less bytes left in the stream. Since this reader needs
-	// to handle race conditions with concurrent writes happening on the very last byte
-	// we make sure to never over more than the minimum requested bits (rounded up to
-	// the next byte). The following code is slower but called less frequently.
+	// We're here if there are 8 or less bytes left in the stream.
+	// The following code is slower but called less frequently.
 	nbytes := int((nbits / 8) + 1)
 	if b.streamOffset+nbytes > len(b.stream) {
 		nbytes = len(b.stream) - b.streamOffset
 	}
 
 	buffer := uint64(0)
-	for i := 0; i < nbytes; i++ {
+	skip := 0
+	if b.streamOffset+nbytes == len(b.stream) {
+		// There can be concurrent writes happening on the very last byte
+		// of the stream, so use the copy we took at initialization time.
+		buffer = buffer | uint64(b.last)
+		// Read up to the byte before
+		skip = 1
+	}
+
+	for i := 0; i < nbytes-skip; i++ {
 		buffer = buffer | (uint64(b.stream[b.streamOffset+i]) << uint(8*(nbytes-i-1)))
 	}
 
