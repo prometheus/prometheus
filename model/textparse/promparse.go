3e5ad99c33aa6891b0423e789204e1bbc2d8a1e0
beorn7 <beorn@grafana.com>
2023-01-11 00:30:55 +0100

    textparse: Minor doc comment improvement
    Signed-off-by: beorn7 <beorn@grafana.com>

--- model/textparse/promparse.go
+++ model/textparse/promparse.go
@@ -168,8 +168,8 @@ func (p *PromParser) Series() ([]byte, *int64, float64) {
 	return p.series, nil, p.val
 }
 
-// Histogram always returns (nil, nil, nil, nil) because the Prometheus text format
-// does not support sparse histograms.
+// Histogram returns (nil, nil, nil, nil) for now because the Prometheus text
+// format does not support sparse histograms yet.
 func (p *PromParser) Histogram() ([]byte, *int64, *histogram.Histogram, *histogram.FloatHistogram) {
 	return nil, nil, nil, nil
 }
