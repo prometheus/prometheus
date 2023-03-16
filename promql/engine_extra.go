e711e1f82b4082e6b440c57812cd97f30d105113
Thomas Jackson <jacksontj.89@gmail.com>
2023-03-16 09:17:43 -0700

    fix typo

--- promql/engine_extra.go
+++ promql/engine_extra.go
@@ -30,7 +30,7 @@ func findPathRange(path []parser.Node, eRanges []evalRange) time.Duration {
 	return evalRange
 }
 
-// evalRange summarizes a defined evalRange (from a MatrixSelector) within the asg
+// evalRange summarizes a defined evalRange (from a MatrixSelector) within the ast
 type evalRange struct {
 	Prefix []posrange.PositionRange
 	Range  time.Duration
