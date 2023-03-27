grafana/mimir-prometheus

tsdb: use abstractions over Labels
Signed-off-by: Bryan Boreham <bjboreham@gmail.com>

tsdb/compact.go
@@ -1160,13 +1160,17 @@ func (c *LeveledCompactor) populateSymbols(sets []storage.ChunkSeriesSet, outBlo
 
 		obIx := s.Labels().Hash() % uint64(len(outBlocks))
 
-		for _, l := range s.Labels() {
+		err := s.Labels().RangeToError(func(l labels.Label) error {
 			if err := batchers[obIx].addSymbol(l.Name); err != nil {
 				return errors.Wrap(err, "addSymbol to batcher")
 			}
 			if err := batchers[obIx].addSymbol(l.Value); err != nil {
 				return errors.Wrap(err, "addSymbol to batcher")
 			}
+			return nil
+		})
+		if err != nil {
+			return err
 		}
 	}
 

tsdb/compact_test.go
@@ -583,17 +583,18 @@ func TestCompaction_CompactWithSplitting(t *testing.T) {
 					require.NoError(t, err)
 
 					var lbls labels.Labels
+					var builder labels.SimpleBuilder
 					for p.Next() {
 						ref := p.At()
-						require.NoError(t, idxr.Series(ref, &lbls, nil))
+						require.NoError(t, idxr.Series(ref, &builder, &lbls, nil))
 
 						require.Equal(t, uint64(shardIndex), lbls.Hash()%shardCount)
 
 						// Collect all symbols used by series.
-						for _, l := range lbls {
+						lbls.Copy().Range(func(l labels.Label) {
 							seriesSymbols[l.Name] = struct{}{}
 							seriesSymbols[l.Value] = struct{}{}
-						}
+						})
 					}
 					require.NoError(t, p.Err())
 

prometheus/prometheus

tsdb/compact.go
@@ -1176,13 +1176,17 @@ func (c *LeveledCompactor) populateSymbols(sets []storage.ChunkSeriesSet, outBlo
 			obIx = labels.StableHash(s.Labels()) % uint64(len(outBlocks))
 		}
 
-		for _, l := range s.Labels() {
+		err := s.Labels().Validate(func(l labels.Label) error {
 			if err := batchers[obIx].addSymbol(l.Name); err != nil {
 				return errors.Wrap(err, "addSymbol to batcher")
 			}
 			if err := batchers[obIx].addSymbol(l.Value); err != nil {
 				return errors.Wrap(err, "addSymbol to batcher")
 			}
+			return nil
+		})
+		if err != nil {
+			return err
 		}
 	}
 

tsdb/compact_test.go
@@ -596,10 +596,10 @@ func TestCompaction_CompactWithSplitting(t *testing.T) {
 						require.Equal(t, uint64(shardIndex), lbls.Labels().Hash()%shardCount)
 
 						// Collect all symbols used by series.
-						for _, l := range lbls.Labels() {
+						lbls.Labels().Range(func(l labels.Label) {
 							seriesSymbols[l.Name] = struct{}{}
 							seriesSymbols[l.Value] = struct{}{}
-						}
+						})
 					}
 					require.NoError(t, p.Err())
 