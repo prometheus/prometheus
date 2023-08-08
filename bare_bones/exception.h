4949803743b7e427afb27009afbe3eeed620e05a
Alexey Gorgurov <alexey.gorgurov@flant.com>
2023-08-08 13:31:32 +0200

    feat(bare_bones/exception): Simple umbrella exception type with our code
    This code can be retrieved via ./scripts/err_code_gen <filename> <line>.
    
    Any constructed exception object must have an unique code in hexadecimal
    digit form, without digits, for ease of grep'ping.

---
+++ bare_bones/exception.h
@@ -0,0 +1,34 @@
+/// @file exception.h
+/// Use this exception for wrapping any third-party libraries exceptions.
+/// If you add any new object, please, generate new exception code via
+/// ./scripts/err_code_gen <filename> <line>, [<git_commit_hash>]
+/// from root folder.
+/// Any exception throw point must contains the unique code
+/// as hexadecimal digit (without delimiters, for ease of grep'ping).
+#pragma once
+
+#include <cstdint>
+#include <exception>
+#include <sstream>
+#include <string>
+
+namespace BareBones {
+
+using Code = uint64_t;
+
+class Exception : public std::exception {
+  std::string msg_;
+  Code code_;
+
+ public:
+  Exception(Code exc_code, std::string_view message) : code_(exc_code) {
+    std::stringstream ss;
+    ss << "Exception " << exc_code << ": " << message;
+    msg_ = std::move(ss.str());
+  }
+
+  const char* what() const noexcept override { return msg_.c_str(); }
+  Code code() const noexcept { return code_; }
+};
+
+}  // namespace BareBones
