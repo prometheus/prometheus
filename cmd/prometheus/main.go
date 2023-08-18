b30b70a94d7899df05df94112cad76de83eb0ba3
Paulin Todev <paulin.todev@gmail.com>
2023-08-18 17:54:06 +0100

    Allow registering scrape metrics with a non-default registerer.
    Signed-off-by: Paulin Todev <paulin.todev@gmail.com>

--- cmd/prometheus/main.go
+++ cmd/prometheus/main.go
@@ -619,8 +619,13 @@ func main() {
 		discoveryManagerNotify = legacymanager.NewManager(ctxNotify, log.With(logger, "component", "discovery manager notify"), legacymanager.Name("notify"))
 	}
 
+	scrapeManager, err := scrape.NewManager(&cfg.scrape, log.With(logger, "component", "scrape manager"), fanoutStorage, prometheus.DefaultRegisterer)
+	if err != nil {
+		level.Error(logger).Log("msg", "failed to create a scrape manager", "err", err)
+		os.Exit(1)
+	}
+
 	var (
-		scrapeManager  = scrape.NewManager(&cfg.scrape, log.With(logger, "component", "scrape manager"), fanoutStorage)
 		tracingManager = tracing.NewManager(logger)
 
 		queryEngine *promql.Engine
