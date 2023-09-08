7f085aeb6af9f2317237b1ea87910afb618791f4
Jayapriya Pai <janantha@redhat.com>
2023-09-08 11:46:03 +0530

    EC2 SD: Validate HTTP config
    Related-to #12810
    
    Signed-off-by: Jayapriya Pai <janantha@redhat.com>

--- config/config_test.go
+++ config/config_test.go
@@ -1746,6 +1746,10 @@ var expectedErrors = []struct {
 		errMsg:   `EC2 SD configuration filter values cannot be empty`,
 	},
 	{
+		filename: "ec2_token_file.bad.yml",
+		errMsg:   `at most one of bearer_token & bearer_token_file must be configured`,
+	},
+	{
 		filename: "section_key_dup.bad.yml",
 		errMsg:   "field scrape_configs already set in type config.plain",
 	},
