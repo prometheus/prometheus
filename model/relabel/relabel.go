5840dfd050eccab3ed8f76842b96949f7ccb2bdc
hainenber <dotronghai96@gmail.com>
2023-10-07 21:49:36 +0700

    fix(model/relabel): string comparison of relabel's regexes
    Signed-off-by: hainenber <dotronghai96@gmail.com>

--- model/relabel/relabel.go
+++ model/relabel/relabel.go
@@ -131,7 +131,7 @@ func (c *Config) UnmarshalYAML(unmarshal func(interface{}) error) error {
 	}
 
 	if c.Action == DropEqual || c.Action == KeepEqual {
-		if c.Regex != DefaultRelabelConfig.Regex ||
+		if c.Regex.String() != DefaultRelabelConfig.Regex.String() ||
 			c.Modulus != DefaultRelabelConfig.Modulus ||
 			c.Separator != DefaultRelabelConfig.Separator ||
 			c.Replacement != DefaultRelabelConfig.Replacement {
