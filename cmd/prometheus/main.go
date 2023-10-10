624b973ebf4d0d7ed863b5538d7940c9c66c5cd0
Bartlomiej Plotka <bwplotka@gmail.com>
2023-10-10 11:16:55 +0100

    Added ability to specify scrape protocols to accept during HTTP content type negotiation. (#12738)
    * Added ability to specify scrape protocols to accept during HTTP content type negotiation.
    
    
    This is done via new option in GlobalConfig and ScrapeConfig: "scrape_protocol"
    
    Signed-off-by: bwplotka <bwplotka@gmail.com>
    
    * Fixed readability and log message.
    
    Signed-off-by: bwplotka <bwplotka@gmail.com>
    
    ---------
    
    Signed-off-by: bwplotka <bwplotka@gmail.com>

--- cmd/prometheus/main.go
+++ cmd/prometheus/main.go
@@ -202,8 +202,9 @@ func (c *flagConfig) setFeatureListOptions(logger log.Logger) error {
 				level.Info(logger).Log("msg", "No default port will be appended to scrape targets' addresses.")
 			case "native-histograms":
 				c.tsdb.EnableNativeHistograms = true
-				c.scrape.EnableProtobufNegotiation = true
-				level.Info(logger).Log("msg", "Experimental native histogram support enabled.")
+				// Change global variable. Hacky, but it's hard to pass new option or default to unmarshaller.
+				config.DefaultConfig.GlobalConfig.ScrapeProtocols = config.DefaultNativeHistogramScrapeProtocols
+				level.Info(logger).Log("msg", "Experimental native histogram support enabled. Changed default scrape_protocols to prefer PrometheusProto format.", "global.scrape_protocols", fmt.Sprintf("%v", config.DefaultConfig.GlobalConfig.ScrapeProtocols))
 			case "":
 				continue
 			case "promql-at-modifier", "promql-negative-offset":
