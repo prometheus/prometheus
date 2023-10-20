7756556eb6d3bf590fe1822f274c0bcc28740eb4
Jeanette Tan <jeanette.tan@grafana.com>
2023-10-20 19:18:37 +0800

    Remove NewPossibleNonCounterInfo until it can be made more efficient

--- promql/functions.go
+++ promql/functions.go
@@ -88,14 +88,6 @@ func extrapolatedRate(vals []parser.Value, args parser.Expressions, enh *EvalNod
 		return enh.Out, annos.Add(annotations.NewMixedFloatsHistogramsWarning(metricName, args[0].PositionRange()))
 	}
 
-	if isCounter && metricName != "" && len(samples.Floats) > 0 &&
-		!strings.HasSuffix(metricName, "_total") &&
-		!strings.HasSuffix(metricName, "_sum") &&
-		!strings.HasSuffix(metricName, "_count") &&
-		!strings.HasSuffix(metricName, "_bucket") {
-		annos.Add(annotations.NewPossibleNonCounterInfo(metricName, args[0].PositionRange()))
-	}
-
 	switch {
 	case len(samples.Histograms) > 1:
 		numSamplesMinusOne = len(samples.Histograms) - 1
