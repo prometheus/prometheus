5a1d95b936d31db5c32dcf58e1542e1ddc47a8b2
Oleksandr Redko <Oleksandr_Redko@epam.com>
2023-11-01 20:26:38 +0200

    Enable default revive rules
    Signed-off-by: Oleksandr Redko <Oleksandr_Redko@epam.com>

--- storage/remote/azuread/azuread.go
+++ storage/remote/azuread/azuread.go
@@ -62,7 +62,7 @@ type OAuthConfig struct {
 }
 
 // AzureADConfig is used to store the config values.
-type AzureADConfig struct {
+type AzureADConfig struct { //nolint:revive // exported.
 	// ManagedIdentity is the managed identity that is being used to authenticate.
 	ManagedIdentity *ManagedIdentityConfig `yaml:"managed_identity,omitempty"`
 
