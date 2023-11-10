d2ecde3ad873ea42497cd278084d7f05e05d100a
Fiona Liao <fiona.y.liao@gmail.com>
2023-11-10 16:05:58 +0000

    Add proper warning for iterable during analyzeCompaction
    Signed-off-by: Fiona Liao <fiona.y.liao@gmail.com>

--- cmd/promtool/tsdb.go
+++ cmd/promtool/tsdb.go
@@ -644,13 +644,14 @@ func analyzeCompaction(ctx context.Context, block tsdb.BlockReader, indexr tsdb.
 
 		for _, chk := range chks {
 			// Load the actual data of the chunk.
-			var iterable chunkenc.Iterable
 			chk, iterable, err := chunkr.ChunkOrIterable(chk)
 			if err != nil {
 				return err
 			}
+			// Chunks within blocks should not require re-encoding, so an iterable is not expected to be returned
+			// from the chunk reader.
 			if iterable != nil {
-				return errors.New("iterable not supported for analyzeCompaction yet")
+				return errors.New("ChunkOrIterable should not return an iterable when reading a block")
 			}
 			switch chk.Encoding() {
 			case chunkenc.EncXOR:
