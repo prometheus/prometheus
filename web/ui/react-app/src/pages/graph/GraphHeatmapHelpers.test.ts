40941d450bd85a0ec31011951b40afc0101b744f
Kevin Mingtarja <kevin.mingtarja@gmail.com>
2024-01-04 02:48:54 +0700

    Fix handling of scalar and string in isHeatmapData
    Signed-off-by: Kevin Mingtarja <kevin.mingtarja@gmail.com>

--- web/ui/react-app/src/pages/graph/GraphHeatmapHelpers.test.ts
+++ web/ui/react-app/src/pages/graph/GraphHeatmapHelpers.test.ts
@@ -1,8 +1,23 @@
+import { DataTableProps } from './DataTable';
 import { isHeatmapData } from './GraphHeatmapHelpers';
 
 describe('GraphHeatmapHelpers', () => {
-  it('isHeatmapData should return false for a vector', () => {
-    const data = {
+  it('isHeatmapData should return false for scalar and string resultType', () => {
+    let data = {
+      resultType: 'scalar',
+      result: [1703091180.125, '1703091180.125'],
+    } as DataTableProps['data'];
+    expect(isHeatmapData(data)).toBe(false);
+
+    data = {
+      resultType: 'string',
+      result: [1704305680.332, '2504'],
+    } as DataTableProps['data'];
+    expect(isHeatmapData(data)).toBe(false);
+  });
+
+  it('isHeatmapData should return false for a vector and matrix if length < 2', () => {
+    let data = {
       resultType: 'vector',
       result: [
         {
@@ -13,15 +28,39 @@ describe('GraphHeatmapHelpers', () => {
           value: [1703091180.683, '6'],
         },
       ],
-    };
+    } as DataTableProps['data'];
+    expect(isHeatmapData(data)).toBe(false);
+
+    data = {
+      resultType: 'matrix',
+      result: [
+        {
+          metric: {},
+          values: [[1703091180.683, '6']],
+        },
+      ],
+    } as DataTableProps['data'];
     expect(isHeatmapData(data)).toBe(false);
   });
 
-  it('isHeatmapData should return false for scalar resultType', () => {
+  it('isHeatmapData should return true for valid heatmap data', () => {
     const data = {
-      resultType: 'scalar',
-      result: [1703091180.125, '1703091180.125'],
-    };
-    expect(isHeatmapData(data)).toBe(false);
+      resultType: 'matrix',
+      result: [
+        {
+          metric: {
+            le: '100',
+          },
+          values: [[1703091180.683, '6']],
+        },
+        {
+          metric: {
+            le: '1000',
+          },
+          values: [[1703091190.683, '6.1']],
+        },
+      ],
+    } as DataTableProps['data'];
+    expect(isHeatmapData(data)).toBe(true);
   });
 });
