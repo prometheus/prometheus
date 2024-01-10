7bc845c8589e50b11b16de5c8e22dbb74136b620
Owen Williams <owen.williams@grafana.com>
2024-01-10 12:31:34 -0500

    make it a top-level flag instead

--- cmd/prometheus/main.go
+++ cmd/prometheus/main.go
@@ -450,7 +450,7 @@ func main() {
 	a.Flag("scrape.discovery-reload-interval", "Interval used by scrape manager to throttle target groups updates.").
 		Hidden().Default("5s").SetValue(&cfg.scrape.DiscoveryReloadInterval)
 
-	a.Flag("scrape.name-escaping-scheme", "method to escape legacy invalid names when sending to an old version of prometheus.  can be one of values (default), underscores, or dots").Default(scrape.DefaultNameEscapingScheme.String()).StringVar(&cfg.scrape.NameEscapingScheme)
+	a.Flag("scrape.name-escaping-scheme", "method to escape legacy invalid names when sending to an old version of prometheus.  can be one of values (default), underscores, or dots").Default(scrape.DefaultNameEscapingScheme.String()).StringVar(&cfg.nameEscapingScheme)
 
 	a.Flag("enable-feature", "Comma separated feature names to enable. Valid options: agent, exemplar-storage, expand-external-labels, memory-snapshot-on-shutdown, promql-at-modifier, promql-negative-offset, promql-per-step-stats, promql-experimental-functions, remote-write-receiver (DEPRECATED), extra-scrape-metrics, new-service-discovery-manager, auto-gomaxprocs, no-default-scrape-port, native-histograms, otlp-write-receiver, utf8-names. See https://prometheus.io/docs/prometheus/latest/feature_flags/ for more details.").
 		Default("").StringsVar(&cfg.featureList)
