2f079dae87db184aa3d78af25554fec964d2dac5
Callum Styan <callumstyan@gmail.com>
2024-01-30 13:33:18 -0800

    another lint fix
    Signed-off-by: Callum Styan <callumstyan@gmail.com>

--- storage/remote/client.go
+++ storage/remote/client.go
@@ -28,7 +28,6 @@ import (
 	"github.com/gogo/protobuf/proto"
 	"github.com/golang/snappy"
 	"github.com/prometheus/client_golang/prometheus"
-	config_util "github.com/prometheus/common/config"
 	"github.com/prometheus/common/model"
 	"github.com/prometheus/common/sigv4"
 	"github.com/prometheus/common/version"
@@ -37,6 +36,7 @@ import (
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/trace"
 
+	config_util "github.com/prometheus/common/config"
 	"github.com/prometheus/prometheus/prompb"
 	"github.com/prometheus/prometheus/storage/remote/azuread"
 )
