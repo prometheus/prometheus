699f31a2a6820e2452ebeb6c3dcabf15e6078c87
michaelact <86778470+michaelact@users.noreply.github.com>
2024-03-08 22:19:39 +0700

    [BUGFIX] Azure SD: Fix 'error: parameter virtualMachineScaleSetName cannot be empty' (#13702)
    Erroneous code was introduced during a merge-back-to-main at #13399.
    
    Signed-off-by: michaelact <86778470+michaelact@users.noreply.github.com>

--- discovery/azure/azure.go
+++ discovery/azure/azure.go
@@ -413,26 +413,20 @@ func (d *Discovery) refresh(ctx context.Context) ([]*targetgroup.Group, error) {
 					} else {
 						networkInterface, err = client.getVMScaleSetVMNetworkInterfaceByID(ctx, nicID, vm.ScaleSet, vm.InstanceID)
 					}
+
 					if err != nil {
 						if errors.Is(err, errorNotFound) {
 							level.Warn(d.logger).Log("msg", "Network interface does not exist", "name", nicID, "err", err)
 						} else {
 							ch <- target{labelSet: nil, err: err}
 						}
-						d.addToCache(nicID, networkInterface)
-					} else {
-						networkInterface, err = client.getVMScaleSetVMNetworkInterfaceByID(ctx, nicID, vm.ScaleSet, vm.InstanceID)
-						if err != nil {
-							if errors.Is(err, errorNotFound) {
-								level.Warn(d.logger).Log("msg", "Network interface does not exist", "name", nicID, "err", err)
-							} else {
-								ch <- target{labelSet: nil, err: err}
-							}
-							// Get out of this routine because we cannot continue without a network interface.
-							return
-						}
-						d.addToCache(nicID, networkInterface)
+
+						// Get out of this routine because we cannot continue without a network interface.
+						return
 					}
+
+					// Continue processing with the network interface
+					d.addToCache(nicID, networkInterface)
 				}
 
 				if networkInterface.Properties == nil {
