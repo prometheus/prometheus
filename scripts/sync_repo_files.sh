a0fbc75f341949bc90335799719599efc9660b99
SuperQ <superq@gmail.com>
2024-03-14 09:20:40 +0100

    Add container_description.yml to repo sync
    Add the container_description.yml workflow to the repo file sync script.
    * Skip sync if there is no Dockerfile.
    * Fixup minor typo in container_description.yml.
    
    Signed-off-by: SuperQ <superq@gmail.com>

--- scripts/sync_repo_files.sh
+++ scripts/sync_repo_files.sh
@@ -37,7 +37,7 @@ if [ -z "${GITHUB_TOKEN}" ]; then
 fi
 
 # List of files that should be synced.
-SYNC_FILES="CODE_OF_CONDUCT.md LICENSE Makefile.common SECURITY.md .yamllint scripts/golangci-lint.yml .github/workflows/scorecards.yml"
+SYNC_FILES="CODE_OF_CONDUCT.md LICENSE Makefile.common SECURITY.md .yamllint scripts/golangci-lint.yml .github/workflows/scorecards.yml .github/workflows/container_description.yml"
 
 # Go to the root of the repo
 cd "$(git rev-parse --show-cdup)" || exit 1
@@ -99,6 +99,15 @@ check_go() {
   curl -sLf -o /dev/null "https://raw.githubusercontent.com/${org_repo}/${default_branch}/go.mod"
 }
 
+check_docker() {
+  local org_repo
+  local default_branch
+  org_repo="$1"
+  default_branch="$2"
+
+  curl -sLf -o /dev/null "https://raw.githubusercontent.com/${org_repo}/${default_branch}/Dockerfile"
+}
+
 process_repo() {
   local org_repo
   local default_branch
@@ -117,6 +126,10 @@ process_repo() {
     source_checksum="$(sha256sum "${source_dir}/${source_file}" | cut -d' ' -f1)"
     if [[ "${source_file}" == 'scripts/golangci-lint.yml' ]] && ! check_go "${org_repo}" "${default_branch}" ; then
       echo "${org_repo} is not Go, skipping golangci-lint.yml."
+      continue
+    fi
+    if [[ "${source_file}" == '.github/workflows/container_description.yml' ]] && ! check_docker "${org_repo}" "${default_branch}" ; then
+      echo "${org_repo} has no Dockerfile, skipping container_description.yml."
       continue
     fi
     if [[ "${source_file}" == 'LICENSE' ]] && ! check_license "${target_file}" ; then
