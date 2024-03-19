a0654eaef61203f09e5b72bc95428ab9b63feb4d
Nick Pillitteri <nick.pillitteri@grafana.com>
2024-03-19 13:52:23 -0400

    Code review
    Signed-off-by: Nick Pillitteri <nick.pillitteri@grafana.com>

--- tsdb/head_test.go
+++ tsdb/head_test.go
@@ -5819,3 +5819,16 @@ func TestHeadAppender_AppendCTZeroSample(t *testing.T) {
 		require.Equal(t, chunkenc.ValNone, it.Next())
 	}
 }
+
+func TestHeadCompactableDoesNotRequireOverflow(t *testing.T) {
+	// Set a chunk range of 1 to ensure that Head.compactable() doesn't require
+	// the default min (math.MaxInt64) and max (math.MinInt64) timestamps for the
+	// head before any samples are added to overflow to return correct results.
+	// See https://github.com/prometheus/prometheus/pull/13755
+	head, _ := newTestHead(t, 1, wlog.CompressionNone, false)
+	defer func() {
+		require.NoError(t, head.Close())
+	}()
+
+	require.False(t, head.compactable())
+}
