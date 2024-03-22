88b4f9c0405f6bcb06b25bf524e0d6a0be5675a1
Jeanette Tan <jeanette.tan@grafana.com>
2024-03-22 22:16:37 +0800

    fix histogram_quantile for native histograms with custom buckets
    Signed-off-by: Jeanette Tan <jeanette.tan@grafana.com>

--- promql/quantile.go
+++ promql/quantile.go
@@ -203,14 +203,19 @@ func histogramQuantile(q float64, h *histogram.FloatHistogram) float64 {
 		rank = (1 - q) * h.Count
 	}
 
+	bucketIndex := -1
 	for it.Next() {
+		bucketIndex++
 		bucket = it.At()
+		if bucket.Count == 0 {
+			continue
+		}
 		count += bucket.Count
 		if count >= rank {
 			break
 		}
 	}
-	if bucket.Lower < 0 && bucket.Upper > 0 {
+	if !h.UsesCustomBuckets() && bucket.Lower < 0 && bucket.Upper > 0 {
 		switch {
 		case len(h.NegativeBuckets) == 0 && len(h.PositiveBuckets) > 0:
 			// The result is in the zero bucket and the histogram has only
@@ -220,6 +225,17 @@ func histogramQuantile(q float64, h *histogram.FloatHistogram) float64 {
 			// The result is in the zero bucket and the histogram has only
 			// negative buckets. So we consider 0 to be the upper bound.
 			bucket.Upper = 0
+		}
+	} else if h.UsesCustomBuckets() && bucketIndex == 0 {
+		if bucket.Lower == math.Inf(-1) {
+			// first bucket, with lower bound -Inf
+			if bucket.Upper <= 0 {
+				return bucket.Upper
+			}
+			bucket.Lower = 0
+		} else if bucket.Upper == math.Inf(1) {
+			// last bucket, with upper bound +Inf
+			return bucket.Lower
 		}
 	}
 	// Due to numerical inaccuracies, we could end up with a higher count
