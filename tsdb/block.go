5c4310aa37a5587e7a4f4cb1487b4873a92f7d07
Arve Knudsen <arve.knudsen@gmail.com>
2024-05-09 11:55:30 +0200

    [ENHANCEMENT] TSDB: Optimize querying with regexp matchers
    Add method `PostingsForLabelMatching` to `tsdb.IndexReader`, to obtain postings for labels with a certain name and values accepted by a provided callback, and use it from `tsdb.PostingsForMatchers`.
    The intention is to optimize regexp matcher paths, especially not having to load all label values before matching on them.
    
    Plus tests, and refactor some `tsdb/index.Reader` methods.
    
    Benchmarking shows memory reduction up to ~100%, and speedup of up to ~50%.
    
    Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>
    Co-authored-by: Bartlomiej Plotka <bwplotka@gmail.com>

--- tsdb/block.go
+++ tsdb/block.go
@@ -77,6 +77,10 @@ type IndexReader interface {
 	// during background garbage collections.
 	Postings(ctx context.Context, name string, values ...string) (index.Postings, error)
 
+	// PostingsForLabelMatching returns a sorted iterator over postings having a label with the given name and a value for which match returns true.
+	// If no postings are found having at least one matching label, an empty iterator is returned.
+	PostingsForLabelMatching(ctx context.Context, name string, match func(value string) bool) index.Postings
+
 	// SortedPostings returns a postings list that is reordered to be sorted
 	// by the label set of the underlying series.
 	SortedPostings(index.Postings) index.Postings
@@ -516,6 +520,10 @@ func (r blockIndexReader) Postings(ctx context.Context, name string, values ...s
 		return p, fmt.Errorf("block: %s: %w", r.b.Meta().ULID, err)
 	}
 	return p, nil
+}
+
+func (r blockIndexReader) PostingsForLabelMatching(ctx context.Context, name string, match func(string) bool) index.Postings {
+	return r.ir.PostingsForLabelMatching(ctx, name, match)
 }
 
 func (r blockIndexReader) SortedPostings(p index.Postings) index.Postings {
