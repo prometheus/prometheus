8ab656996cbdd7fa186a4d4e2a3d16498ef1e9a0
Manik Rana <manikrana54@gmail.com>
2024-05-15 21:50:52 +0530

    broken: try out new formulae
    Signed-off-by: Manik Rana <manikrana54@gmail.com>

--- web/ui/react-app/src/pages/graph/HistogramChart.tsx
+++ web/ui/react-app/src/pages/graph/HistogramChart.tsx
@@ -16,6 +16,14 @@ const HistogramChart: FC<{ histogram: Histogram; index: number; scale: ScaleType
 
   const rangeMax = buckets ? parseFloat(buckets[buckets.length - 1][2]) : 0;
   const rangeMin = buckets ? parseFloat(buckets[0][1]) : 0;
+
+  const expBucketWidth = buckets
+    ? Math.abs(
+        Math.log(Math.abs(parseFloat(buckets[buckets.length - 1][2]))) -
+          Math.log(Math.abs(parseFloat(buckets[buckets.length - 1][1])))
+      )
+    : 0; //bw
+
   const countMax = buckets ? buckets.map((b) => parseFloat(b[3])).reduce((a, b) => Math.max(a, b)) : 0;
 
   // The count of a histogram bucket is represented by its area rather than its height. This means it considers
@@ -36,6 +44,19 @@ const HistogramChart: FC<{ histogram: Histogram; index: number; scale: ScaleType
     scale === 'linear'
       ? ((0 - rangeMin) / (rangeMax - rangeMin)) * 100 + '%'
       : (closestToZero.closestIdx / (buckets ? buckets.length : 1)) * 100 + '%';
+
+  const maxPositive = buckets ? parseFloat(buckets[buckets.length - 1][2]) : 0;
+  const minPositive = buckets ? parseFloat(buckets[closestToZero.closestIdx + 1][1]) : 0;
+  const maxNegative = buckets ? parseFloat(buckets[closestToZero.closestIdx - 1][2]) : 0;
+  const minNegative = buckets ? parseFloat(buckets[0][1]) : 0;
+  const startNegative = buckets ? Math.log(Math.abs(minNegative)) : 0; //start_neg
+  const endNegative = buckets ? Math.log(Math.abs(maxNegative)) : 0; //end_neg
+  const startPositive = buckets ? Math.log(minPositive) : 0; //start_pos
+  const endPositive = buckets ? Math.log(maxPositive) : 0; //end_pos
+
+  const widthNegative = startNegative - endNegative; //width_neg
+  const widthPositive = endPositive - startPositive; //width_pos
+  const widthTotal = widthNegative + expBucketWidth + widthPositive; //width_total
 
   function findClosestToZero(numbers: number[]): closestToZeroType {
     let closest = numbers[0];
@@ -94,6 +115,13 @@ const HistogramChart: FC<{ histogram: Histogram; index: number; scale: ScaleType
               fds={fds}
               fdMax={fdMax}
               countMax={countMax}
+              bw={expBucketWidth}
+              maxPositive={maxPositive}
+              startPositive={startPositive}
+              startNegative={startNegative}
+              endPositive={endPositive}
+              widthNegative={widthNegative}
+              widthTotal={widthTotal}
             />
           )}
 
@@ -122,6 +150,13 @@ interface RenderHistogramProps {
   fds: number[];
   fdMax: number;
   countMax: number;
+  bw: number;
+  maxPositive: number;
+  startPositive: number;
+  startNegative: number;
+  endPositive: number;
+  widthNegative: number;
+  widthTotal: number;
 }
 
 const RenderHistogramBars: FC<RenderHistogramProps> = ({
@@ -133,21 +168,61 @@ const RenderHistogramBars: FC<RenderHistogramProps> = ({
   fds,
   fdMax,
   countMax,
+  bw,
+  maxPositive,
+  startPositive,
+  startNegative,
+  endPositive,
+  widthNegative,
+  widthTotal,
 }) => {
   return (
     <React.Fragment>
       {buckets.map((b, bIdx) => {
+        const left = parseFloat(b[1]);
+        const right = parseFloat(b[2]);
+        const count = parseFloat(b[3]);
+        console.log('new stuff');
+
         const bucketIdx = `bucket-${index}-${bIdx}-${Math.ceil(parseFloat(b[3]) * 100)}`;
+        const bucketWidth =
+          scale === 'linear' ? ((right - left) / (rangeMax - rangeMin)) * 100 + '%' : (bw / widthTotal) * 100 + '%';
         const bucketLeft =
           scale === 'linear'
-            ? ((parseFloat(b[1]) - rangeMin) / (rangeMax - rangeMin)) * 100 + '%'
-            : (bIdx / buckets.length) * 100 + '%';
-        const bucketWidth =
-          scale === 'linear'
-            ? ((parseFloat(b[2]) - parseFloat(b[1])) / (rangeMax - rangeMin)) * 100 + '%'
-            : 100 / buckets.length + '%';
-        const bucketHeight =
-          scale === 'linear' ? (fds[bIdx] / fdMax) * 100 + '%' : (parseFloat(b[3]) / countMax) * 100 + '%';
+            ? ((left - rangeMin) / (rangeMax - rangeMin)) * 100 + '%'
+            : left < 0
+            ? (Math.log(Math.abs(left)) - startNegative / widthTotal) * 100 + '%' // negative buckets boundary
+            : (Math.log(left) - startPositive + bw + widthNegative / widthTotal) * 100 + '%'; // positive buckets boundary
+        const bucketHeight = scale === 'linear' ? (fds[bIdx] / fdMax) * 100 + '%' : (count / countMax) * 100 + '%';
+
+        console.log(
+          'left',
+          left,
+          '\n',
+          'right',
+          right,
+          '\n',
+          'count',
+          count,
+          '\n',
+          'bucketWidth',
+          bucketWidth,
+          '\n',
+          'bucketLeft',
+          bucketLeft,
+          '\n',
+          'bw',
+          bw,
+          '\n',
+          'widthTotal',
+          widthTotal,
+          '\n',
+          'widthNegative',
+          widthNegative,
+          '\n',
+          'startPositive',
+          startPositive
+        );
         return (
           <React.Fragment key={bIdx}>
             <div
@@ -172,7 +247,7 @@ const RenderHistogramBars: FC<RenderHistogramProps> = ({
               >
                 <strong>range:</strong> {bucketRangeString(b)}
                 <br />
-                <strong>count:</strong> {b[3]}
+                <strong>count:</strong> {count}
               </UncontrolledTooltip>
             </div>
           </React.Fragment>
