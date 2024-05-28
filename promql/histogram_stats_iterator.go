fb3818fd0c0eab1b9365e79762cf08d6b7e77243
Filip Petkovski <filip.petkovsky@gmail.com>
2024-05-28 11:29:58 +0200

    Add license and fix lint
    Signed-off-by: Filip Petkovski <filip.petkovsky@gmail.com>

--- promql/histogram_stats_iterator.go
+++ promql/histogram_stats_iterator.go
@@ -1,3 +1,16 @@
+// Copyright 2024 The Prometheus Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package promql
 
 import (
@@ -101,9 +114,8 @@ func (f *histogramStatsIterator) getFloatResetHint(hint histogram.CounterResetHi
 
 	if f.fhReader.DetectReset(f.lastFH) {
 		return histogram.CounterReset
-	} else {
-		return histogram.NotCounterReset
 	}
+	return histogram.NotCounterReset
 }
 
 func (f *histogramStatsIterator) getResetHint(h *histogram.Histogram) histogram.CounterResetHint {
@@ -117,7 +129,6 @@ func (f *histogramStatsIterator) getResetHint(h *histogram.Histogram) histogram.
 	fh, prevFH := h.ToFloat(nil), f.lastH.ToFloat(nil)
 	if fh.DetectReset(prevFH) {
 		return histogram.CounterReset
-	} else {
-		return histogram.NotCounterReset
 	}
+	return histogram.NotCounterReset
 }
