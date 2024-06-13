fd9335c7c00dcca4e4f0fbe8d7b7fea3ea7e8e6d
Mustafain Ali Khan <mustalik@amazon.com>
2024-06-13 14:08:02 -0700

    Store alert state using file storage
    Signed-off-by: Mustafain Ali Khan <mustalik@amazon.com>

---
+++ rules/store_test.go
@@ -0,0 +1,46 @@
+package rules
+
+import (
+	"os"
+	"testing"
+	"time"
+
+	"github.com/go-kit/log"
+	"github.com/prometheus/prometheus/model/labels"
+	"github.com/stretchr/testify/require"
+)
+
+func TestAlertStore(t *testing.T) {
+	alertStore := NewFileStore(log.NewNopLogger(), "alertstoretest")
+	t.Cleanup(func() {
+		os.Remove("alertstoretest")
+	})
+
+	alertsByRule := make(map[uint64][]*Alert)
+	baseTime := time.Now()
+	al1 := &Alert{State: StateFiring, Labels: labels.FromStrings("a1", "1"), Annotations: labels.FromStrings("annotation1", "a1"), ActiveAt: baseTime, KeepFiringSince: baseTime}
+	al2 := &Alert{State: StateFiring, Labels: labels.FromStrings("a2", "2"), Annotations: labels.FromStrings("annotation2", "a2"), ActiveAt: baseTime, KeepFiringSince: baseTime}
+
+	alertsByRule[1] = []*Alert{al1, al2}
+	alertsByRule[2] = []*Alert{al2}
+	alertsByRule[3] = []*Alert{al1}
+	alertsByRule[4] = []*Alert{}
+
+	for key, alerts := range alertsByRule {
+		err := alertStore.SetAlerts(key, alerts)
+		require.NoError(t, err)
+
+		got, err := alertStore.GetAlerts(key)
+		require.NoError(t, err)
+		require.Equal(t, len(alerts), len(got))
+		j := 0
+		for _, al := range got {
+			require.Equal(t, alerts[j].State, al.State)
+			require.Equal(t, alerts[j].Labels, al.Labels)
+			require.Equal(t, alerts[j].Annotations, al.Annotations)
+			require.Equal(t, alerts[j].ActiveAt, al.ActiveAt)
+			require.Equal(t, alerts[j].KeepFiringSince, al.KeepFiringSince)
+			j++
+		}
+	}
+}
