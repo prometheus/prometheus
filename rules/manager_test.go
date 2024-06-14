33382e5f2e231fc077c2872150f33ecccc095304
Oleg Zaytsev <mail@olegzaytsev.com>
2024-06-14 14:05:07 +0200

    Reduce the flakiness of TestAsyncRuleEvaluation
    This tests sleeps for 15 millisecond per rule group, and then comprares
    the entire execution time to be smaller than a multiple of that delay.
    
    The ruleCount is 6, so it assumes that the test will come to the
    assertions in less than 90ms.
    
    Meanwhile, the Github's Windows runner:
    - ...Huh, oh? What? How much time? milliwhat? Sorry I don't speak that.
    
    TL;DR, this increases the delay to 250 millisecond. This won't prevent
    the test from being flaky, but will reduce the flakiness by several
    orders of magnitude and hopefully won't be an issue anymore.
    
    Signed-off-by: Oleg Zaytsev <mail@olegzaytsev.com>

--- rules/manager_test.go
+++ rules/manager_test.go
@@ -2098,7 +2098,7 @@ func TestBoundedRuleEvalConcurrency(t *testing.T) {
 	require.EqualValues(t, maxInflight.Load(), int32(maxConcurrency)+int32(groupCount))
 }
 
-const artificialDelay = 15 * time.Millisecond
+const artificialDelay = 250 * time.Millisecond
 
 func optsFactory(storage storage.Storage, maxInflight, inflightQueries *atomic.Int32, maxConcurrent int64) *ManagerOptions {
 	var inflightMu sync.Mutex
