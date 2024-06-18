b841fc3e7768c674f4388aa26d6ebb4db3dc3abc
Vladimir Pustovalov <vladimir.pustovalov01@flant.com>
2024-06-18 18:15:58 +0300

    fixed OutdatedSampleEncoder compare logic

--- series_data/outdated_sample_encoder.h
+++ series_data/outdated_sample_encoder.h
@@ -13,7 +13,7 @@ class OutdatedSampleEncoder {
   template <EncoderInterface Encoder>
   void encode(Encoder& encoder, uint32_t ls_id, int64_t timestamp, double value) {
     if (auto it = storage_.outdated_chunks_.try_emplace(ls_id, timestamp, value); !it.second) {
-      if (it.first->second.encode(timestamp, value) > kMaxChunkSize) {
+      if (it.first->second.encode(timestamp, value) >= kMaxChunkSize) {
         OutdatedChunkMerger<Encoder> merger{storage_, encoder};
         merger.merge(ls_id, it.first->second);
       }
