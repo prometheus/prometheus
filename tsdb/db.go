2c5e88748e58c09485f64049b82bcdd5b0f58aaf
Charles Korn <charles.korn@grafana.com>
2024-06-25 14:22:44 +1000

    Fix issue where pending OOO read can be left dangling if creating querier fails
    Signed-off-by: Charles Korn <charles.korn@grafana.com>

--- tsdb/db.go
+++ tsdb/db.go
@@ -2077,6 +2077,9 @@ func (db *DB) blockChunkQuerierForRange(mint, maxt int64) (_ []storage.ChunkQuer
 		rh := NewOOORangeHead(db.head, mint, maxt, db.lastGarbageCollectedMmapRef)
 		outOfOrderHeadQuerier, err := NewBlockChunkQuerier(rh, mint, maxt)
 		if err != nil {
+			// If NewBlockQuerier() failed, make sure to clean up the pending read created by NewOOORangeHead.
+			rh.isoState.Close()
+
 			return nil, fmt.Errorf("open block chunk querier for ooo head %s: %w", rh, err)
 		}
 
