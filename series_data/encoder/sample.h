00629ba71781ebfdb35fb4974ab7b4d0866d4290
Vladimir Pustovalov <vladimir.pustovalov01@flant.com>
2024-06-28 15:46:55 +0300

    fixed Sample compare operators

--- series_data/encoder/sample.h
+++ series_data/encoder/sample.h
@@ -9,11 +9,13 @@ struct Sample {
   int64_t timestamp{};
   double value{};
 
-  bool operator==(const Sample& other) const noexcept { return timestamp == other.timestamp && value::is_values_strictly_equals(value, other.value); }
-  bool operator<(const Sample& other) const noexcept { return timestamp < other.timestamp; }
-  bool operator<=(const Sample& other) const noexcept { return timestamp < other.timestamp || value::is_values_strictly_equals(value, other.value); }
-  bool operator>(const Sample& other) const noexcept { return !(*this <= other); }
-  bool operator>=(const Sample& other) const noexcept { return !(*this < other); }
+  PROMPP_ALWAYS_INLINE bool operator==(const Sample& other) const noexcept {
+    return timestamp == other.timestamp && value::is_values_strictly_equals(value, other.value);
+  }
+  PROMPP_ALWAYS_INLINE bool operator<(const Sample& other) const noexcept { return timestamp < other.timestamp; }
+  PROMPP_ALWAYS_INLINE bool operator<=(const Sample& other) const noexcept { return *this < other || *this == other; }
+  PROMPP_ALWAYS_INLINE bool operator>(const Sample& other) const noexcept { return timestamp > other.timestamp; }
+  PROMPP_ALWAYS_INLINE bool operator>=(const Sample& other) const noexcept { return *this > other || *this == other; }
 };
 
 using SampleList = BareBones::Vector<Sample>;
