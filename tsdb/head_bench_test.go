7c5e04e99de336fc1ad3ccc5de7b606f6a7a21b0
Oleg Zaytsev <mail@olegzaytsev.com>
2024-07-08 11:00:27 +0200

    Add/improve benchmarks
    Signed-off-by: Oleg Zaytsev <mail@olegzaytsev.com>

--- tsdb/head_bench_test.go
+++ tsdb/head_bench_test.go
@@ -35,9 +35,53 @@ func BenchmarkHeadStripeSeriesCreate(b *testing.B) {
 	require.NoError(b, err)
 	defer h.Close()
 
+	lsets := make([]labels.Labels, b.N)
+	hashes := make([]uint64, b.N)
 	for i := 0; i < b.N; i++ {
-		h.getOrCreate(uint64(i), labels.FromStrings("a", strconv.Itoa(i)))
+		lsets[i] = labels.FromStrings(labels.MetricName, "test_metric", "lbl", strconv.Itoa(i))
+		hashes[i] = lsets[i].Hash()
 	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, created, _ := h.getOrCreate(hashes[i], lsets[i])
+		if !created {
+			panic("should create new series")
+		}
+	}
+}
+
+func BenchmarkHeadStripeSeriesGetExisting(b *testing.B) {
+	chunkDir := b.TempDir()
+	// Put a series, select it. GC it and then access it.
+	opts := DefaultHeadOptions()
+	opts.ChunkRange = 1000
+	opts.ChunkDirRoot = chunkDir
+	opts.StripeSize = 16
+	lsets := make([]labels.Labels, 1024)
+	hashes := make([]uint64, 1024)
+	h, err := NewHead(nil, nil, nil, nil, opts, nil)
+	require.NoError(b, err)
+	defer h.Close()
+
+	for i := range lsets {
+		lsets[i] = labels.FromStrings(labels.MetricName, "test_metric", "lbl", strconv.Itoa(i))
+		hashes[i] = lsets[i].Hash()
+		_, created, _ := h.getOrCreate(hashes[i], lsets[i])
+		if !created {
+			panic("should create new series")
+		}
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		n := i % 1024
+		_, created, _ := h.getOrCreate(hashes[n], lsets[n])
+		if created {
+			panic("unexpected series creation")
+		}
+	}
+	b.StopTimer()
 }
 
 func BenchmarkHeadStripeSeriesCreateParallel(b *testing.B) {
@@ -54,8 +98,13 @@ func BenchmarkHeadStripeSeriesCreateParallel(b *testing.B) {
 
 	b.RunParallel(func(pb *testing.PB) {
 		for pb.Next() {
-			i := count.Inc()
-			h.getOrCreate(uint64(i), labels.FromStrings("a", strconv.Itoa(int(i))))
+			lset := labels.FromStrings("a", strconv.Itoa(int(count.Inc())))
+			// Use a real hash instead of just the count because the seriesHashmap relies
+			// on the fact that hashes are evenly distributed across uint64 values.
+			_, created, _ := h.getOrCreate(lset.Hash(), lset)
+			if !created {
+				panic("should create new series")
+			}
 		}
 	})
 }
