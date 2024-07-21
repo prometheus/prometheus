28a936ea226c763dde37a381f6dc056392875cf2
IrfanMohammad <irfan.haslanded@gmail.com>
2024-07-21 23:08:31 +0000

    bugfix query_logger mmap file when no disk space
    If sparse files are supported, ftruncate will modify the filesize
    as asked, but will not actually allocate/reserve the space on disk.
    
    When a write is attempted after memory mapping this file, SIGBUS is
    received.
    
    It is preferrable to report the lack of disk space to the user and
    terminate gracefully.
    
    Additionally, mapped memory must be sync'd or changes may not be
    actually written. This is done by calling the Flush() api from mmap.
    
    Signed-off-by: IrfanMohammad <irfan.haslanded@gmail.com>

--- promql/engine_test.go
+++ promql/engine_test.go
@@ -60,7 +60,7 @@ func TestQueryConcurrency(t *testing.T) {
 	dir, err := os.MkdirTemp("", "test_concurrency")
 	require.NoError(t, err)
 	defer os.RemoveAll(dir)
-	queryTracker := promql.NewActiveQueryTracker(dir, maxConcurrency, nil)
+	queryTracker, _ := promql.NewActiveQueryTracker(dir, maxConcurrency, nil)
 	t.Cleanup(func() {
 		require.NoError(t, queryTracker.Close())
 	})
