0c55b259196e5ff3e3699ae1d22c4bf508accfb5
Julius Volz <julius.volz@gmail.com>
2024-08-06 09:16:18 +0200

    Show a gray health ring for empty pools
    Signed-off-by: Julius Volz <julius.volz@gmail.com>

--- web/ui/mantine-ui/src/pages/targets/ScrapePoolsList.tsx
+++ web/ui/mantine-ui/src/pages/targets/ScrapePoolsList.tsx
@@ -236,13 +236,15 @@ const ScrapePoolList: FC<ScrapePoolListProp> = ({
                           value: (pool.upCount / pool.count) * 100,
                           color: "green.4",
                         },
-                        {
-                          value: (pool.downCount / pool.count) * 100,
-                          color: "red.5",
-                        },
+                        // Important that gray is the middle one, since the middle one seems to be
+                        // the default color if all three values are 0 (empty pool = gray).
                         {
                           value: (pool.unknownCount / pool.count) * 100,
                           color: "gray.4",
+                        },
+                        {
+                          value: (pool.downCount / pool.count) * 100,
+                          color: "red.5",
                         },
                       ]}
                     />
