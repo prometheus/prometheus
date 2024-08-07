e15fc999a8d3f8d3aeebb1301be90a771629d9a7
Oleg Zaytsev <mail@olegzaytsev.com>
2024-08-07 12:14:48 +0200

    make lint happy
    Signed-off-by: Oleg Zaytsev <mail@olegzaytsev.com>

--- storage/errors_test.go
+++ storage/errors_test.go
@@ -14,7 +14,6 @@
 package storage
 
 import (
-	"errors"
 	"fmt"
 	"testing"
 
@@ -23,16 +22,16 @@ import (
 
 func TestErrDuplicateSampleForTimestamp(t *testing.T) {
 	// All errDuplicateSampleForTimestamp are ErrDuplicateSampleForTimestamp
-	require.True(t, errors.Is(ErrDuplicateSampleForTimestamp, errDuplicateSampleForTimestamp{}))
+	require.ErrorIs(t, ErrDuplicateSampleForTimestamp, errDuplicateSampleForTimestamp{})
 
 	// Same type only is if it has same properties.
 	err1 := NewDuplicateFloatErr(1_000, 10, 20)
 	err2 := NewDuplicateFloatErr(1_001, 30, 40)
 
-	require.True(t, errors.Is(err1, err1))
-	require.False(t, errors.Is(err1, err2))
+	require.ErrorIs(t, err1, err1)
+	require.NotErrorIs(t, err1, err2)
 
 	// Also works when err is wrapped.
-	require.True(t, errors.Is(fmt.Errorf("failed: %w", err1), err1))
-	require.False(t, errors.Is(fmt.Errorf("failed: %w", err1), err2))
+	require.ErrorIs(t, fmt.Errorf("failed: %w", err1), err1)
+	require.NotErrorIs(t, fmt.Errorf("failed: %w", err1), err2)
 }
