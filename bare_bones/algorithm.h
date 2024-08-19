2732aa983fa7066dc98ed0c91b53439c3ab2065a
Vladimir Pustovalov <vladimir.pustovalov01@flant.com>
2024-08-19 11:38:46 +0300

    added comment for BareBones::accumulate

--- bare_bones/algorithm.h
+++ bare_bones/algorithm.h
@@ -2,6 +2,8 @@
 
 namespace BareBones {
 
+// implementation for std::ranges::accumulate
+// TODO: remove this function on C++23 and use std::ranges::fold_left instead
 template <class Range, class ValueType, class Method>
 ValueType accumulate(const Range& range, ValueType initial_value, Method&& method) {
   for (auto& item : range) {
