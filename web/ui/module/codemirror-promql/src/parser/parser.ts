dcabc40d8f8a9634fbed50e41b6b8997efd1be1a
Arve Knudsen <arve.knudsen@gmail.com>
2024-08-23 10:42:30 +0200

    Update web/ui/module/codemirror-promql/src/parser/parser.ts
    Co-authored-by: Augustin Husson <augustin.husson@amadeus.com>
    Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>

--- web/ui/module/codemirror-promql/src/parser/parser.ts
+++ web/ui/module/codemirror-promql/src/parser/parser.ts
@@ -276,10 +276,9 @@ export class Parser {
     }
 
     if (funcSignature.name === 'info') {
-      if (args.length > 1) {
-        if (args[1].getChild(Identifier)) {
-          this.addDiagnostic(node, `expected label selectors as the second argument to "info" function, got ${args[1].type}`);
-        }
+      // Verify that the data label selector expression is not prefixed with metric name.
+      if (args.length > 1 && args[1].getChild(Identifier)) {
+        this.addDiagnostic(node, `expected label selectors as the second argument to "info" function, got ${args[1].type}`);
       }
     }
 
