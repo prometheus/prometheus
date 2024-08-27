d14eb733e8483d11ef294c4e877a9bb97e69bf40
Manik Rana <manikrana54@gmail.com>
2024-08-27 21:01:21 +0530

    refac: allow opt-in to OM CT ingestion
    Signed-off-by: Manik Rana <manikrana54@gmail.com>

--- model/textparse/interface.go
+++ model/textparse/interface.go
@@ -80,7 +80,7 @@ type Parser interface {
 //
 // This function always returns a valid parser, but might additionally
 // return an error if the content type cannot be parsed.
-func New(b []byte, contentType string, parseClassicHistograms bool, st *labels.SymbolTable) (Parser, error) {
+func New(b []byte, contentType string, parseClassicHistograms bool, skipOMSeries bool, st *labels.SymbolTable) (Parser, error) {
 	if contentType == "" {
 		return NewPromParser(b, st), nil
 	}
@@ -91,7 +91,10 @@ func New(b []byte, contentType string, parseClassicHistograms bool, st *labels.S
 	}
 	switch mediaType {
 	case "application/openmetrics-text":
-		return NewOpenMetricsParser(b, st, WithOMParserCTSeriesSkipped()), nil
+		opts := func(o * openMetricsParserOptions) {
+			o.SkipCTSeries = skipOMSeries
+		}
+		return NewOpenMetricsParser(b, st, opts), nil
 	case "application/vnd.google.protobuf":
 		return NewProtobufParser(b, parseClassicHistograms, st), nil
 	default:
