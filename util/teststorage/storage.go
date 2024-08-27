f2c5225f4895b3e5090ea37716fb195fc0ec6438
Vanshikav123 <vanshikav928@gmail.com>
2024-08-27 23:15:42 +0530

    testutil
    Signed-off-by: Vanshikav123 <vanshikav928@gmail.com>

--- util/teststorage/storage.go
+++ util/teststorage/storage.go
@@ -51,7 +51,7 @@ func NewWithError() (*TestStorage, error) {
 	opts.MaxBlockDuration = int64(24 * time.Hour / time.Millisecond)
 	opts.RetentionDuration = 0
 	opts.EnableNativeHistograms = true
-	opts.OutOfOrderTimeWindow = 600000
+	// opts.OutOfOrderTimeWindow = 600000
 	db, err := tsdb.Open(dir, nil, nil, opts, tsdb.NewDBStats())
 	if err != nil {
 		return nil, fmt.Errorf("opening test storage: %w", err)
