cec2af631db2f28a2fa908f071a35e56a364ce18
Arve Knudsen <arve.knudsen@gmail.com>
2024-09-26 10:18:41 +0200

    PromQL: use promqltest for info tests
    Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>

--- promql/info_test.go
+++ promql/info_test.go
@@ -14,479 +14,98 @@
 package promql_test
 
 import (
-	"context"
 	"testing"
-	"time"
 
-	"github.com/stretchr/testify/require"
-
-	"github.com/prometheus/prometheus/model/labels"
-	"github.com/prometheus/prometheus/promql"
 	"github.com/prometheus/prometheus/promql/promqltest"
-	"github.com/prometheus/prometheus/util/testutil"
 )
 
-func TestInfoFunc(t *testing.T) {
-	const (
-		load1 = `
-load 10s
+// The "info" function is experimental. This is why we write those tests here for now instead of promqltest/testdata/info.test.
+func TestInfo(t *testing.T) {
+	engine := promqltest.NewTestEngine(t, false, 0, promqltest.DefaultMaxSamplesPerQuery)
+	promqltest.RunTest(t, `
+load 5m
   metric{instance="a", job="1", label="value"} 0 1 2
   metric_not_matching_target_info{instance="a", job="2", label="value"} 0 1 2
   metric_with_overlapping_label{instance="a", job="1", label="value", data="base"} 0 1 2
   target_info{instance="a", job="1", data="info", another_data="another info"} 1 1 1
   build_info{instance="a", job="1", build_data="build"} 1 1 1
-`
-		// Overlapping target_info series.
-		load2 = `
-load 10s
+
+# Include one info metric data label.
+eval range from 0m to 10m step 5m info(metric, {data=~".+"})
+  metric{data="info", instance="a", job="1", label="value"} 0 1 2
+
+# Include all info metric data labels.
+eval range from 0m to 10m step 5m info(metric)
+  metric{data="info", instance="a", job="1", label="value", another_data="another info"} 0 1 2
+
+# Try including all info metric data labels, but non-matching identifying labels.
+eval range from 0m to 10m step 5m info(metric_not_matching_target_info)
+  metric_not_matching_target_info{instance="a", job="2", label="value"} 0 1 2
+
+# Try including a certain info metric data label with a non-matching matcher not accepting empty labels.
+# Metric is ignored, due there being a data label matcher not matching empty labels,
+# and there being no info series matches.
+eval range from 0m to 10m step 5m info(metric, {non_existent=~".+"})
+
+# Include a certain info metric data label together with a non-matching matcher accepting empty labels.
+# Since the non_existent matcher matches empty labels, it's simply ignored when there's no match.
+# XXX: This case has to include a matcher not matching empty labels, due the PromQL limitation
+# that vector selectors have to contain at least one matcher not accepting empty labels.
+# We might need another construct than vector selector to get around this limitation.
+eval range from 0m to 10m step 5m info(metric, {data=~".+", non_existent=~".*"})
+  metric{data="info", instance="a", job="1", label="value"} 0 1 2
+
+# Info series data labels overlapping with those of base series are ignored.
+eval range from 0m to 10m step 5m info(metric_with_overlapping_label)
+  metric_with_overlapping_label{data="base", instance="a", job="1", label="value", another_data="another info"} 0 1 2
+
+# Include data labels from target_info specifically.
+eval range from 0m to 10m step 5m info(metric, {__name__="target_info"})
+  metric{data="info", instance="a", job="1", label="value", another_data="another info"} 0 1 2
+
+# Try to include all data labels from a non-existent info metric.
+eval range from 0m to 10m step 5m info(metric, {__name__="non_existent"})
+  metric{instance="a", job="1", label="value"} 0 1 2
+
+# Try to include a certain data label from a non-existent info metric.
+eval range from 0m to 10m step 5m info(metric, {__name__="non_existent", data=~".+"})
+
+# Include data labels from build_info.
+eval range from 0m to 10m step 5m info(metric, {__name__="build_info"})
+  metric{instance="a", job="1", label="value", build_data="build"} 0 1 2
+
+# Include data labels from build_info and target_info.
+eval range from 0m to 10m step 5m info(metric, {__name__=~".+_info"})
+  metric{instance="a", job="1", label="value", build_data="build", data="info", another_data="another info"} 0 1 2
+
+# Info metrics themselves are ignored when it comes to enriching with info metric data labels.
+eval range from 0m to 10m step 5m info(build_info, {__name__=~".+_info", build_data=~".+"})
+  build_info{instance="a", job="1", build_data="build"} 1 1 1
+
+clear
+
+# Overlapping target_info series.
+load 5m
   metric{instance="a", job="1", label="value"} 0 1 2
   target_info{instance="a", job="1", data="info", another_data="another info"} 1 1 _
   target_info{instance="a", job="1", data="updated info", another_data="another info"} _ _ 1
-`
-		// Non-overlapping target_info series.
-		load3 = `
-load 10s
+
+# Conflicting info series are resolved through picking the latest sample.
+eval range from 0m to 10m step 5m info(metric)
+  metric{data="info", instance="a", job="1", label="value", another_data="another info"} 0 1 _
+  metric{data="updated info", instance="a", job="1", label="value", another_data="another info"} _ _ 2
+
+clear
+
+# Non-overlapping target_info series.
+load 5m
   metric{instance="a", job="1", label="value"} 0 1 2
   target_info{instance="a", job="1", data="info"} 1 1 stale
   target_info{instance="a", job="1", data="updated info"} _ _ 1
-`
-	)
 
-	testCases := []struct {
-		name   string
-		load   string
-		query  string
-		result promql.Matrix
-		expErr error
-	}{
-		{
-			name:  "include one info metric data label",
-			load:  load1,
-			query: `info(metric, {data=~".+"})`,
-			result: promql.Matrix{
-				promql.Series{
-					Metric: labels.FromStrings(
-						labels.MetricName, "metric",
-						"instance", "a",
-						"job", "1",
-						"label", "value",
-						"data", "info",
-					),
-					Floats: []promql.FPoint{
-						{
-							T: 0,
-							F: 0,
-						},
-						{
-							T: 10000,
-							F: 1,
-						},
-						{
-							T: 20000,
-							F: 2,
-						},
-					},
-				},
-			},
-		},
-		{
-			name:  "include all info metric data labels",
-			load:  load1,
-			query: `info(metric)`,
-			result: promql.Matrix{
-				promql.Series{
-					Metric: labels.FromStrings(
-						labels.MetricName, "metric",
-						"instance", "a",
-						"job", "1",
-						"label", "value",
-						"another_data", "another info",
-						"data", "info",
-					),
-					Floats: []promql.FPoint{
-						{
-							T: 0,
-							F: 0,
-						},
-						{
-							T: 10000,
-							F: 1,
-						},
-						{
-							T: 20000,
-							F: 2,
-						},
-					},
-				},
-			},
-		},
-		{
-			name:  "try including all info metric data labels, but non-matching identifying labels",
-			load:  load1,
-			query: `info(metric_not_matching_target_info)`,
-			result: promql.Matrix{
-				promql.Series{
-					Metric: labels.FromStrings(
-						labels.MetricName, "metric_not_matching_target_info",
-						"instance", "a",
-						"job", "2",
-						"label", "value",
-					),
-					Floats: []promql.FPoint{
-						{
-							T: 0,
-							F: 0,
-						},
-						{
-							T: 10000,
-							F: 1,
-						},
-						{
-							T: 20000,
-							F: 2,
-						},
-					},
-				},
-			},
-		},
-		{
-			name:  "try including a certain info metric data label with a non-matching matcher not accepting empty labels",
-			load:  load1,
-			query: `info(metric, {non_existent=~".+"})`,
-			// metric is ignored, due there being a data label matcher not matching empty labels,
-			// and there being no info series matches.
-			result: promql.Matrix{},
-		},
-		{
-			// XXX: This case has to include a matcher not matching empty labels, due the PromQL limitation
-			// that vector selectors have to contain at least one matcher not accepting empty labels.
-			// We might need another construct than vector selector to get around this limitation.
-			name:  "include a certain info metric data label together with a non-matching matcher accepting empty labels",
-			load:  load1,
-			query: `info(metric, {data=~".+", non_existent=~".*"})`,
-			// Since the non_existent matcher matches empty labels, it's simply ignored when there's no match.
-			result: promql.Matrix{
-				promql.Series{
-					Metric: labels.FromStrings(
-						labels.MetricName, "metric",
-						"instance", "a",
-						"job", "1",
-						"label", "value",
-						"data", "info",
-					),
-					Floats: []promql.FPoint{
-						{
-							T: 0,
-							F: 0,
-						},
-						{
-							T: 10000,
-							F: 1,
-						},
-						{
-							T: 20000,
-							F: 2,
-						},
-					},
-				},
-			},
-		},
-		{
-			name:  "info series data labels overlapping with those of base series are ignored",
-			load:  load1,
-			query: `info(metric_with_overlapping_label)`,
-			result: promql.Matrix{
-				promql.Series{
-					Metric: labels.FromStrings(
-						labels.MetricName, "metric_with_overlapping_label",
-						"data", "base",
-						"instance", "a",
-						"job", "1",
-						"label", "value",
-						"another_data", "another info",
-					),
-					Floats: []promql.FPoint{
-						{
-							T: 0,
-							F: 0,
-						},
-						{
-							T: 10000,
-							F: 1,
-						},
-						{
-							T: 20000,
-							F: 2,
-						},
-					},
-				},
-			},
-		},
-		{
-			name:  "include data labels from target_info specifically",
-			load:  load1,
-			query: `info(metric, {__name__="target_info"})`,
-			result: promql.Matrix{
-				promql.Series{
-					Metric: labels.FromStrings(
-						labels.MetricName, "metric",
-						"instance", "a",
-						"job", "1",
-						"label", "value",
-						"another_data", "another info",
-						"data", "info",
-					),
-					Floats: []promql.FPoint{
-						{
-							T: 0,
-							F: 0,
-						},
-						{
-							T: 10000,
-							F: 1,
-						},
-						{
-							T: 20000,
-							F: 2,
-						},
-					},
-				},
-			},
-		},
-		{
-			name:  "try to include all data labels from a non-existent info metric",
-			load:  load1,
-			query: `info(metric, {__name__="non_existent"})`,
-			result: promql.Matrix{
-				promql.Series{
-					Metric: labels.FromStrings(
-						labels.MetricName, "metric",
-						"instance", "a",
-						"job", "1",
-						"label", "value",
-					),
-					Floats: []promql.FPoint{
-						{
-							T: 0,
-							F: 0,
-						},
-						{
-							T: 10000,
-							F: 1,
-						},
-						{
-							T: 20000,
-							F: 2,
-						},
-					},
-				},
-			},
-		},
-		{
-			name:   "try to include a certain data label from a non-existent info metric",
-			load:   load1,
-			query:  `info(metric, {__name__="non_existent", data=~".+"})`,
-			result: promql.Matrix{},
-		},
-		{
-			name:  "include data labels from build_info",
-			load:  load1,
-			query: `info(metric, {__name__="build_info"})`,
-			result: promql.Matrix{
-				promql.Series{
-					Metric: labels.FromStrings(
-						labels.MetricName, "metric",
-						"build_data", "build",
-						"instance", "a",
-						"job", "1",
-						"label", "value",
-					),
-					Floats: []promql.FPoint{
-						{
-							T: 0,
-							F: 0,
-						},
-						{
-							T: 10000,
-							F: 1,
-						},
-						{
-							T: 20000,
-							F: 2,
-						},
-					},
-				},
-			},
-		},
-		{
-			name:  "include data labels from build_info and target_info",
-			load:  load1,
-			query: `info(metric, {__name__=~".+_info"})`,
-			result: promql.Matrix{
-				promql.Series{
-					Metric: labels.FromStrings(
-						labels.MetricName, "metric",
-						"another_data", "another info",
-						"build_data", "build",
-						"data", "info",
-						"instance", "a",
-						"job", "1",
-						"label", "value",
-					),
-					Floats: []promql.FPoint{
-						{
-							T: 0,
-							F: 0,
-						},
-						{
-							T: 10000,
-							F: 1,
-						},
-						{
-							T: 20000,
-							F: 2,
-						},
-					},
-				},
-			},
-		},
-		{
-			name:  "info metrics themselves are ignored when it comes to enriching with info metric data labels",
-			load:  load1,
-			query: `info(build_info, {__name__=~".+_info", build_data=~".+"})`,
-			result: promql.Matrix{
-				promql.Series{
-					Metric: labels.FromStrings(
-						labels.MetricName, "build_info",
-						"build_data", "build",
-						"instance", "a",
-						"job", "1",
-					),
-					Floats: []promql.FPoint{
-						{
-							T: 0,
-							F: 1,
-						},
-						{
-							T: 10000,
-							F: 1,
-						},
-						{
-							T: 20000,
-							F: 1,
-						},
-					},
-				},
-			},
-		},
-		{
-			name:  "conflicting info series are resolved through picking the latest sample",
-			load:  load2,
-			query: `info(metric)`,
-			result: promql.Matrix{
-				promql.Series{
-					Metric: labels.FromStrings(
-						labels.MetricName, "metric",
-						"instance", "a",
-						"job", "1",
-						"label", "value",
-						"another_data", "another info",
-						"data", "info",
-					),
-					Floats: []promql.FPoint{
-						{
-							T: 0,
-							F: 0,
-						},
-						{
-							T: 10000,
-							F: 1,
-						},
-					},
-				},
-				promql.Series{
-					Metric: labels.FromStrings(
-						labels.MetricName, "metric",
-						"instance", "a",
-						"job", "1",
-						"label", "value",
-						"another_data", "another info",
-						"data", "updated info",
-					),
-					Floats: []promql.FPoint{
-						{
-							T: 20000,
-							F: 2,
-						},
-					},
-				},
-			},
-		},
-		{
-			name:  "include info metric data labels from a metric which data labels change over time",
-			load:  load3,
-			query: `info(metric)`,
-			result: promql.Matrix{
-				promql.Series{
-					Metric: labels.FromStrings(
-						labels.MetricName, "metric",
-						"instance", "a",
-						"job", "1",
-						"label", "value",
-						"data", "info",
-					),
-					Floats: []promql.FPoint{
-						{
-							T: 0,
-							F: 0,
-						},
-						{
-							T: 10000,
-							F: 1,
-						},
-					},
-				},
-				promql.Series{
-					Metric: labels.FromStrings(
-						labels.MetricName, "metric",
-						"instance", "a",
-						"job", "1",
-						"label", "value",
-						"data", "updated info",
-					),
-					Floats: []promql.FPoint{
-						{
-							T: 20000,
-							F: 2,
-						},
-					},
-				},
-			},
-		},
-	}
-	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
-			engine := promqltest.NewTestEngine(t, false, 0, promqltest.DefaultMaxSamplesPerQuery)
-			ctx := context.Background()
-			storage := promqltest.LoadedStorage(t, tc.load)
-			t.Cleanup(func() { _ = storage.Close() })
-
-			start := time.Unix(0, 0)
-			end := time.Unix(20, 0)
-			qry, err := engine.NewRangeQuery(ctx, storage, nil, tc.query, start, end, 10*time.Second)
-			require.NoError(t, err)
-
-			res := qry.Exec(ctx)
-			if tc.expErr != nil {
-				require.EqualError(t, res.Err, tc.expErr.Error())
-				return
-			}
-
-			require.NoError(t, res.Err)
-			require.Empty(t, res.Warnings)
-			mat, ok := res.Value.(promql.Matrix)
-			require.True(t, ok)
-			testutil.RequireEqual(t, tc.result, mat)
-		})
-	}
+# Include info metric data labels from a metric which data labels change over time.
+eval range from 0m to 10m step 5m info(metric)
+  metric{data="info", instance="a", job="1", label="value"} 0 1 _
+  metric{data="updated info", instance="a", job="1", label="value"} _ _ 2
+`, engine)
 }
