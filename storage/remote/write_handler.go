9218e8c677ef6adcffd6303f3556252aae2c4465
Tony Li <122298175+tonyli233@users.noreply.github.com>
2024-09-28 11:53:29 -0400

    cleanup (#12)
    Co-authored-by: Junang Li <junangli@Junangs-MacBook-Air.local>

--- storage/remote/write_handler.go
+++ storage/remote/write_handler.go
@@ -191,7 +191,6 @@ func (h *writeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	// Remote Write 2.x proto message handling.
 	req := writev2.RequestFromVTPool()
 	defer req.ReturnToVTPool()
-	// Timeseries as well
 	if err := req.UnmarshalVT(decompressed); err != nil {
 		// TODO(bwplotka): Add more context to responded error?
 		level.Error(h.logger).Log("msg", "Error decoding v2 remote write request", "protobuf_message", msgType, "err", err.Error())
