821ab5ff1e1832efd4a4aa5e12d3544c8d884cea
Fiona Liao <fiona.liao@grafana.com>
2024-11-04 10:54:17 +0000

    Make hacky fix to read-only slice issue
    To check that reset fix works
    
    Signed-off-by: Fiona Liao <fiona.liao@grafana.com>

--- tsdb/chunkenc/histogram.go
+++ tsdb/chunkenc/histogram.go
@@ -106,6 +106,10 @@ func (c *HistogramChunk) GetCounterResetHeader() CounterResetHeader {
 // ClearCounterReset sets the counter reset header to UnknownCounterReset.
 func (c *HistogramChunk) ClearCounterReset() {
 	// FIXME: this could be read only memory when reading from stored blocks
+	// Hacky fix - make copy (needed for read-only slices)
+	writeable := make([]byte, len(c.Bytes()))
+	copy(writeable, c.Bytes())
+	c.Reset(writeable)
 	c.Bytes()[2] = (c.Bytes()[2] & (^CounterResetHeaderMask)) | byte(UnknownCounterReset)
 }
 
