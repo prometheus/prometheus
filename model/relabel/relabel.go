30ff07ea65f23c43294a0a4202914d9c985d6291
Ben Ye <benye@amazon.com>
2024-11-24 10:01:22 -0800

    address comments
    Signed-off-by: Ben Ye <benye@amazon.com>

--- model/relabel/relabel.go
+++ model/relabel/relabel.go
@@ -85,7 +85,7 @@ func (a *Action) UnmarshalYAML(unmarshal func(interface{}) error) error {
 type Config struct {
 	// A list of labels from which values are taken and concatenated
 	// with the configured separator in order.
-	SourceLabels model.LabelNames `yaml:"source_labels,flow,omitempty" json:"source_labels,omitempty"`
+	SourceLabels model.LabelNames `yaml:"source_labels,flow,omitempty" json:"sourceLabels,omitempty"`
 	// Separator is the string between concatenated values from the source labels.
 	Separator string `yaml:"separator,omitempty" json:"separator,omitempty"`
 	// Regex against which the concatenation is matched.
@@ -94,7 +94,7 @@ type Config struct {
 	Modulus uint64 `yaml:"modulus,omitempty" json:"modulus,omitempty"`
 	// TargetLabel is the label to which the resulting string is written in a replacement.
 	// Regexp interpolation is allowed for the replace action.
-	TargetLabel string `yaml:"target_label,omitempty" json:"target_label,omitempty"`
+	TargetLabel string `yaml:"target_label,omitempty" json:"targetLabel,omitempty"`
 	// Replacement is the regex replacement pattern to be used.
 	Replacement string `yaml:"replacement,omitempty" json:"replacement,omitempty"`
 	// Action is the action to be performed for the relabeling.
