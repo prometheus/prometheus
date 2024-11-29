98a82b630dafe56854a200d47c7d3d31ae6b2808
sh0rez <me@shorez.de>
2024-11-29 13:20:50 +0100

    util/otel: noop telemetry
    Signed-off-by: sh0rez <me@shorez.de>

--- go.mod
+++ go.mod
@@ -73,10 +73,8 @@ require (
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.32.0
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc v1.32.0
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp v1.32.0
-	go.opentelemetry.io/otel/exporters/prometheus v0.53.0
 	go.opentelemetry.io/otel/metric v1.32.0
 	go.opentelemetry.io/otel/sdk v1.32.0
-	go.opentelemetry.io/otel/sdk/metric v1.32.0
 	go.opentelemetry.io/otel/trace v1.32.0
 	go.uber.org/atomic v1.11.0
 	go.uber.org/automaxprocs v1.6.0
--- util/otel/meter.go
+++ util/otel/meter.go
@@ -15,36 +15,17 @@ package otel
 import (
 	"github.com/prometheus/client_golang/prometheus"
 
-	promex "go.opentelemetry.io/otel/exporters/prometheus"
 	otel "go.opentelemetry.io/otel/metric"
 	"go.opentelemetry.io/otel/metric/noop"
-	sdk "go.opentelemetry.io/otel/sdk/metric"
 
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/config/configtelemetry"
 )
 
-func MeterProvider(reg prometheus.Registerer) (otel.MeterProvider, error) {
-	if reg == nil {
-		return noop.NewMeterProvider(), nil
-	}
-
-	ex, err := promex.New(promex.WithRegisterer(reg))
-	if err != nil {
-		return nil, err
-	}
-
-	return sdk.NewMeterProvider(sdk.WithReader(ex)), nil
-}
-
 func TelemetrySettings(reg prometheus.Registerer) (component.TelemetrySettings, error) {
+	mp := noop.NewMeterProvider()
+
 	var set component.TelemetrySettings
-
-	mp, err := MeterProvider(reg)
-	if err != nil {
-		return set, err
-	}
-
 	set.MeterProvider = mp
 	set.LeveledMeterProvider = func(_ configtelemetry.Level) otel.MeterProvider {
 		return mp
