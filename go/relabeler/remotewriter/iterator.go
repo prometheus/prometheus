a34eba4f4ac0ed54690b06cd92f1fac80840652a
Alexandr Yudin <alexandr.yudin@flant.com>
2024-12-26 14:16:46 +0300

    for save

--- go/relabeler/remotewriter/iterator.go
+++ go/relabeler/remotewriter/iterator.go
@@ -4,14 +4,16 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"math"
+	"sync"
+	"time"
+
 	"github.com/cenkalti/backoff/v4"
 	"github.com/jonboulle/clockwork"
 	"github.com/prometheus/prometheus/pp/go/cppbridge"
 	"github.com/prometheus/prometheus/pp/go/relabeler/logger"
 	"github.com/prometheus/prometheus/config"
-	"math"
-	"sync"
-	"time"
+	"github.com/prometheus/prometheus/storage/remote"
 )
 
 type DataSource interface {
@@ -243,9 +245,9 @@ readLoop:
 				i.metrics.sentBatchDuration.Observe(i.clock.Since(begin).Seconds())
 				if writeErr != nil {
 					logger.Errorf("failed to send protobuf: %v", writeErr)
-					return
 				}
-				shrd.Delivered = true
+
+				shrd.Delivered = !errors.Is(writeErr, remote.RecoverableError{})
 			}(shrd)
 		}
 		wg.Wait()
@@ -369,7 +371,8 @@ func (i *Iterator) tryAck(_ context.Context) error {
 func (i *Iterator) minTimestamp() int64 {
 	sampleAgeLimit := time.Duration(i.queueConfig.SampleAgeLimit)
 	if sampleAgeLimit == 0 {
-		sampleAgeLimit = time.Hour * 24 * 30
+		// sampleAgeLimit = time.Hour * 24 * 30
+		sampleAgeLimit = time.Hour
 	}
 	return i.clock.Now().Add(-sampleAgeLimit).UnixMilli()
 }
