9823a93c4238b3b549321ece051bbba6d3922741
machine424 <ayoubmrini424@gmail.com>
2024-12-30 05:14:44 +0100

    fix(main.go): avoid closing the query engine until it is guaranteed to no longer be in use.
    partially reverts https://github.com/prometheus/prometheus/pull/14064
    
    fixes https://github.com/prometheus/prometheus/issues/15232
    
    supersedes https://github.com/prometheus/prometheus/pull/15533
    
    reusing Engine.Close() outside of tests will require more consideration.
    
    Signed-off-by: machine424 <ayoubmrini424@gmail.com>

--- cmd/prometheus/main.go
+++ cmd/prometheus/main.go
@@ -989,18 +989,12 @@ func main() {
 	listeners, err := webHandler.Listeners()
 	if err != nil {
 		logger.Error("Unable to start web listener", "err", err)
-		if err := queryEngine.Close(); err != nil {
-			logger.Warn("Closing query engine failed", "err", err)
-		}
 		os.Exit(1)
 	}
 
 	err = toolkit_web.Validate(*webConfig)
 	if err != nil {
 		logger.Error("Unable to validate web configuration file", "err", err)
-		if err := queryEngine.Close(); err != nil {
-			logger.Warn("Closing query engine failed", "err", err)
-		}
 		os.Exit(1)
 	}
 
@@ -1021,9 +1015,6 @@ func main() {
 					logger.Warn("Received termination request via web service, exiting gracefully...")
 				case <-cancel:
 					reloadReady.Close()
-				}
-				if err := queryEngine.Close(); err != nil {
-					logger.Warn("Closing query engine failed", "err", err)
 				}
 				return nil
 			},
