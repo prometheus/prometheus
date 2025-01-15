01b270362531d5f03bbddbd7a8660e07f287eff7
Vandit Singh <vanditsinghkv@gmail.com>
2025-01-15 13:46:30 +0530

    replace prometheus.DefaultRegisterer with single instance of prometheus.NewRegistry
    Signed-off-by: Vandit Singh <vanditsinghkv@gmail.com>

--- cmd/prometheus/main.go
+++ cmd/prometheus/main.go
@@ -139,10 +139,12 @@ var (
 	agentOnlyFlags, serverOnlyFlags []string
 )
 
+var DefaultRegistry = prometheus.NewRegistry()
+
 func init() {
 	// This can be removed when the default validation scheme in common is updated.
 	model.NameValidationScheme = model.UTF8Validation
-	prometheus.MustRegister(versioncollector.NewCollector(strings.ReplaceAll(appName, "-", "_")))
+	DefaultRegistry.MustRegister(versioncollector.NewCollector(strings.ReplaceAll(appName, "-", "_")))
 
 	var err error
 	defaultRetentionDuration, err = model.ParseDuration(defaultRetentionString)
@@ -295,7 +297,7 @@ func main() {
 
 	// Unregister the default GoCollector, and reregister with our defaults.
 	if prometheus.Unregister(collectors.NewGoCollector()) {
-		prometheus.MustRegister(
+		DefaultRegistry.MustRegister(
 			collectors.NewGoCollector(
 				collectors.WithGoCollectorRuntimeMetrics(
 					collectors.MetricsGC,
@@ -308,10 +310,10 @@ func main() {
 
 	cfg := flagConfig{
 		notifier: notifier.Options{
-			Registerer: prometheus.DefaultRegisterer,
+			Registerer: DefaultRegistry,
 		},
 		web: web.Options{
-			Registerer: prometheus.DefaultRegisterer,
+			Registerer: DefaultRegistry,
 			Gatherer:   prometheus.DefaultGatherer,
 		},
 		promslogConfig: promslog.Config{},
@@ -545,7 +547,7 @@ func main() {
 	logger := promslog.New(&cfg.promslogConfig)
 	slog.SetDefault(logger)
 
-	notifs := notifications.NewNotifications(cfg.maxNotificationsSubscribers, prometheus.DefaultRegisterer)
+	notifs := notifications.NewNotifications(cfg.maxNotificationsSubscribers, DefaultRegistry)
 	cfg.web.NotificationsSub = notifs.Sub
 	cfg.web.NotificationsGetter = notifs.Get
 	notifs.AddNotification(notifications.StartingUp)
@@ -699,7 +701,7 @@ func main() {
 	var (
 		localStorage  = &readyStorage{stats: tsdb.NewDBStats()}
 		scraper       = &readyScrapeManager{}
-		remoteStorage = remote.NewStorage(logger.With("component", "remote"), prometheus.DefaultRegisterer, localStorage.StartTime, localStoragePath, time.Duration(cfg.RemoteFlushDeadline), scraper, cfg.scrape.AppendMetadata)
+		remoteStorage = remote.NewStorage(logger.With("component", "remote"), DefaultRegistry, localStorage.StartTime, localStoragePath, time.Duration(cfg.RemoteFlushDeadline), scraper, cfg.scrape.AppendMetadata)
 		fanoutStorage = storage.NewFanout(logger, localStorage, remoteStorage)
 	)
 
@@ -720,25 +722,25 @@ func main() {
 	// can only register metrics specific to a SD instance.
 	// Kubernetes client metrics are the same for the whole process -
 	// they are not specific to an SD instance.
-	err = discovery.RegisterK8sClientMetricsWithPrometheus(prometheus.DefaultRegisterer)
+	err = discovery.RegisterK8sClientMetricsWithPrometheus(DefaultRegistry)
 	if err != nil {
 		logger.Error("failed to register Kubernetes client metrics", "err", err)
 		os.Exit(1)
 	}
 
-	sdMetrics, err := discovery.CreateAndRegisterSDMetrics(prometheus.DefaultRegisterer)
+	sdMetrics, err := discovery.CreateAndRegisterSDMetrics(DefaultRegistry)
 	if err != nil {
 		logger.Error("failed to register service discovery metrics", "err", err)
 		os.Exit(1)
 	}
 
-	discoveryManagerScrape = discovery.NewManager(ctxScrape, logger.With("component", "discovery manager scrape"), prometheus.DefaultRegisterer, sdMetrics, discovery.Name("scrape"))
+	discoveryManagerScrape = discovery.NewManager(ctxScrape, logger.With("component", "discovery manager scrape"), DefaultRegistry, sdMetrics, discovery.Name("scrape"))
 	if discoveryManagerScrape == nil {
 		logger.Error("failed to create a discovery manager scrape")
 		os.Exit(1)
 	}
 
-	discoveryManagerNotify = discovery.NewManager(ctxNotify, logger.With("component", "discovery manager notify"), prometheus.DefaultRegisterer, sdMetrics, discovery.Name("notify"))
+	discoveryManagerNotify = discovery.NewManager(ctxNotify, logger.With("component", "discovery manager notify"), DefaultRegistry, sdMetrics, discovery.Name("notify"))
 	if discoveryManagerNotify == nil {
 		logger.Error("failed to create a discovery manager notify")
 		os.Exit(1)
@@ -749,7 +751,7 @@ func main() {
 		logger.With("component", "scrape manager"),
 		logging.NewJSONFileLogger,
 		fanoutStorage,
-		prometheus.DefaultRegisterer,
+		DefaultRegistry,
 	)
 	if err != nil {
 		logger.Error("failed to create a scrape manager", "err", err)
@@ -789,7 +791,7 @@ func main() {
 	if !agentMode {
 		opts := promql.EngineOpts{
 			Logger:                   logger.With("component", "query engine"),
-			Reg:                      prometheus.DefaultRegisterer,
+			Reg:                      DefaultRegistry,
 			MaxSamples:               cfg.queryMaxSamples,
 			Timeout:                  time.Duration(cfg.queryTimeout),
 			ActiveQueryTracker:       promql.NewActiveQueryTracker(localStoragePath, cfg.queryConcurrency, logger.With("component", "activeQueryTracker")),
@@ -812,7 +814,7 @@ func main() {
 			NotifyFunc:             rules.SendAlerts(notifierManager, cfg.web.ExternalURL.String()),
 			Context:                ctxRule,
 			ExternalURL:            cfg.web.ExternalURL,
-			Registerer:             prometheus.DefaultRegisterer,
+			Registerer:             DefaultRegistry,
 			Logger:                 logger.With("component", "rule manager"),
 			OutageTolerance:        time.Duration(cfg.outageTolerance),
 			ForGracePeriod:         time.Duration(cfg.forGracePeriod),
@@ -966,8 +968,8 @@ func main() {
 		},
 	}
 
-	prometheus.MustRegister(configSuccess)
-	prometheus.MustRegister(configSuccessTime)
+	DefaultRegistry.MustRegister(configSuccess)
+	DefaultRegistry.MustRegister(configSuccessTime)
 
 	// Start all components while we wait for TSDB to open but only load
 	// initial config and mark ourselves as ready after it completed.
@@ -1237,7 +1239,7 @@ func main() {
 					}
 				}
 
-				db, err := openDBWithMetrics(localStoragePath, logger, prometheus.DefaultRegisterer, &opts, localStorage.getStats())
+				db, err := openDBWithMetrics(localStoragePath, logger, DefaultRegistry, &opts, localStorage.getStats())
 				if err != nil {
 					return fmt.Errorf("opening storage failed: %w", err)
 				}
@@ -1289,7 +1291,7 @@ func main() {
 				}
 				db, err := agent.Open(
 					logger,
-					prometheus.DefaultRegisterer,
+					DefaultRegistry,
 					remoteStorage,
 					localStoragePath,
 					&opts,
