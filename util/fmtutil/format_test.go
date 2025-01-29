dd7b37896d658563a9326b52733dd21d6960a2bd
Clark McCauley <clarkmccauley@gmail.com>
2025-01-29 09:51:44 -0700

    Fixing linter errors
    Signed-off-by: Clark McCauley <clarkmccauley@gmail.com>

--- util/fmtutil/format_test.go
+++ util/fmtutil/format_test.go
@@ -16,13 +16,11 @@ package fmtutil
 import (
 	"bytes"
 	dto "github.com/prometheus/client_model/go"
+	"github.com/prometheus/prometheus/prompb"
+	"github.com/stretchr/testify/require"
 	"math"
 	"testing"
 	"time"
-
-	"github.com/stretchr/testify/require"
-
-	"github.com/prometheus/prometheus/prompb"
 )
 
 var writeRequestFixture = &prompb.WriteRequest{
