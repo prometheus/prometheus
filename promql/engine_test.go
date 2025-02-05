026d0198d53376b2539995c6919aafbbe452dfb5
Charles Korn <charles.korn@grafana.com>
2025-02-05 11:40:54 +1100

    Add unit tests
    Signed-off-by: Charles Korn <charles.korn@grafana.com>

--- promql/engine_test.go
+++ promql/engine_test.go
@@ -1900,6 +1900,15 @@ func TestSubquerySelector(t *testing.T) {
 					},
 					Start: time.Unix(35, 0),
 				},
+				{
+					Query: "metric[0:10s]",
+					Result: promql.Result{
+						nil,
+						promql.Matrix{},
+						nil,
+					},
+					Start: time.Unix(10, 0),
+				},
 			},
 		},
 		{
@@ -3250,6 +3259,11 @@ func TestInstantQueryWithRangeVectorSelector(t *testing.T) {
 					},
 				},
 			},
+		},
+		"matches series but range is 0": {
+			expr:     "some_metric[0]",
+			ts:       baseT.Add(2 * time.Minute),
+			expected: promql.Matrix{},
 		},
 	}
 
