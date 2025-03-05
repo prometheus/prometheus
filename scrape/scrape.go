e86b9812f4547fcc4b5ec89fc913a3fb1e8860b9
Bryan Boreham <bjboreham@gmail.com>
2025-03-05 17:08:05 +0000

    [BUGFIX] Scraping: bump cache iteration after error
    Backport of #16174 to LTS release-2.53 branch.
    
    We use the cache iteration number to detect when the same metric has
    occurred twice in a scrape. We need to bump this number at the end of
    every scrape, not just successful scrapes.
    
    The `iter` number is also used:
    * After a successful scrape, to delete older metrics and metadata.
    * To detect when metadata changed in this scrape.
    
    None of those additional cases is broken by incrementing the number
    on error.
    
    Signed-off-by: Bryan Boreham <bjboreham@gmail.com>

--- scrape/scrape.go
+++ scrape/scrape.go
@@ -947,8 +947,6 @@ func (c *scrapeCache) iterDone(flushCache bool) {
 			}
 		}
 		c.metaMtx.Unlock()
-
-		c.iter++
 	}
 
 	// Swap current and previous series.
@@ -958,6 +956,8 @@ func (c *scrapeCache) iterDone(flushCache bool) {
 	for k := range c.seriesCur {
 		delete(c.seriesCur, k)
 	}
+
+	c.iter++
 }
 
 func (c *scrapeCache) get(met []byte) (*cacheEntry, bool, bool) {
