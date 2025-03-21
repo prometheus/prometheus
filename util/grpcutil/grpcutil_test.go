38c6c5e397bd4b7ad01c4bdb5a5119c8ef10a82f
Justin Jung <jungjust@amazon.com>
2025-03-21 12:40:13 -0700

    Lint
    Signed-off-by: Justin Jung <jungjust@amazon.com>

--- util/grpcutil/grpcutil_test.go
+++ util/grpcutil/grpcutil_test.go
@@ -1,3 +1,16 @@
+// Copyright 2025 The Prometheus Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package grpcutil
 
 import (
@@ -14,7 +27,7 @@ func TestStatusCodeFromErrorChain(t *testing.T) {
 	err1 := status.Error(codes.ResourceExhausted, "resource exhausted")
 	err2 := errors.Wrapf(err1, "some error")
 
-	code, ok := StatusCodeFromErrorChain(err2)
+	code, ok := HTTPStatusCodeFromErrorChain(err2)
 	require.True(t, ok)
 	require.Equal(t, int32(codes.ResourceExhausted), code)
 
@@ -24,13 +37,13 @@ func TestStatusCodeFromErrorChain(t *testing.T) {
 	})
 	err2 = errors.Wrapf(err1, "some error")
 
-	code, ok = StatusCodeFromErrorChain(err2)
+	code, ok = HTTPStatusCodeFromErrorChain(err2)
 	require.True(t, ok)
 	require.Equal(t, int32(429), code)
 
 	err1 = errors.New("error without code")
 	err2 = errors.Wrapf(err1, "some error")
 
-	code, ok = StatusCodeFromErrorChain(err2)
+	code, ok = HTTPStatusCodeFromErrorChain(err2)
 	require.False(t, ok)
 }
