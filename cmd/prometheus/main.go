5585a858fa70f838d703678909c03f322c1ecb38
bwplotka <bwplotka@gmail.com>
2025-03-30 22:13:03 +0100

    Add support for overrides.
    Signed-off-by: bwplotka <bwplotka@gmail.com>

--- cmd/prometheus/main.go
+++ cmd/prometheus/main.go
@@ -786,14 +786,15 @@ func main() {
 	)
 
 	var (
-		localStorage  = &readyStorage{stats: tsdb.NewDBStats()}
-		scraper       = &readyScrapeManager{}
+		localStorage    = &readyStorage{stats: tsdb.NewDBStats()}
+		scraper         = &readyScrapeManager{}
 		remoteStorage = remote.NewStorage(logger.With("component", "remote"), prometheus.DefaultRegisterer, localStorage.StartTime, localStoragePath, time.Duration(cfg.RemoteFlushDeadline), scraper, cfg.scrape.EnableTypeAndUnitLabels)
-		fanoutStorage = storage.NewFanout(logger, localStorage, remoteStorage)
+		fanoutStorage   = storage.NewFanout(logger, localStorage, remoteStorage)
+		semconvReloader func(*config.Config) error
 	)
 
 	if cfg.enableSemconvVersionedRead {
-		fanoutStorage = semconv.AwareStorage(fanoutStorage)
+		fanoutStorage, semconvReloader = semconv.AwareStorage(fanoutStorage)
 	}
 
 	var (
@@ -1005,6 +1006,15 @@ func main() {
 					c[k] = v.ServiceDiscoveryConfigs
 				}
 				return discoveryManagerNotify.ApplyConfig(c)
+			},
+		},
+		{
+			name: "semconv",
+			reloader: func(c *config.Config) error {
+				if !cfg.enableSemconvVersionedRead {
+					return nil
+				}
+				return semconvReloader(c)
 			},
 		}, {
 			name: "rules",
