6a079925c9d9cf913f8d0bc15d28b8dca693165c
Ryan Wu <rongjun0821@gmail.com>
2025-04-09 16:37:15 +0800

    check the type and error early and add 'TestEndpointSliceDiscoveryWithUnrelatedServiceUpdate' unit test to give a regression test
    Signed-off-by: Ryan Wu <rongjun0821@gmail.com>

--- discovery/kubernetes/kubernetes.go
+++ discovery/kubernetes/kubernetes.go
@@ -717,40 +717,40 @@ func (d *Discovery) newEndpointsByNodeInformer(plw *cache.ListWatch) cache.Share
 func (d *Discovery) newEndpointSlicesByNodeInformer(plw *cache.ListWatch, object runtime.Object) cache.SharedIndexInformer {
 	indexers := make(map[string]cache.IndexFunc)
 	indexers[serviceIndex] = func(obj interface{}) ([]string, error) {
-		var services []string
-		switch e := obj.(type) {
-		case *disv1.EndpointSlice:
-			if serviceName, exists := e.ObjectMeta.Labels[disv1.LabelServiceName]; exists {
-				return []string{serviceName}, nil
-			}
-		default:
+		e, ok := obj.(*disv1.EndpointSlice)
+		if !ok {
 			return nil, errors.New("object is not an endpointslice")
 		}
 
-		return services, nil
+		svcName, exists := e.ObjectMeta.Labels[disv1.LabelServiceName]
+		if !exists {
+			return nil, nil
+		}
+
+		return []string{svcName}, nil
 	}
 	if !d.attachMetadata.Node {
 		return d.mustNewSharedIndexInformer(plw, object, resyncDisabled, indexers)
 	}
 
 	indexers[nodeIndex] = func(obj interface{}) ([]string, error) {
+		e, ok := obj.(*disv1.EndpointSlice)
+		if !ok {
+			return nil, errors.New("object is not an endpointslice")
+		}
+
 		var nodes []string
-		switch e := obj.(type) {
-		case *disv1.EndpointSlice:
-			for _, target := range e.Endpoints {
-				if target.TargetRef != nil {
-					switch target.TargetRef.Kind {
-					case "Pod":
-						if target.NodeName != nil {
-							nodes = append(nodes, *target.NodeName)
-						}
-					case "Node":
-						nodes = append(nodes, target.TargetRef.Name)
+		for _, target := range e.Endpoints {
+			if target.TargetRef != nil {
+				switch target.TargetRef.Kind {
+				case "Pod":
+					if target.NodeName != nil {
+						nodes = append(nodes, *target.NodeName)
 					}
+				case "Node":
+					nodes = append(nodes, target.TargetRef.Name)
 				}
 			}
-		default:
-			return nil, errors.New("object is not an endpointslice")
 		}
 
 		return nodes, nil
