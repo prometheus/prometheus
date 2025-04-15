780330247cf6248d843fd82775d990f40b438e25
Ryan Wu <rongjun0821@gmail.com>
2025-04-15 21:52:08 +0800

    Update discovery/kubernetes/endpointslice.go
    Co-authored-by: Ayoub Mrini <ayoubmrini424@gmail.com>
    Signed-off-by: Ryan Wu <rongjun0821@gmail.com>

--- discovery/kubernetes/endpointslice.go
+++ discovery/kubernetes/endpointslice.go
@@ -113,7 +113,8 @@ func NewEndpointSlice(l *slog.Logger, eps cache.SharedIndexInformer, svc, pod, n
 			if es.Namespace != svc.Namespace {
 				continue
 			}
-			if lv, exists := es.Labels[v1.LabelServiceName]; exists && lv == svc.Name {
+                        // Only consider the underlying EndpointSlices in the same namespace.
+			if svcName, exists := es.Labels[v1.LabelServiceName]; exists && svcName == svc.Name && es.Namespace == svc.Namespace {
 				e.enqueue(es)
 			}
 		}
