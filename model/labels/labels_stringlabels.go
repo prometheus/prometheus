bdda35c3fc40c8c56a92176d0f4933bc0bcc4604
Jon Kartago Lamida <me@lamida.net>
2025-06-13 13:35:31 +0700

    Add string and slice header size into ByteSize
    Signed-off-by: Jon Kartago Lamida <me@lamida.net>

--- model/labels/labels_stringlabels.go
+++ model/labels/labels_stringlabels.go
@@ -284,9 +284,8 @@ func (ls Labels) WithoutEmpty() Labels {
 }
 
 // ByteSize returns the approximate size of the labels in bytes.
-// String header size is ignored because it should be amortized to zero.
 func (ls Labels) ByteSize() int {
-	return len(ls.data)
+	return len(ls.data) + int(unsafe.Sizeof(""))
 }
 
 // Equal returns whether the two label sets are equal.
