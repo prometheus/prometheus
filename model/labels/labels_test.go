e7ac3f440d918f0c3a4d6c4297be96e9c65f2347
Bryan Boreham <bjboreham@gmail.com>
2025-07-01 11:44:37 +0100

    [TESTS] Labels: Add a test for SizeOfLabels
    This requires a bit of repetition to cover all the different builds, but
    it seems worth checking that the function does what is expected.
    
    Signed-off-by: Bryan Boreham <bjboreham@gmail.com>

--- model/labels/labels_test.go
+++ model/labels/labels_test.go
@@ -37,6 +37,7 @@ var testCaseLabels = []Labels{
 	FromStrings("service.name", "t1", "whatever\\whatever", "t2"),
 	FromStrings("aaa", "111", "xx", s254),
 	FromStrings("aaa", "111", "xx", s255),
+	FromStrings("__name__", "kube_pod_container_status_last_terminated_exitcode", "cluster", "prod-af-north-0", " container", "prometheus", "instance", "kube-state-metrics-0:kube-state-metrics:ksm", "job", "kube-state-metrics/kube-state-metrics", " namespace", "observability-prometheus", "pod", "observability-prometheus-0", "uid", "d3ec90b2-4975-4607-b45d-b9ad64bb417e"),
 }
 
 func TestLabels_String(t *testing.T) {
@@ -46,6 +47,7 @@ func TestLabels_String(t *testing.T) {
 		`{"service.name"="t1", "whatever\\whatever"="t2"}`,
 		`{aaa="111", xx="` + s254 + `"}`,
 		`{aaa="111", xx="` + s255 + `"}`,
+		`{" container"="prometheus", " namespace"="observability-prometheus", __name__="kube_pod_container_status_last_terminated_exitcode", cluster="prod-af-north-0", instance="kube-state-metrics-0:kube-state-metrics:ksm", job="kube-state-metrics/kube-state-metrics", pod="observability-prometheus-0", uid="d3ec90b2-4975-4607-b45d-b9ad64bb417e"}`,
 	}
 	require.Equal(t, len(testCaseLabels), len(expected))
 	for i, c := range expected {
@@ -58,6 +60,17 @@ func BenchmarkString(b *testing.B) {
 	ls := New(benchmarkLabels...)
 	for i := 0; i < b.N; i++ {
 		_ = ls.String()
+	}
+}
+
+func TestSizeOfLabels(t *testing.T) {
+	require.Equal(t, len(testCaseLabels), len(expectedSizeOfLabels))
+	for i, c := range expectedSizeOfLabels { // Declared in build-tag-specific files, e.g. labels_slicelabels_test.go.
+		var total uint64
+		testCaseLabels[i].Range(func(l Label) {
+			total += SizeOfLabels(l.Name, l.Value, 1)
+		})
+		require.Equal(t, c, total)
 	}
 }
 
