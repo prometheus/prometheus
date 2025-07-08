d5fb9162fa1716707f64a95f90bd9189c18eac9b
Kyle Eckhart <kgeckhart@users.noreply.github.com>
2025-07-08 13:47:31 -0400

    RemoveTmpDirs function to tsdbutil
    Signed-off-by: Kyle Eckhart <kgeckhart@users.noreply.github.com>

---
+++ tsdb/tsdbutil/remove_tmp_dirs.go
@@ -0,0 +1,43 @@
+// Copyright 2018 The Prometheus Authors
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package tsdbutil
+
+import (
+	"io/fs"
+	"log/slog"
+	"os"
+	"path/filepath"
+)
+
+// RemoveTmpDirs attempts to remove directories in the specified directory which match the isTmpDir predicate.
+func RemoveTmpDirs(l *slog.Logger, dir string, isTmpDir func(fi fs.DirEntry) bool) error {
+	files, err := os.ReadDir(dir)
+	if os.IsNotExist(err) {
+		return nil
+	}
+	if err != nil {
+		return err
+	}
+	for _, f := range files {
+		if isTmpDir(f) {
+			if err := os.RemoveAll(filepath.Join(dir, f.Name())); err != nil {
+				l.Error("failed to delete tmp dir", "dir", filepath.Join(dir, f.Name()), "err", err)
+				continue
+			}
+			l.Info("Found and deleted tmp dir", "dir", filepath.Join(dir, f.Name()))
+		}
+	}
+	return nil
+}
