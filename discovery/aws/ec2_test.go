d8414a5453438945c4973ce43af06107adca6ac1
Joe Adams <github@joeadams.io>
2025-07-29 23:17:23 -0400

    Fix import formatting
    Signed-off-by: Joe Adams <github@joeadams.io>
    Signed-off-by: Andrew Hall <andrew.hall@grafana.com>

--- discovery/aws/ec2_test.go
+++ discovery/aws/ec2_test.go
@@ -20,9 +20,7 @@ import (
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
-
 	ec2Types "github.com/aws/aws-sdk-go-v2/service/ec2/types"
-
 	"github.com/prometheus/common/model"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/goleak"
