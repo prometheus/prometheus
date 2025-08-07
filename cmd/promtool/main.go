84b19696df1e500695a82ba0cad40122d7d59989
Matthieu MOREL <matthieu.morel35@gmail.com>
2025-08-07 18:12:29 +0200

    chore: enable httpNoBody rule from go-critic
    Signed-off-by: Matthieu MOREL <matthieu.morel35@gmail.com>

--- cmd/promtool/main.go
+++ cmd/promtool/main.go
@@ -559,7 +559,7 @@ func CheckServerStatus(serverURL *url.URL, checkEndpoint string, roundTripper ht
 		return err
 	}
 
-	request, err := http.NewRequest(http.MethodGet, config.Address, nil)
+	request, err := http.NewRequest(http.MethodGet, config.Address, http.NoBody)
 	if err != nil {
 		return err
 	}
