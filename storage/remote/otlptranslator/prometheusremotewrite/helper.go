7cf4b5da55682cbc9472bf61abf041fa7c9981d8
Arve Knudsen <arve.knudsen@gmail.com>
2025-09-09 08:48:50 +0200

    OTLP: Upgrade prometheus/otlptranslator
    The upgrade to prometheus/otlptranslator@7f02967de014 fixes two label
    name translation bugs, when in legacy name translation mode:
    * 'key' is no longer prefixed when label names start with an underscore
    * Multiple consecutive underscores are combined into one
    
    Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>

--- storage/remote/otlptranslator/prometheusremotewrite/helper.go
+++ storage/remote/otlptranslator/prometheusremotewrite/helper.go
@@ -88,7 +88,11 @@ func (c *PrometheusConverter) createAttributes(resource pcommon.Resource, attrib
 	c.scratchBuilder.Sort()
 	sortedLabels := c.scratchBuilder.Labels()
 
-	labelNamer := otlptranslator.LabelNamer{UTF8Allowed: settings.AllowUTF8}
+	labelNamer := otlptranslator.LabelNamer{
+		UTF8Allowed:                 settings.AllowUTF8,
+		UnderscoreLabelSanitization: settings.LabelNameUnderscoreLabelSanitization,
+		PreserveMultipleUnderscores: settings.LabelNamePreserveMultipleUnderscores,
+	}
 
 	if settings.AllowUTF8 {
 		// UTF8 is allowed, so conflicts aren't possible.
