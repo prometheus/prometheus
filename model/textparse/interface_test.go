594f9d63a5f9635d48d6e26b68b708e21630f9ee
NamanParlecha <namanparlecha@gmail.com>
2025-09-11 15:19:42 +0530

    refactor(textparse): Introduce Variadic options in textParse.New (#17155)
    * refactor(textparse): introduce ParserOptions struct for cleaner parser initialization
    
    Signed-off-by: Naman-B-Parlecha <namanparlecha@gmail.com>
    
    * refactor(fuzz): update fuzzParseMetricWithContentType to use ParserOptions
    
    Signed-off-by: Naman-B-Parlecha <namanparlecha@gmail.com>
    
    * refactor(parser): simplify ParserOptions usage in tests and implementations
    
    Signed-off-by: Naman-B-Parlecha <namanparlecha@gmail.com>
    
    * refactor(parse): using variadic options
    
    Signed-off-by: Naman-B-Parlecha <namanparlecha@gmail.com>
    
    * refactor(parser): add fallbackType & SymbolTable to variadic options
    
    Signed-off-by: Naman-B-Parlecha <namanparlecha@gmail.com>
    
    * refactor(parser): private fields
    
    Signed-off-by: Naman-B-Parlecha <namanparlecha@gmail.com>
    
    * refactor(scrape): compose parser options
    
    Signed-off-by: Naman-B-Parlecha <namanparlecha@gmail.com>
    
    * refactor(parser): add comments
    
    Signed-off-by: Naman-B-Parlecha <namanparlecha@gmail.com>
    
    * refactor(parser): update to use ParserOptions struct for configuration
    
    Signed-off-by: Naman-B-Parlecha <namanparlecha@gmail.com>
    
    * refactor(scrape): remove unused parserOptions field from scrapeLoop
    
    Signed-off-by: Naman-B-Parlecha <namanparlecha@gmail.com>
    
    * refactor(parser): update ParserOptions field names and add comments for clarity
    
    Signed-off-by: Naman-B-Parlecha <namanparlecha@gmail.com>
    
    ---------
    
    Signed-off-by: Naman-B-Parlecha <namanparlecha@gmail.com>

--- model/textparse/interface_test.go
+++ model/textparse/interface_test.go
@@ -168,7 +168,7 @@ func TestNewParser(t *testing.T) {
 
 			fallbackProtoMediaType := tt.fallbackScrapeProtocol.HeaderMediaType()
 
-			p, err := New([]byte{}, tt.contentType, fallbackProtoMediaType, false, false, false, false, labels.NewSymbolTable())
+			p, err := New([]byte{}, tt.contentType, labels.NewSymbolTable(), ParserOptions{FallbackContentType: fallbackProtoMediaType})
 			tt.validateParser(t, p)
 			if tt.err == "" {
 				require.NoError(t, err)
