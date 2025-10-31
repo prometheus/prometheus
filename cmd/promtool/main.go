9f93c2d2e13798c22229347d2606a1e7108ad27b
Minh Nguyen <148210689+pipiland2612@users.noreply.github.com>
2025-10-31 13:38:40 +0200

    promtool: Add Remote Write 2.0 support to push metrics command (#17417)
    * add feature flag for remote write v2
    
    Signed-off-by: pipiland2612 <nguyen.t.dang.minh@gmail.com>
    
    * change from number to protobuf_message
    
    Signed-off-by: pipiland2612 <nguyen.t.dang.minh@gmail.com>
    
    * fix test
    
    Signed-off-by: pipiland2612 <nguyen.t.dang.minh@gmail.com>
    
    * fix name
    
    Signed-off-by: pipiland2612 <nguyen.t.dang.minh@gmail.com>
    
    * run make cli-documentation
    
    Signed-off-by: pipiland2612 <nguyen.t.dang.minh@gmail.com>
    
    * fix help
    
    Signed-off-by: pipiland2612 <nguyen.t.dang.minh@gmail.com>
    
    * run make cli-documentation
    
    Signed-off-by: pipiland2612 <nguyen.t.dang.minh@gmail.com>
    
    ---------
    
    Signed-off-by: pipiland2612 <nguyen.t.dang.minh@gmail.com>

--- cmd/promtool/main.go
+++ cmd/promtool/main.go
@@ -222,6 +222,7 @@ func main() {
 	pushMetricsLabels := pushMetricsCmd.Flag("label", "Label to attach to metrics. Can be specified multiple times.").Default("job=promtool").StringMap()
 	pushMetricsTimeout := pushMetricsCmd.Flag("timeout", "The time to wait for pushing metrics.").Default("30s").Duration()
 	pushMetricsHeaders := pushMetricsCmd.Flag("header", "Prometheus remote write header.").StringMap()
+	pushMetricsProtoMsg := pushMetricsCmd.Flag("protobuf_message", "Protobuf message to use when writing (prometheus.WriteRequest or io.prometheus.write.v2.Request).").Default("prometheus.WriteRequest").String()
 
 	testCmd := app.Command("test", "Unit testing.")
 	junitOutFile := testCmd.Flag("junit", "File path to store JUnit XML test results.").OpenFile(os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0o644)
@@ -377,7 +378,7 @@ func main() {
 		os.Exit(CheckMetrics(*checkMetricsExtended))
 
 	case pushMetricsCmd.FullCommand():
-		os.Exit(PushMetrics(remoteWriteURL, httpRoundTripper, *pushMetricsHeaders, *pushMetricsTimeout, *pushMetricsLabels, *metricFiles...))
+		os.Exit(PushMetrics(remoteWriteURL, httpRoundTripper, *pushMetricsHeaders, *pushMetricsTimeout, *pushMetricsProtoMsg, *pushMetricsLabels, *metricFiles...))
 
 	case queryInstantCmd.FullCommand():
 		os.Exit(QueryInstant(serverURL, httpRoundTripper, *queryInstantExpr, *queryInstantTime, p))
