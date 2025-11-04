614f039904c7b831c1f713dbd708f48f0264399c
bwplotka <bwplotka@gmail.com>
2025-11-04 13:49:07 +0000

    move to separate benches per op
    Signed-off-by: bwplotka <bwplotka@gmail.com>

--- tsdb/chunkenc/benchmark_test.go
+++ tsdb/chunkenc/benchmark_test.go
@@ -2,6 +2,7 @@ package chunkenc
 
 import (
 	"errors"
+	"fmt"
 	"io"
 	"math/rand"
 	"testing"
@@ -9,120 +10,172 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-/*
-	export bench=chunkenc && go test \
-	  -run '^$' -bench '^BenchmarkChunk' \
-	  -benchtime 5s -count 6 -cpu 2 -timeout 999m \
-	  | tee ${bench}.txt
-*/
-func BenchmarkChunk(b *testing.B) {
-	r := rand.New(rand.NewSource(1))
+type sampleCase struct {
+	name    string
+	samples []pair
+}
 
-	for _, data := range []struct {
-		name   string
-		deltas func() (int64, float64)
-	}{
+type fmtCase struct {
+	name       string
+	newChunkFn func() Chunk
+}
+
+func sampleCases() []sampleCase {
+	const nSamples = 120 // Same as tsdb.DefaultSamplesPerChunk.
+
+	var (
+		r     = rand.New(rand.NewSource(1))
+		initT = int64(1234123324)
+		initV = 1243535.123
+	)
+
+	return []sampleCase{
 		{
 			name: "constant",
-			deltas: func() (int64, float64) {
-				return 1000, 0
-			},
+			samples: func() (ret []pair) {
+				t, v := initT, initV
+				for range nSamples {
+					t += 1000
+					ret = append(ret, pair{t: t, v: v})
+				}
+				return ret
+			}(),
 		},
 		{
 			name: "random steps",
-			deltas: func() (int64, float64) {
-				return int64(r.Intn(100) - 50 + 15000), // 15 seconds +- up to 100ms of jitter.
-					float64(r.Intn(100) - 50) // Varying from -50 to +50 in 100 discrete steps.
-			},
+			samples: func() (ret []pair) {
+				t, v := initT, initV
+				for range nSamples {
+					t += int64(r.Intn(100) - 50 + 15000) // 15 seconds +- up to 100ms of jitter.
+					v += float64(r.Intn(100) - 50)       // Varying from -50 to +50 in 100 discrete steps.
+					ret = append(ret, pair{t: t, v: v})
+				}
+				return ret
+			}(),
 		},
 		{
 			name: "random 0-1",
-			deltas: func() (int64, float64) {
-				return int64(r.Intn(100) - 50 + 15000), // 15 seconds +- up to 100ms of jitter.
-					r.Float64() // Random between 0 and 1.0.
-			},
+			samples: func() (ret []pair) {
+				t, v := initT, initV
+				for range nSamples {
+					t += int64(r.Intn(100) - 50 + 15000) // 15 seconds +- up to 100ms of jitter.
+					v += r.Float64()                     // Random between 0 and 1.0.
+					ret = append(ret, pair{t: t, v: v})
+				}
+				return ret
+			}(),
 		},
-	} {
-		b.Run("data="+data.name, func(b *testing.B) {
-			b.Run("format=XOR", func(b *testing.B) {
-				benchmarkChunk(b, data.deltas, func() Chunk { return NewXORChunk() })
-			})
-		})
 	}
 }
 
-func benchmarkChunk(b *testing.B, deltas func() (int64, float64), newChunk func() Chunk) {
-	var (
-		t = int64(1234123324)
-		v = 1243535.123
-	)
-	const nSamples = 120 // Same as tsdb.DefaultSamplesPerChunk.
-	var samples []pair
-	for range nSamples {
-		dt, dv := deltas()
-		t += dt
-		v += dv
-		samples = append(samples, pair{t: t, v: v})
+func fmtCases() []fmtCase {
+	return []fmtCase{
+		{name: "XOR", newChunkFn: func() Chunk { return NewXORChunk() }},
 	}
+}
 
-	// Measure encoding efficiency.
-	b.Run("op=Append", func(b *testing.B) {
-		b.ReportAllocs()
+/*
+	export bench=append && go test \
+	  -run '^$' -bench '^BenchmarkAppender' \
+	  -benchtime 5s -count 6 -cpu 2 -timeout 999m \
+	  | tee ${bench}.txt
+*/
+func BenchmarkAppender(b *testing.B) {
+	for _, fc := range fmtCases() {
+		for _, sc := range sampleCases() {
+			b.Run(fmt.Sprintf("fmt=%s/samples=%s", fc.name, sc.name), func(b *testing.B) {
+				b.ReportAllocs()
 
-		for b.Loop() {
-			c := newChunk()
+				for b.Loop() {
+					c := fc.newChunkFn()
 
-			a, err := c.Appender()
-			if err != nil {
-				b.Fatalf("get appender: %s", err)
-			}
-			for _, p := range samples {
-				a.Append(p.t, p.v)
-			}
-			b.ReportMetric(float64(len(c.Bytes())), "B/chunk")
+					a, err := c.Appender()
+					if err != nil {
+						b.Fatalf("get appender: %s", err)
+					}
+					for _, p := range sc.samples {
+						a.Append(p.t, p.v)
+					}
+					b.ReportMetric(float64(len(c.Bytes())), "B/chunk")
+				}
+			})
 		}
-	})
-
-	// Create a chunk for decoding and compact benchmarks.
-	c := newChunk()
-	a, err := c.Appender()
-	if err != nil {
-		b.Fatalf("get appender: %s", err)
 	}
-	for _, p := range samples {
-		a.Append(p.t, p.v)
+}
+
+/*
+	export bench=iter && go test \
+	  -run '^$' -bench '^BenchmarkIterator' \
+	  -benchtime 5s -count 6 -cpu 2 -timeout 999m \
+	  | tee ${bench}.txt
+*/
+func BenchmarkIterator(b *testing.B) {
+	for _, fc := range fmtCases() {
+		for _, sc := range sampleCases() {
+			b.Run(fmt.Sprintf("fmt=%s/samples=%s", fc.name, sc.name), func(b *testing.B) {
+				b.ReportAllocs()
+
+				c := fc.newChunkFn()
+				a, err := c.Appender()
+				if err != nil {
+					b.Fatalf("get appender: %s", err)
+				}
+				for _, p := range sc.samples {
+					a.Append(p.t, p.v)
+				}
+
+				var (
+					sink float64
+					it   Iterator
+				)
+				// Measure decoding efficiency.
+				for i := 0; b.Loop(); {
+					b.ReportMetric(float64(len(c.Bytes())), "B/chunk")
+
+					it := c.Iterator(it)
+					for it.Next() == ValFloat {
+						_, v := it.At()
+						sink = v
+						i++
+					}
+					if err := it.Err(); err != nil && !errors.Is(err, io.EOF) {
+						require.NoError(b, err)
+					}
+					_ = sink
+				}
+			})
+		}
 	}
+}
 
-	// Measure decoding efficiency.
-	b.Run("op=Iterate", func(b *testing.B) {
-		b.ReportAllocs()
+/*
+	export bench=compact && go test \
+	  -run '^$' -bench '^BenchmarkCompact' \
+	  -benchtime 5s -count 6 -cpu 2 -timeout 999m \
+	  | tee ${bench}.txt
+*/
+func BenchmarkCompact(b *testing.B) {
+	for _, fc := range fmtCases() {
+		for _, sc := range sampleCases() {
+			b.Run(fmt.Sprintf("fmt=%s/samples=%s", fc.name, sc.name), func(b *testing.B) {
+				b.ReportAllocs()
 
-		var (
-			sink float64
-			it   Iterator
-		)
-		for i := 0; b.Loop(); {
-			b.ReportMetric(float64(len(c.Bytes())), "B/chunk")
+				c := fc.newChunkFn()
+				a, err := c.Appender()
+				if err != nil {
+					b.Fatalf("get appender: %s", err)
+				}
+				for _, p := range sc.samples {
+					a.Append(p.t, p.v)
+				}
 
-			it := c.Iterator(it)
-			for it.Next() == ValFloat {
-				_, v := it.At()
-				sink = v
-				i++
-			}
-			if err := it.Err(); err != nil && !errors.Is(err, io.EOF) {
-				require.NoError(b, err)
-			}
-			_ = sink
+				// Measure size before and after compaction and it's efficiency.
+				for b.Loop() {
+					b.ReportMetric(float64(len(c.Bytes())), "B/chunk")
+					c.Compact()
+					b.ReportMetric(float64(len(c.Bytes())), "B/compacted_chunk")
+				}
+			})
 		}
-	})
-
-	// Measure size before and after compaction and it's efficiency.
-	b.Run("op=Compact", func(b *testing.B) {
-		for b.Loop() {
-			b.ReportMetric(float64(len(c.Bytes())), "B/chunk")
-			c.Compact()
-			b.ReportMetric(float64(len(c.Bytes())), "B/compacted_chunk")
-		}
-	})
+	}
 }
