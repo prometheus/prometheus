9d508a4888edc2d9a7fd41e071482018003ae25d
Clark McCauley <clarkmccauley@gmail.com>
2025-11-11 04:43:04 -0700

    util: add +Inf bucket in MetricFamiliesToWriteRequest when not present (#15864)
    * Add +Inf bucket in MetricFamiliesToWriteRequest if not present
    
    Signed-off-by: Clark McCauley <clarkmccauley@gmail.com>

--- util/fmtutil/format.go
+++ util/fmtutil/format.go
@@ -18,6 +18,7 @@ import (
 	"fmt"
 	"io"
 	"maps"
+	"math"
 	"sort"
 	"time"
 
@@ -140,11 +141,23 @@ func makeTimeseries(wr *prompb.WriteRequest, labels map[string]string, m *dto.Me
 		// Add Histogram bucket timeseries
 		bucketLabels := make(map[string]string, len(labels)+1)
 		maps.Copy(bucketLabels, labels)
+		var hasInf bool
 		for _, b := range m.GetHistogram().Bucket {
+			if b.GetUpperBound() == math.Inf(1) {
+				hasInf = true
+			}
 			bucketLabels[model.MetricNameLabel] = metricName + bucketStr
 			bucketLabels[model.BucketLabel] = fmt.Sprint(b.GetUpperBound())
 			toTimeseries(wr, bucketLabels, timestamp, float64(b.GetCumulativeCount()))
 		}
+
+		// Add +Inf bucket if not present
+		if !hasInf {
+			bucketLabels[model.MetricNameLabel] = metricName + bucketStr
+			bucketLabels[model.BucketLabel] = "+Inf"
+			toTimeseries(wr, bucketLabels, timestamp, float64(m.GetHistogram().GetSampleCount()))
+		}
+
 		// Overwrite label model.MetricNameLabel for count and sum metrics
 		// Add Histogram sum timeseries
 		labels[model.MetricNameLabel] = metricName + sumStr
