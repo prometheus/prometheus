b8febd7c5684d1bf4de76a9ac2f92759ff96f49f
Arve Knudsen <arve.knudsen@gmail.com>
2025-12-15 18:44:32 +0100

    promql: Implement support for versioned OTel semantic conventions
    Implement support for versioned OTel semantic conventions, with
    automatic querying for all metric versions.
    
    Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>

--- cmd/prometheus/main.go
+++ cmd/prometheus/main.go
@@ -74,6 +74,7 @@ import (
 	"github.com/prometheus/prometheus/scrape"
 	"github.com/prometheus/prometheus/storage"
 	"github.com/prometheus/prometheus/storage/remote"
+	"github.com/prometheus/prometheus/storage/semconv"
 	"github.com/prometheus/prometheus/template"
 	"github.com/prometheus/prometheus/tracing"
 	"github.com/prometheus/prometheus/tsdb"
@@ -223,6 +224,8 @@ type flagConfig struct {
 	parserOpts parser.Options
 
 	promslogConfig promslog.Config
+
+	enableSemconvVersionedRead bool
 }
 
 // setFeatureListOptions sets the corresponding options from the featureList.
@@ -333,6 +336,9 @@ func (c *flagConfig) setFeatureListOptions(logger *slog.Logger) error {
 			case "fast-startup":
 				c.tsdb.EnableFastStartup = true
 				logger.Info("Experimental fast startup is enabled.")
+			case "semconv-versioned-read":
+				c.enableSemconvVersionedRead = true
+				logger.Info("Experimental OTel semconv versioned read enabled")
 			default:
 				logger.Warn("Unknown option for --enable-feature", "option", o)
 			}
@@ -872,6 +878,10 @@ func main() {
 		remoteStorage = remote.NewStorage(logger.With("component", "remote"), prometheus.DefaultRegisterer, localStorage.StartTime, localStoragePath, time.Duration(cfg.RemoteFlushDeadline), scraper, cfg.scrape.EnableTypeAndUnitLabels)
 		fanoutStorage = storage.NewFanout(logger, localStorage, remoteStorage)
 	)
+
+	if cfg.enableSemconvVersionedRead {
+		fanoutStorage = semconv.AwareStorage(fanoutStorage)
+	}
 
 	var (
 		ctxWeb, cancelWeb = context.WithCancel(context.Background())
