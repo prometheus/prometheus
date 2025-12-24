54e6eb08c8a71b90b64df85ba42fd5646f9b7657
aimuz <mr.imuz@gmail.com>
2025-12-24 09:32:45 +0800

    license header
    Signed-off-by: aimuz <mr.imuz@gmail.com>

--- tsdb/wlog/benchmark_test.go
+++ tsdb/wlog/benchmark_test.go
@@ -1,7 +1,22 @@
+// Copyright 2025 The Prometheus Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package wlog
 
 import (
 	"bytes"
+	"errors"
 	"io"
 	"os"
 	"testing"
@@ -50,7 +65,7 @@ func BenchmarkLiveReader_ReadSegment(b *testing.B) {
 			_ = r.Record()
 		}
 		r.Close()
-		if r.Err() != nil && r.Err() != io.EOF {
+		if r.Err() != nil && !errors.Is(r.Err(), io.EOF) {
 			b.Fatal(r.Err())
 		}
 	}
