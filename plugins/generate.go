b78cb4099fde5a169fcbbb6b651ff9301fa74769
Arthur Silva Sens <arthursens2005@gmail.com>
2026-01-03 10:06:34 -0300

    Remove year from License header
    Signed-off-by: Arthur Silva Sens <arthursens2005@gmail.com>

--- plugins/generate.go
+++ plugins/generate.go
@@ -27,7 +27,7 @@ import (
 
 //go:generate go run generate.go
 
-const licenseHeader = `// Copyright 2022 The Prometheus Authors
+const licenseHeader = `// Copyright The Prometheus Authors
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
