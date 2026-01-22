9b549fa1183b008d10f77cd7453c06a406832ada
Ben Kochie <superq@gmail.com>
2026-01-22 16:36:30 +0100

    Update Go yaml v3 library (#17913)
    Replace archived `gopkg.in/yaml.v3` with supported `go.yaml.in/yaml/v3`.
    
    Fixes: https://github.com/prometheus/prometheus/issues/16415
    
    Signed-off-by: SuperQ <superq@gmail.com>

--- model/rulefmt/rulefmt.go
+++ model/rulefmt/rulefmt.go
@@ -24,7 +24,7 @@ import (
 	"time"
 
 	"github.com/prometheus/common/model"
-	"gopkg.in/yaml.v3"
+	"go.yaml.in/yaml/v3"
 
 	"github.com/prometheus/prometheus/model/timestamp"
 	"github.com/prometheus/prometheus/promql"
