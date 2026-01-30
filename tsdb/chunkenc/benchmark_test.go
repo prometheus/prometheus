a0314d5539a14992fe46416fec8fde505871d50c
György Krajcsovits <gyorgy.krajcsovits@grafana.com>
2026-01-30 09:01:29 +0100

    Fix benchmark expectations on ST in interator
    Signed-off-by: György Krajcsovits <gyorgy.krajcsovits@grafana.com>

--- tsdb/chunkenc/benchmark_test.go
+++ tsdb/chunkenc/benchmark_test.go
@@ -213,7 +213,7 @@ func foreachFmtSampleCase(b *testing.B, fn func(b *testing.B, f fmtCase, s sampl
 	}
 
 	for _, f := range []fmtCase{
-		{name: "XOR", newChunkFn: func() Chunk { return NewXORChunk() }},
+		{name: "XOR", newChunkFn: func() Chunk { return NewXORChunk() }, stUnsupported: true},
 		{name: "XOR_OPT_ST", newChunkFn: func() Chunk { return NewXOROptSTChunk() }},
 	} {
 		for _, s := range sampleCases {
