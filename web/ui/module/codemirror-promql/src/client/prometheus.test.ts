de3936dfd086b52c4dfe44283a21855d956a8f50
Arve Knudsen <arve.knudsen@gmail.com>
2026-02-05 14:46:36 +0100

    lezer-promql: add PrometheusClient for info() autocomplete
    Port the PrometheusClient interface and implementation from
    codemirror-promql to lezer-promql. This allows Grafana's UI to use
    lezer-promql directly for info() function autocomplete support.
    
    Changes:
    - Add TypeScript client code to lezer-promql with HTTPPrometheusClient
      and CachedPrometheusClient classes
    - Update rollup.config.js to bundle client code in ESM and CJS formats
    - Add subpath export "./client" in package.json
    - Update codemirror-promql to re-export from lezer-promql/client
    - Add Jest resolver to handle subpath exports in tests
    
    Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>

--- web/ui/module/codemirror-promql/src/client/prometheus.test.ts
+++ web/ui/module/codemirror-promql/src/client/prometheus.test.ts
@@ -11,7 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import { HTTPPrometheusClient, CachedPrometheusClient } from './prometheus';
+import { HTTPPrometheusClient, CachedPrometheusClient } from '.';
 
 describe('HTTPPrometheusClient destroy', () => {
   it('should be safe to call destroy multiple times', () => {
