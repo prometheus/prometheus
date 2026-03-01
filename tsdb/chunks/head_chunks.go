8bd7a3a114ee289f3d7da6d2a8374b2ae3f9a9aa
komisan19 <18901496+komisan19@users.noreply.github.com>
2026-03-01 15:50:03 +0900

    chore: replace make+copy byte clones with bytes.Clone()
    Signed-off-by: komisan19 <18901496+komisan19@users.noreply.github.com>

--- tsdb/chunks/head_chunks.go
+++ tsdb/chunks/head_chunks.go
@@ -15,6 +15,7 @@ package chunks
 
 import (
 	"bufio"
+	"bytes"
 	"encoding/binary"
 	"errors"
 	"fmt"
@@ -811,10 +812,7 @@ func (cdm *ChunkDiskMapper) Chunk(ref ChunkDiskMapperRef) (chunkenc.Chunk, error
 	// Make a copy of the chunk data to prevent a panic occurring because the returned
 	// chunk data slice references an mmap-ed file which could be closed after the
 	// function returns but while the chunk is still in use.
-	chkDataCopy := make([]byte, len(chkData))
-	copy(chkDataCopy, chkData)
-
-	chk, err := cdm.pool.Get(chunkenc.Encoding(chkEnc), chkDataCopy)
+	chk, err := cdm.pool.Get(chunkenc.Encoding(chkEnc), bytes.Clone(chkData))
 	if err != nil {
 		return nil, &CorruptionErr{
 			Dir:       cdm.dir.Name(),
