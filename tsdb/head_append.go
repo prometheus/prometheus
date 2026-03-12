c4a701aae2eb7a65ef943ac966b0eaab51502204
Arve Knudsen <arve.knudsen@gmail.com>
2026-03-12 06:32:07 +0100

    seriesmetadata: add observability metrics for resource/scope metadata
    Add metrics to detect memory pressure from series metadata early:
    
    GaugeFunc metrics (per-tenant, read from live MemStore state):
    - prometheus_tsdb_head_seriesmetadata_{resource,scope}_entries
    - prometheus_tsdb_head_seriesmetadata_{resource,scope}_versions
    - prometheus_tsdb_head_seriesmetadata_{resource,scope}_canonical
    - prometheus_tsdb_head_seriesmetadata_attr_index_keys
    
    Counter metrics (with kind="resource"|"scope" label):
    - prometheus_tsdb_head_seriesmetadata_content_changes_total
    - prometheus_tsdb_head_seriesmetadata_inserts_total
    
    Gauge metric:
    - prometheus_tsdb_head_seriesmetadata_wal_replay_duration_seconds
    
    These would have caught the significant memory scope metadata issue early by
    showing version count growing disproportionately to entry count.
    
    Signed-off-by: Arve Knudsen <arve.knudsen@gmail.com>

--- tsdb/head_append.go
+++ tsdb/head_append.go
@@ -1913,6 +1913,10 @@ func (a *headAppenderBase) commitAndFilterResources(b *appendBatch) int {
 		if !contentChanged {
 			continue
 		}
+		if oldVR == nil {
+			a.head.metrics.seriesmetadataInserts.WithLabelValues("resource").Inc()
+		}
+		a.head.metrics.seriesmetadataContentChanges.WithLabelValues("resource").Inc()
 		a.head.seriesMeta.UpdateResourceAttrIndex(hash, oldVR, newVR)
 		b.resources[n] = b.resources[i]
 		b.resourceSeries[n] = b.resourceSeries[i]
@@ -1943,7 +1947,8 @@ func (a *headAppenderBase) commitAndFilterScopes(b *appendBatch) int {
 		s.Unlock()
 
 		var contentChanged bool
-		contentChanged, _, _, keysBuf = seriesmetadata.CommitScopeToStoreReusableWithRef(store, hash, seriesmetadata.ScopeCommitData{
+		var oldVS *seriesmetadata.VersionedScope
+		contentChanged, oldVS, _, keysBuf = seriesmetadata.CommitScopeToStoreReusableWithRef(store, hash, seriesmetadata.ScopeCommitData{
 			Name:      sc.Name,
 			Version:   sc.Version,
 			SchemaURL: sc.SchemaURL,
@@ -1956,6 +1961,10 @@ func (a *headAppenderBase) commitAndFilterScopes(b *appendBatch) int {
 		if !contentChanged {
 			continue
 		}
+		if oldVS == nil {
+			a.head.metrics.seriesmetadataInserts.WithLabelValues("scope").Inc()
+		}
+		a.head.metrics.seriesmetadataContentChanges.WithLabelValues("scope").Inc()
 		b.scopes[n] = b.scopes[i]
 		b.scopeSeries[n] = b.scopeSeries[i]
 		n++
