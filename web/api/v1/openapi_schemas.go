d4935fbf6e64ec350bb42d8b8191a44a9e3580d1
Julius Volz <julius.volz@gmail.com>
2026-04-10 19:13:39 +0200

    Fix OpenAPI defs
    Signed-off-by: Julius Volz <julius.volz@gmail.com>

--- web/api/v1/openapi_schemas.go
+++ web/api/v1/openapi_schemas.go
@@ -112,6 +112,7 @@ func (b *OpenAPIBuilder) buildComponents() *v3.Components {
 	schemas.Set("StatusTSDBBlocksOutputBody", b.refResponseBodySchema("StatusTSDBBlocksData", "Response body for status TSDB blocks endpoint."))
 	schemas.Set("StatusWALReplayData", b.statusWALReplayDataSchema())
 	schemas.Set("StatusWALReplayOutputBody", b.refResponseBodySchema("StatusWALReplayData", "Response body for status WAL replay endpoint."))
+	schemas.Set("StatusSelfMetricsOutputBody", b.simpleResponseBodySchema())
 
 	// Admin schemas.
 	schemas.Set("DeleteSeriesOutputBody", b.statusOnlyResponseBodySchema())
