5e36ed265a4fdd6beef2f31cab2b66b0dedfecc4
Jeon Insoo <mumberrymountain@gmail.com>
2026-05-01 14:33:04 +0900

    fix: fix empty results when @ subquery is matrix arg with step-varying scalars (#18610)
    Signed-off-by: Jeon Insoo <mumberrymountain@gmail.com>

--- promql/engine.go
+++ promql/engine.go
@@ -4431,9 +4431,14 @@ func preprocessExprHelper(expr parser.Expr, start, end time.Time) (isStepInvaria
 		}
 
 		for i, isi := range shouldWrap {
-			if isi {
-				n.Args[i] = newStepInvariantExpr(n.Args[i])
+			if !isi {
+				continue
 			}
+			
+			if _, ok := n.Args[i].(*parser.SubqueryExpr); ok {
+				continue
+			}
+			n.Args[i] = newStepInvariantExpr(n.Args[i])
 		}
 		return false, false
 
