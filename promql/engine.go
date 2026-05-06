0baf1ca3df0cb78d0326eca34e1cf3e651aa7016
vpranckaitis <vpranckaitis@gmail.com>
2026-05-06 15:33:57 +0300

    PromQL: `resets()` function considers start timestamp resets
    Signed-off-by: vpranckaitis <vpranckaitis@gmail.com>

--- promql/engine.go
+++ promql/engine.go
@@ -2100,7 +2100,7 @@ func (ev *evaluator) eval(ctx context.Context, expr parser.Expr) (parser.Value, 
 		var chkIter chunkenc.Iterator
 
 		var startTimestamps *StartTimestamps
-		if ev.useStartTimestamps && (e.Func.Name == "rate" || e.Func.Name == "irate" || e.Func.Name == "increase") {
+		if ev.useStartTimestamps && (e.Func.Name == "rate" || e.Func.Name == "irate" || e.Func.Name == "increase" || e.Func.Name == "resets") {
 			// TODO: consider pooling this.
 			startTimestamps = &StartTimestamps{}
 		}
