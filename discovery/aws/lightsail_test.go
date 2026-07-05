83d923934695fa0e33ac9655682e0a6249ee3950
Anas Khan <83116240+anxkhn@users.noreply.github.com>
2026-07-05 08:50:31 +0530

    discovery/aws: paginate Lightsail GetInstances
    GetInstances is paginated, but Lightsail service discovery only returned the first page of instances.
    
    Collect all pages in the client adapter and test that page tokens are followed and instances are combined.
    
    Signed-off-by: Anas Khan <83116240+anxkhn@users.noreply.github.com>

---
+++ discovery/aws/lightsail_test.go
@@ -0,0 +1,60 @@
+// Copyright The Prometheus Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package aws
+
+import (
+	"context"
+	"testing"
+
+	"github.com/aws/aws-sdk-go-v2/service/lightsail"
+	lightsailTypes "github.com/aws/aws-sdk-go-v2/service/lightsail/types"
+	"github.com/stretchr/testify/require"
+)
+
+func TestLightsailClientAdapterGetInstancesPaginated(t *testing.T) {
+	ctx := context.Background()
+
+	pages := []*lightsail.GetInstancesOutput{
+		{
+			Instances:     []lightsailTypes.Instance{{Name: strptr("instance-1")}},
+			NextPageToken: strptr("page-2"),
+		},
+		{
+			Instances: []lightsailTypes.Instance{{Name: strptr("instance-2")}},
+		},
+	}
+
+	var calls int
+	client := &lightsailClientAdapter{
+		getInstances: func(_ context.Context, params *lightsail.GetInstancesInput, _ ...func(*lightsail.Options)) (*lightsail.GetInstancesOutput, error) {
+			if calls == 0 {
+				require.Nil(t, params.PageToken)
+			} else {
+				require.Equal(t, "page-2", *params.PageToken)
+			}
+			out := pages[calls]
+			calls++
+			return out, nil
+		},
+	}
+
+	output, err := client.GetInstances(ctx, &lightsail.GetInstancesInput{})
+	require.NoError(t, err)
+	require.Equal(t, 2, calls)
+	require.Equal(t, []lightsailTypes.Instance{
+		{Name: strptr("instance-1")},
+		{Name: strptr("instance-2")},
+	}, output.Instances)
+	require.Nil(t, output.NextPageToken)
+}
