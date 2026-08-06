361f1cc7777aa8f11fe7d3d0905b1e9f8e76967b
Minh Vu <vuhoangminh97@gmail.com>
2026-08-06 17:37:56 +0200

    discovery/aws: log invalid ElastiCache ARNs
    Signed-off-by: Minh Vu <vuhoangminh97@gmail.com>

--- discovery/aws/elasticache_test.go
+++ discovery/aws/elasticache_test.go
@@ -22,6 +22,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/elasticache"
 	"github.com/aws/aws-sdk-go-v2/service/elasticache/types"
 	"github.com/prometheus/common/model"
+	"github.com/prometheus/common/promslog"
 	"github.com/stretchr/testify/require"
 
 	"github.com/prometheus/prometheus/discovery/targetgroup"
@@ -612,7 +613,7 @@ func TestSplitCacheDeploymentOptions(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			serverlessCacheIDs, cacheClusterIDs := splitCacheDeploymentOptions(tt.caches)
+			serverlessCacheIDs, cacheClusterIDs := splitCacheDeploymentOptions(tt.caches, promslog.NewNopLogger())
 
 			require.Equal(t, tt.expectedServerlessCacheIDs, serverlessCacheIDs, "serverless cache IDs mismatch")
 			require.Equal(t, tt.expectedCacheClusterIDs, cacheClusterIDs, "cache cluster IDs mismatch")
