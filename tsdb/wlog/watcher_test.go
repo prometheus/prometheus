88e681434df7f51877a65ee5f82c53ffc0f748ff
Yuri Nikolic <durica.nikolic@grafana.com>
2026-09-22 17:49:33 +0200

    wlog: exercise MinValidTime checkpoint records in watcher startup tests
    Signed-off-by: Yuri Nikolic <durica.nikolic@grafana.com>

--- tsdb/wlog/watcher_test.go
+++ tsdb/wlog/watcher_test.go
@@ -453,7 +453,7 @@ func TestReadToEndWithCheckpoint(t *testing.T) {
 					}
 				}
 
-				Checkpoint(promslog.NewNopLogger(), w, 0, 1, func(chunks.HeadSeriesRef) bool { return true }, 0, enableSTStorage, false)
+				Checkpoint(promslog.NewNopLogger(), w, 0, 1, func(chunks.HeadSeriesRef) bool { return true }, 0, enableSTStorage, true)
 				w.Truncate(1)
 
 				// Write more records after checkpointing.
@@ -546,7 +546,7 @@ func TestReadCheckpoint(t *testing.T) {
 				}
 				_, err = w.NextSegmentSync()
 				require.NoError(t, err)
-				_, err = Checkpoint(promslog.NewNopLogger(), w, 30, 31, func(chunks.HeadSeriesRef) bool { return true }, 0, enableSTStorage, false)
+				_, err = Checkpoint(promslog.NewNopLogger(), w, 30, 31, func(chunks.HeadSeriesRef) bool { return true }, 0, enableSTStorage, true)
 				require.NoError(t, err)
 				require.NoError(t, w.Truncate(32))
 
