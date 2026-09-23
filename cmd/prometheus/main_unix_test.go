873baf4f8e27012d686bf9bf1b05aa04bee477b7
nikhil2004 <myemailnikhilsaxena@gmail.com>
2026-09-23 18:55:44 +0530

    ci: fix flaky TestStartupInterrupt and FuzzParseOpenMetric timeouts
    Signed-off-by: nikhil2004 <myemailnikhilsaxena@gmail.com>

--- cmd/prometheus/main_unix_test.go
+++ cmd/prometheus/main_unix_test.go
@@ -53,7 +53,7 @@ func TestStartupInterrupt(t *testing.T) {
 	url := "http://localhost" + port + "/graph"
 
 Loop:
-	for range 10 {
+	for range 60 {
 		// error=nil means prometheus has started, so we can send the interrupt
 		// signal and wait for the graceful shutdown.
 		if _, err := http.Get(url); err == nil {
